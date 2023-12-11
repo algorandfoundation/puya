320fec547746da5b3d58e0203ff6cd2b42da580f
Adam Chidlow <achidlow@users.noreply.github.com>
2023-12-11 21:41:33 +0800

    🤫

--- src/puyapy-stubs/__init__.pyi
+++ src/puyapy-stubs/__init__.pyi
@@ -1,7 +1,6 @@
 # ruff: noqa: F403
 # note: arc4 deliberately imported as module instead of re-exporting
 from puyapy import arc4 as arc4
-from puyapy._array import *
 from puyapy._constants import *
 from puyapy._contract import *
 from puyapy._gen import *
@@ -9,7 +8,6 @@ from puyapy._hints import *
 from puyapy._primitives import *
 from puyapy._reference import *
 from puyapy._storage import *
-from puyapy._struct import *
 from puyapy._transactions import *
 from puyapy._unsigned_builtins import *
 from puyapy._util import *
