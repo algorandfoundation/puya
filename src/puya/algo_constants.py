716834a3403c324805240dfd23f6312531fd9b9b
Tristan Menzel <tristanmenzel@gmail.com>
2024-01-29 16:47:05 -0800

    refactor: Move version info to algo constants file and specify 10 as the new default

--- src/puya/algo_constants.py
+++ src/puya/algo_constants.py
@@ -6,3 +6,11 @@ MAX_BIGUINT_BITS = 512
 MAX_BIGUINT_BYTES = MAX_BIGUINT_BITS // 8
 MAX_BYTES_LENGTH = 4096
 MAX_SCRATCH_SLOT_NUMBER = 255
+
+
+# Which language versions does this version of puya support targeting
+# This will typically just be the current mainnet version and potentially the vNext if it doesn't
+# contain breaking changes
+SUPPORTED_TEAL_LANGUAGE_VERSIONS = [10]
+# Which language version is currently deployed to mainnet
+MAINNET_TEAL_LANGUAGE_VERSION = 10
