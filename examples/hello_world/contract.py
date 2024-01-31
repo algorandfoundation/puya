2dfd1b45501af4d97478d56674d584b5670645a6
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-01-31 12:30:13 +0800

    docs: make hello_world ARC example consistent with Puya template example

--- examples/hello_world/contract.py
+++ examples/hello_world/contract.py
@@ -4,7 +4,7 @@ from puyapy import Contract, Transaction, log
 class HelloWorldContract(Contract):
     def approval_program(self) -> bool:
         name = Transaction.application_args(0)
-        log(b"Hello " + name)
+        log(b"Hello, " + name)
         return True
 
     def clear_state_program(self) -> bool:
