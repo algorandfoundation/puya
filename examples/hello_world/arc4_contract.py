f294371c9046371deebc07995f5eea751c860194
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-01-31 12:30:13 +0800

    docs: make hello_world ARC example consistent with Puya template example

--- examples/hello_world/arc4_contract.py
+++ examples/hello_world/arc4_contract.py
@@ -1,8 +1,11 @@
-from puyapy import ARC4Contract
-from puyapy.arc4 import String, abimethod
+from puyapy import ARC4Contract, arc4
 
 
+# Note: this contract is also used in the Puya AlgoKit template. So any breaking changes
+# that require fixing this contract should also be made there
+# https://github.com/algorandfoundation/algokit-puya-template/blob/main/template_content/pyproject.toml.jinja
+# https://github.com/algorandfoundation/algokit-puya-template/blob/main/template_content/.algokit/generators/create_contract/smart_contracts/%7B%25%20raw%20%25%7D%7B%7B%20contract_name%20%7D%7D%7B%25%20endraw%20%25%7D/contract.py.j2
 class HelloWorldContract(ARC4Contract):
-    @abimethod
-    def say_hello(self, name: String) -> String:
-        return "Hello " + name
+    @arc4.abimethod
+    def hello(self, name: arc4.String) -> arc4.String:
+        return "Hello, " + name
