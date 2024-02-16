a29fba9d859cf8f201efe4797309f5137cfbc6cd
Adam Chidlow <achidlow@users.noreply.github.com>
2024-02-16 18:25:43 +0800

    feat: improve coalescing by performing before sequentialisation, thus reducing chances of interference

--- src/puya/ir/destructure/main.py
+++ src/puya/ir/destructure/main.py
@@ -13,8 +13,8 @@ logger = structlog.get_logger(__name__)
 def destructure_ssa(context: CompileContext, contract_ir: models.Contract) -> models.Contract:
     contract_ir = convert_contract_to_cssa(context, contract_ir)
     contract_ir = remove_phi_nodes(context, contract_ir)
-    contract_ir = sequentialize_parallel_copies(context, contract_ir)
     contract_ir = coalesce_locals(context, contract_ir)
+    contract_ir = sequentialize_parallel_copies(context, contract_ir)
     if context.options.optimization_level > 0:
         contract_ir = post_ssa_optimizer(context, contract_ir)
     return contract_ir
