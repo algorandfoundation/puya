597b9393ffa792ec0ddb2a684b10734d5c5910e6
Adam Chidlow <achidlow@users.noreply.github.com>
2024-02-16 17:28:15 +0800

    fix: reduce number of iterations required in TEAL optimiser

--- src/puya/teal/optimize/main.py
+++ src/puya/teal/optimize/main.py
@@ -2,7 +2,10 @@ import copy
 
 from puya.context import CompileContext
 from puya.teal import models
-from puya.teal.optimize.constant_stack_shuffling import perform_constant_stack_shuffling
+from puya.teal.optimize.constant_stack_shuffling import (
+    constant_dupn_insertion,
+    perform_constant_stack_shuffling,
+)
 from puya.teal.optimize.peephole import peephole
 from puya.teal.optimize.repeated_rotations import simplify_repeated_rotation_ops
 from puya.teal.optimize.repeated_rotations_search import repeated_rotation_ops_search
@@ -12,8 +15,11 @@ def optimize_block(block: models.TealBlock, *, level: int) -> None:
     modified = True
     while modified:
         modified = perform_constant_stack_shuffling(block)
-        modified = modified or simplify_repeated_rotation_ops(block)
-        modified = modified or peephole(block)
+        modified = simplify_repeated_rotation_ops(block) or modified
+        modified = peephole(block) or modified
+
+    # don't do this one in a loop, only after everything else
+    constant_dupn_insertion(block)
 
     if level >= 2:
         block.ops = repeated_rotation_ops_search(block.ops)
