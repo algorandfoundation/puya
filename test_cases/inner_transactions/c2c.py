4b8a5c8a7f96224e839b91dd9f7dca583fcfde38
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-02-16 16:15:32 +0800

    test: add test case for contract to contract call

---
+++ test_cases/inner_transactions/c2c.py
@@ -0,0 +1,31 @@
+from puyapy import Application, ARC4Contract, Bytes, UInt64, arc4, itxn, log
+
+from test_cases.inner_transactions.programs import HELLO_WORLD_APPROVAL_HEX, HELLO_WORLD_CLEAR
+
+
+class Greeter(ARC4Contract):
+    def __init__(self) -> None:
+        self.hello_app = Application(0)
+
+    @arc4.abimethod()
+    def bootstrap(self) -> UInt64:
+        assert not self.hello_app, "already bootstrapped"
+        self.hello_app = (
+            itxn.ApplicationCallTransactionParams(
+                approval_program=Bytes.from_hex(HELLO_WORLD_APPROVAL_HEX),
+                clear_state_program=HELLO_WORLD_CLEAR,
+                fee=0,
+            )
+            .submit()
+            .created_application
+        )
+        return self.hello_app.application_id
+
+    @arc4.abimethod()
+    def log_greetings(self, name: arc4.String) -> None:
+        hello_call = itxn.ApplicationCallTransactionParams(
+            application_id=self.hello_app.application_id,
+            application_args=(arc4.arc4_signature("hello(string)string"), name),
+        ).submit()
+        greeting = arc4.String.from_log(hello_call.last_log)
+        log(b"HelloWorld returned: ", greeting.decode())
