07662a01d9f62ec4861937f705671706950ae811
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-03-26 17:07:36 +0800

    review feedback

--- stubs/algopy.py
+++ stubs/algopy.py
@@ -4,7 +4,7 @@ if not typing.TYPE_CHECKING:
     raise RuntimeError(
         "`import algopy` cannot be executed by the python interpreter. "
         "It is intended to be used by the puyapy compiler to compile "
-        "Algorand Smart Contracts written in Python to TEAL. "
+        "Algorand smart contracts written in Algorand Python."
         "For more details see "
         "https://algorandfoundation.github.io/puya/index.html#compiler-usage"
     )
