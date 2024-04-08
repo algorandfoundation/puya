7c96a8c8dee6561f3f79e4227d7481eb08bc2dcb
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-04-08 12:41:38 +0800

    test: add additional test coverage for arc4 .copy check, and fix issue found

--- src/puya/awst_build/validation/arc4_copy.py
+++ src/puya/awst_build/validation/arc4_copy.py
@@ -18,74 +18,13 @@ class ARC4CopyValidator(AWSTTraverser):
             module_statement.accept(validator)
 
     def visit_assignment_statement(self, statement: awst_nodes.AssignmentStatement) -> None:
-        self._check_for_arc4_tuple_destructure(statement.target, statement.value)
+        _check_assignment(statement.target, statement.value)
         statement.value.accept(self)
-        self._check_for_arc4_copy(statement.value, "being assigned to another variable")
 
     def visit_tuple_expression(self, expr: awst_nodes.TupleExpression) -> None:
         super().visit_tuple_expression(expr)
         for item in expr.items:
-            self._check_for_arc4_copy(item, "being passed to a tuple expression")
-
-    @staticmethod
-    def _is_referable_expression(expr: awst_nodes.Expression) -> bool:
-        """
-        Returns True if expr represents something that can be referenced multiple times.
-        """
-        match expr:
-            case (
-                awst_nodes.VarExpression()
-                | awst_nodes.AppStateExpression()
-                | awst_nodes.AppAccountStateExpression()
-            ):
-                return True
-            case (
-                awst_nodes.IndexExpression(base=base_expr)
-                | awst_nodes.TupleItemExpression(base=base_expr)
-            ):
-                return ARC4CopyValidator._is_referable_expression(base_expr)
-        return False
-
-    @staticmethod
-    def _is_arc4_mutable(wtype: wtypes.WType) -> bool:
-        """
-        Returns True if expr represents an arc4 type that is mutable
-        """
-        match wtype:
-            case wtypes.ARC4Type(immutable=False):
-                return True
-        return False
-
-    def _check_for_arc4_tuple_destructure(
-        self, target: awst_nodes.Expression, value: awst_nodes.Expression
-    ) -> None:
-        if not isinstance(target, awst_nodes.TupleExpression):
-            return
-        match value.wtype:
-            case wtypes.WTuple(types=item_types):
-                if self._is_referable_expression(value):
-                    problem_type = next((i for i in item_types if self._is_arc4_mutable(i)), None)
-                    if problem_type:
-                        logger.error(
-                            f"Tuple cannot be destructured as it contains an item of type"
-                            f" {problem_type} which requires copying. Use index access instead",
-                            location=value.source_location,
-                        )
-
-    def _check_for_arc4_copy(self, expr: awst_nodes.Expression, context_desc: str) -> None:
-        if self._is_arc4_mutable(expr.wtype) and self._is_referable_expression(expr):
-            logger.error(
-                f"{expr.wtype} must be copied using .copy() when {context_desc}",
-                location=expr.source_location,
-            )
-
-    @staticmethod
-    def _expand_tuple_items(expr: awst_nodes.Expression) -> Iterator[awst_nodes.Expression]:
-        match expr:
-            case awst_nodes.TupleExpression(items=items):
-                yield from items
-            case _:
-                yield expr
+            _check_for_arc4_copy(item, "being passed to a tuple expression")
 
     def visit_for_in_loop(self, statement: awst_nodes.ForInLoop) -> None:
         if not isinstance(statement.sequence, awst_nodes.Range):
@@ -93,10 +32,8 @@ class ARC4CopyValidator(AWSTTraverser):
         statement.loop_body.accept(self)
 
     def visit_assignment_expression(self, expr: awst_nodes.AssignmentExpression) -> None:
-        self._check_for_arc4_tuple_destructure(expr.target, expr.value)
-
+        _check_assignment(expr.target, expr.value)
         expr.value.accept(self)
-        self._check_for_arc4_copy(expr.value, "being assigned to another variable")
 
     def visit_subroutine_call_expression(self, expr: awst_nodes.SubroutineCallExpression) -> None:
         super().visit_subroutine_call_expression(expr)
@@ -110,15 +47,78 @@ class ARC4CopyValidator(AWSTTraverser):
                     message = "being passed to a subroutine from state"
                 case _:
                     message = "being passed to a subroutine"
-            self._check_for_arc4_copy(arg.value, message)
+            _check_for_arc4_copy(arg.value, message)
 
     def visit_arc4_array_encode(self, expr: awst_nodes.ARC4ArrayEncode) -> None:
         super().visit_arc4_array_encode(expr)
         for v in expr.values:
-            self._check_for_arc4_copy(v, "being passed to an array constructor")
+            _check_for_arc4_copy(v, "being passed to an array constructor")
 
     def visit_arc4_encode(self, expr: awst_nodes.ARC4Encode) -> None:
         super().visit_arc4_encode(expr)
 
-        for item in self._expand_tuple_items(expr.value):
-            self._check_for_arc4_copy(item, "being passed to a constructor")
+        for item in _expand_tuple_items(expr.value):
+            _check_for_arc4_copy(item, "being passed to a constructor")
+
+
+def _is_referable_expression(expr: awst_nodes.Expression) -> bool:
+    """
+    Returns True if expr represents something that can be referenced multiple times.
+    """
+    match expr:
+        case (
+            awst_nodes.VarExpression()
+            | awst_nodes.AppStateExpression()
+            | awst_nodes.AppAccountStateExpression()
+        ):
+            return True
+        case (
+            awst_nodes.IndexExpression(base=base_expr)
+            | awst_nodes.TupleItemExpression(base=base_expr)
+        ):
+            return _is_referable_expression(base_expr)
+    return False
+
+
+def _check_assignment(target: awst_nodes.Expression, value: awst_nodes.Expression) -> None:
+    if not isinstance(target, awst_nodes.TupleExpression):
+        _check_for_arc4_copy(value, "being assigned to another variable")
+    else:
+        match value.wtype:
+            case wtypes.WTuple(types=item_types):
+                if _is_referable_expression(value):
+                    problem_type = next((i for i in item_types if _is_arc4_mutable(i)), None)
+                    if problem_type:
+                        logger.error(
+                            f"Tuple cannot be destructured as it contains an item of type"
+                            f" {problem_type} which requires copying. Use index access instead",
+                            location=value.source_location,
+                        )
+
+
+def _check_for_arc4_copy(expr: awst_nodes.Expression, context_desc: str) -> None:
+    if _is_arc4_mutable(expr.wtype) and _is_referable_expression(expr):
+        logger.error(
+            f"{expr.wtype} must be copied using .copy() when {context_desc}",
+            location=expr.source_location,
+        )
+
+
+def _expand_tuple_items(expr: awst_nodes.Expression) -> Iterator[awst_nodes.Expression]:
+    match expr:
+        case awst_nodes.TupleExpression(items=items):
+            yield from items
+        case _:
+            yield expr
+
+
+def _is_arc4_mutable(wtype: wtypes.WType) -> bool:
+    """
+    Returns True if expr represents an arc4 type that is mutable
+    """
+    match wtype:
+        case wtypes.ARC4Type(immutable=False):
+            return True
+        case wtypes.WTuple(types=types):
+            return any(_is_arc4_mutable(t) for t in types)
+    return False
