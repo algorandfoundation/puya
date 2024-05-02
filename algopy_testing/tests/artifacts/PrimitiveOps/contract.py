aadfa580f086884a419f643e2f9e9605b5f87a07
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-05-02 18:27:23 +0800

    test: assign op results before returning so errors can be verified to occur at the correct point

--- algopy_testing/tests/artifacts/PrimitiveOps/contract.py
+++ algopy_testing/tests/artifacts/PrimitiveOps/contract.py
@@ -4,52 +4,65 @@ from algopy import ARC4Contract, Bytes, UInt64, arc4, op
 class PrimitiveOpsContract(ARC4Contract):
     @arc4.abimethod()
     def verify_uint64_init(self, raw_value: Bytes) -> UInt64:
-        return op.btoi(raw_value)
+        result = op.btoi(raw_value)
+        return result
 
     @arc4.abimethod()
     def verify_uint64_add(self, a: UInt64, b: UInt64) -> UInt64:
-        return a + b
+        result = a + b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_sub(self, a: UInt64, b: UInt64) -> UInt64:
-        return a - b
+        result = a - b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_mul(self, a: UInt64, b: UInt64) -> UInt64:
-        return a * b
+        result = a * b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_div(self, a: UInt64, b: UInt64) -> UInt64:
-        return a // b
+        result = a // b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_mod(self, a: UInt64, b: UInt64) -> UInt64:
-        return a % b
+        result = a % b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_and(self, a: UInt64, b: UInt64) -> UInt64:
-        return a & b
+        result = a & b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_or(self, a: UInt64, b: UInt64) -> UInt64:
-        return a | b
+        result = a | b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_xor(self, a: UInt64, b: UInt64) -> UInt64:
-        return a ^ b
+        result = a ^ b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_not(self, a: UInt64) -> UInt64:
-        return ~a
+        result = ~a
+        return result
 
     @arc4.abimethod()
     def verify_uint64_lshift(self, a: UInt64, b: UInt64) -> UInt64:
-        return a << b
+        result = a << b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_rshift(self, a: UInt64, b: UInt64) -> UInt64:
-        return a >> b
+        result = a >> b
+        return result
 
     @arc4.abimethod()
     def verify_uint64_pow(self, a: UInt64, b: UInt64) -> UInt64:
-        return a**b
+        result = a**b
+        return result
