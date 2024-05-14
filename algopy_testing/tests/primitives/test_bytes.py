84c9cf38e63f65041ad12d18afa5ab5edf61fc06
Bobby Lat <bobby.lat@makerx.com.au>
2024-05-14 11:50:38 +0800

    pad params to the given size to test bytes size limit and return hash of the result to bypass the log op limit

--- algopy_testing/tests/primitives/test_bytes.py
+++ algopy_testing/tests/primitives/test_bytes.py
@@ -1,3 +1,6 @@
+from hashlib import sha256
+
+import algokit_utils
 import pytest
 from algopy._constants import MAX_BYTES_SIZE
 from algopy.primitives.bytes import Bytes
@@ -19,24 +22,62 @@ def test_bytes_bool(value: bytes) -> None:
 
 
 @pytest.mark.parametrize(
-    ("a", "b"),
+    ("a", "b", "pad_a_size", "pad_b_size"),
     [
-        (b"", b""),
-        (b"1", b""),
-        (b"", b"1"),
-        (b"1", b"1"),
-        # throws "application args total length too long, max len 2048 bytes" error
-        # (b"", b"0" * MAX_BYTES_SIZE),
-        # (b"0" * MAX_BYTES_SIZE, b""),
-        # (b"1", b"0" * (MAX_BYTES_SIZE - 1)),
-        # (b"0" * (MAX_BYTES_SIZE - 1), 1),
+        (b"", b"", 0, 0),
+        (b"1", b"", 0, 0),
+        (b"", b"1", 0, 0),
+        (b"1", b"1", 0, 0),
+        (b"", b"0", 0, MAX_BYTES_SIZE - 1),
+        (b"0", b"", MAX_BYTES_SIZE - 1, 0),
+        (b"1", b"0", 0, MAX_BYTES_SIZE - 2),
+        (b"1", b"0", MAX_BYTES_SIZE - 2, 0),
     ],
 )
-def test_bytes_addition(get_avm_result: AVMInvoker, a: bytes, b: bytes) -> None:
-    avm_result = get_avm_result("verify_bytes_add", a=a, b=b)
-    assert avm_result == Bytes(a) + Bytes(b)
-    assert avm_result == Bytes(a) + b
-    assert avm_result == a + Bytes(b)
+def test_bytes_addition(
+    get_avm_result: AVMInvoker, a: bytes, b: bytes, pad_a_size: int, pad_b_size: int
+) -> None:
+    avm_result = get_avm_result(
+        "verify_bytes_add", a=a, b=b, pad_a_size=pad_a_size, pad_b_size=pad_b_size
+    )
+
+    a = (b"\x00" * pad_a_size) + a
+    b = (b"\x00" * pad_b_size) + b
+    assert avm_result == get_sha256_hash(Bytes(a) + Bytes(b))
+    assert avm_result == get_sha256_hash(Bytes(a) + b)
+    assert avm_result == get_sha256_hash(a + Bytes(b))
     i = Bytes(a)
     i += b
-    assert avm_result == i
+    assert avm_result == get_sha256_hash(i)
+
+
+@pytest.mark.parametrize(
+    ("a", "b", "pad_a_size", "pad_b_size"),
+    [
+        (b"", b"", 1, MAX_BYTES_SIZE),
+        (b"1", b"", 0, MAX_BYTES_SIZE),
+        (b"", b"", MAX_BYTES_SIZE, MAX_BYTES_SIZE),
+    ],
+)
+def test_bytes_addition_overflow(
+    get_avm_result: AVMInvoker, a: bytes, b: bytes, pad_a_size: int, pad_b_size: int
+) -> None:
+    with pytest.raises(
+        algokit_utils.LogicError, match="concat produced a too big \\(\\d+\\) byte-array"
+    ):
+        get_avm_result("verify_bytes_add", a=a, b=b, pad_a_size=pad_a_size, pad_b_size=pad_b_size)
+
+    a = (b"\x00" * pad_a_size) + a
+    b = (b"\x00" * pad_b_size) + b
+    with pytest.raises(OverflowError):
+        Bytes(a) + Bytes(b)
+
+    with pytest.raises(OverflowError):
+        Bytes(a) + b
+
+    with pytest.raises(OverflowError):
+        a + Bytes(b)
+
+
+def get_sha256_hash(v: Bytes) -> Bytes:
+    return Bytes(sha256(v.value).digest())
