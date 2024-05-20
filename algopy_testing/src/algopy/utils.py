796ed04a757f18dbda3595b3f6296365119b472d
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-05-20 13:42:14 +0800

    refactor: rebase and fix conflicts and move circular dependency resolution into utility methods

--- algopy_testing/src/algopy/utils.py
+++ algopy_testing/src/algopy/utils.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from algopy_testing.constants import MAX_BYTES_SIZE
-
-from algopy import BigUInt, Bytes, UInt64
+from algopy_testing.constants import MAX_BYTES_SIZE, MAX_UINT64, MAX_UINT512
 
 
 def as_int(value: object, *, max: int | None) -> int:  # noqa: A002
@@ -13,6 +11,9 @@ def as_int(value: object, *, max: int | None) -> int:  # noqa: A002
         TypeError: If `value` is not a numeric type
         ValueError: If not 0 <= `value` <= max
     """
+
+    from algopy import BigUInt, UInt64
+
     match value:
         case int(int_value):
             pass
@@ -30,6 +31,14 @@ def as_int(value: object, *, max: int | None) -> int:  # noqa: A002
     return int_value
 
 
+def as_int64(value: object) -> int:
+    return as_int(value, max=MAX_UINT64)
+
+
+def as_int512(value: object) -> int:
+    return as_int(value, max=MAX_UINT512)
+
+
 def as_bytes(value: object, *, max_size: int = MAX_BYTES_SIZE) -> bytes:
     """
     Returns the underlying bytes value for bytes or Bytes type up to 4096
@@ -38,6 +47,8 @@ def as_bytes(value: object, *, max_size: int = MAX_BYTES_SIZE) -> bytes:
         TypeError: If `value` is not a bytes type
         ValueError: If not 0 <= `len(value)` <= max_size
     """
+    from algopy import Bytes
+
     match value:
         case bytes(bytes_value):
             pass
