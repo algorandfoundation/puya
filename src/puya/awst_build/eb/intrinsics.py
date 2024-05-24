0ed22a1f5b2175249d5c5c894746efb69c7c2b81
Adam Chidlow <achidlow@users.noreply.github.com>
2024-05-24 14:26:57 +0800

    remove stub_name from WType (WIP) - test/fixes

--- src/puya/awst_build/eb/intrinsics.py
+++ src/puya/awst_build/eb/intrinsics.py
@@ -15,7 +15,6 @@ from puya.awst_build.eb.var_factory import builder_for_instance
 from puya.awst_build.intrinsic_models import FunctionOpMapping, PropertyOpMapping
 from puya.awst_build.utils import convert_literal, get_arg_mapping
 from puya.errors import CodeError
-from puya.utils import StableSet
 
 if typing.TYPE_CHECKING:
     from collections.abc import Mapping, Sequence
@@ -137,11 +136,8 @@ class IntrinsicFunctionExpressionBuilder(FunctionBuilder):
         primary_mapping = self._mappings[0]  # TODO: remove this assumption
         func_arg_names = (*primary_mapping.literal_arg_names, *primary_mapping.stack_inputs.keys())
 
-        resolved_args: list[Expression | Literal] = [
-            a.rvalue() if isinstance(a, ExpressionBuilder) else a for a in args
-        ]
         arg_mapping = get_arg_mapping(
-            func_arg_names, args=zip(arg_names, resolved_args, strict=False), location=location
+            func_arg_names, args=zip(arg_names, args, strict=False), location=location
         )
         intrinsic_expr = _map_call(
             self._mappings, callee=self._fullname, node_location=location, args=arg_mapping
@@ -150,7 +146,7 @@ class IntrinsicFunctionExpressionBuilder(FunctionBuilder):
 
 
 def _best_op_mapping(
-    op_mappings: Sequence[FunctionOpMapping], args: dict[str, Expression | Literal]
+    op_mappings: Sequence[FunctionOpMapping], args: dict[str, ExpressionBuilder | Literal]
 ) -> FunctionOpMapping:
     """Find op mapping that matches as many arguments to immediate args as possible"""
     literal_arg_names = {arg_name for arg_name, arg in args.items() if isinstance(arg, Literal)}
@@ -165,7 +161,7 @@ def _map_call(
     ast_mapper: Sequence[FunctionOpMapping],
     callee: str,
     node_location: SourceLocation,
-    args: dict[str, Expression | Literal],
+    args: dict[str, ExpressionBuilder | Literal],
 ) -> ExpressionBuilder:
     if len(ast_mapper) == 1:
         (op_mapping,) = ast_mapper
@@ -197,26 +193,23 @@ def _map_call(
 
     stack_args = list[Expression]()
     for arg_name, allowed_pytypes in op_mapping.stack_inputs.items():
-        allowed_types = StableSet.from_iter(  # TODO: use PyTypes instead
-            pt.wtype for pt in allowed_pytypes
-        )
         arg_in = args.pop(arg_name, None)
         if arg_in is None:
             logger.error(f"Missing expected argument {arg_name}", location=node_location)
-        elif isinstance(arg_in, Expression):
-            # TODO this is identity based, match types instead?
-            if arg_in.wtype not in allowed_types:
+        elif isinstance(arg_in, ExpressionBuilder):
+            if arg_in.pytype not in allowed_pytypes:
                 logger.error(
-                    f'Invalid argument type "{arg_in.wtype}"'
+                    f'Invalid argument type "{arg_in.pytype}"'
                     f' for argument "{arg_name}" when calling {callee}',
                     location=arg_in.source_location,
                 )
-            stack_args.append(arg_in)
+            stack_args.append(arg_in.rvalue())
         else:
             literal_value = arg_in.value
-            for allowed_type in allowed_types:
-                if allowed_type.is_valid_literal(literal_value):
-                    literal_expr = convert_literal(arg_in, allowed_type)
+            for allowed_type in allowed_pytypes:
+                allowed_wtype = allowed_type.wtype  # TODO yeet me
+                if allowed_wtype.is_valid_literal(literal_value):
+                    literal_expr = convert_literal(arg_in, allowed_wtype)
                     stack_args.append(literal_expr)
                     break
             else:
