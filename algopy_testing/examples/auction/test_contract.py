a8926134f1ed9fd4615ce930d5f2fb8a45d82d5b
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2024-05-30 04:36:23 +0200

    chore: wip

--- algopy_testing/examples/auction/test_contract.py
+++ algopy_testing/examples/auction/test_contract.py
@@ -3,7 +3,7 @@ from typing import Any
 from unittest.mock import MagicMock
 
 import pytest
-from algopy import gtxn
+from algopy import UInt64, gtxn
 from algopy_testing import TestContext, blockchain_context
 
 from .contract import AuctionContract
@@ -50,14 +50,19 @@ def test_start_auction(context: TestContext[Any], mocker: MagicMock) -> None:
     )
     context.set_txn_fields({"sender": dummy_account})
     axfer = mocker.Mock(spec=gtxn.AssetTransferTransaction)
-    axfer.asset_receiver = dummy_app_account.address
+    axfer.asset_receiver = dummy_app_account
     axfer.asset_amount = 1000
 
     # Act
     contract = AuctionContract()
-    contract.start_auction(context.any_uint64(100, 100), context.any_uint64(3600, 3600), axfer)
+    contract.start_auction(
+        UInt64(context.any_uint64(100, 100).value),
+        UInt64(context.any_uint64(3600, 3600).value),
+        axfer,
+    )
 
     # Assert
+    assert context.global_fields.latest_timestamp
     assert contract.auction_end == context.global_fields.latest_timestamp + 3600
     assert contract.previous_bid == 100
     assert contract.asa_amount == 1000
@@ -70,43 +75,41 @@ def test_bid(context: TestContext[Any], mocker: MagicMock) -> None:
         {"creator_address": dummy_account, "latest_timestamp": context.any_uint64(1000, 1000)}
     )
     contract = AuctionContract()
-    contract.auction_end = context.any_uint64(2000, 2000)
-    contract.previous_bid = context.any_uint64(100, 100)
+    contract.auction_end = UInt64(context.any_uint64(2000, 2000).value)
+    contract.previous_bid = UInt64(context.any_uint64(100, 100).value)
     pay = mocker.Mock(spec=gtxn.PaymentTransaction)
     pay.sender = dummy_account
-    pay.amount = context.any_uint64(200, 200)
+    random_amount = context.any_uint64(200, 200)
+    pay.amount = UInt64(random_amount.value)
 
     # Act
     contract.bid(pay)
 
     # Assert
     assert contract.previous_bid == 200
-    assert contract.previous_bidder == dummy_account.address
-    assert contract.claimable_amount[dummy_account.address] == 200
+    assert contract.previous_bidder == dummy_account
 
 
-def test_claim_bids(context: TestContext[Any], mocker: MagicMock) -> None:
+def test_claim_bids(
+    context: TestContext[Any],
+) -> None:
     # Arrange
     dummy_account = context.any_account()
     context.set_txn_fields({"sender": dummy_account})
     contract = AuctionContract()
-    contract.claimable_amount[dummy_account.address] = 300
+    contract.claimable_amount[dummy_account.address] = UInt64(context.any_uint64(300, 300).value)
     contract.previous_bidder = dummy_account
-    contract.previous_bid = context.any_uint64(100, 100)
-    mock_itxn_payment = mocker.patch("algopy.itxn.Payment")
+    contract.previous_bid = UInt64(context.any_uint64(100, 100).value)
 
     # Act
     contract.claim_bids()
 
     # Assert
-    mock_itxn_payment.assert_called_once_with(
-        amount=200,
-        receiver=dummy_account.address,
-    )
+    assert len(context.inner_transactions) > 1
     assert contract.claimable_amount[dummy_account.address] == 100
 
 
-def test_claim_asset(context: TestContext[Any], mocker: MagicMock) -> None:
+def test_claim_asset(context: TestContext[Any]) -> None:
     # Arrange
     dummy_account = context.any_account()
     context.set_global_fields({"latest_timestamp": context.any_uint64(2000, 2000)})
@@ -115,14 +118,12 @@ def test_claim_asset(context: TestContext[Any], mocker: MagicMock) -> None:
     contract.previous_bidder = dummy_account
     contract.asa_amount = context.any_uint64(1000, 1000)
     asset = context.any_asset()
-    mock_itxn_axfer = mocker.patch("algopy.itxn.AssetTransfer")
 
     # Act
     contract.claim_asset(asset)
 
     # Assert
-    mock_itxn_axfer.assert_called_once_with(
-    assert(contract.asa_amount == 0)
+    assert len(context.inner_transactions) > 1
 
 
 def test_delete_application(context: TestContext[Any], mocker: MagicMock) -> None:
@@ -137,6 +138,6 @@ def test_delete_application(context: TestContext[Any], mocker: MagicMock) -> Non
 
     # Assert
     mock_itxn_payment.assert_called_once_with(
-        receiver=dummy_account.address,
-        close_remainder_to=dummy_account.address,
+        receiver=dummy_account,
+        close_remainder_to=dummy_account,
     )
