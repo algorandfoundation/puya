ec180075cd108b3690fc512fbbc6f5c9653ad8bb
Adam Chidlow <achidlow@users.noreply.github.com>
2024-06-17 15:45:23 +0800

    fix: fix semantic issues with tuple comparisons of different length / types

--- test_cases/tuple_support/tuple_comparisons.py
+++ test_cases/tuple_support/tuple_comparisons.py
@@ -4,6 +4,7 @@ from algopy import Contract, String, UInt64, log, subroutine
 class TupleComparisons(Contract):
     def approval_program(self) -> bool:
         test_tuple_cmp_eval()
+        test_tuple_cmp_empty()
         return True
 
     def clear_state_program(self) -> bool:
@@ -18,8 +19,37 @@ def test_tuple_cmp_eval() -> None:
     ) != (
         String("abc"),
     )  # type: ignore[comparison-overlap]
-    assert (UInt64(1), UInt64(2)) != (UInt64(3), log_and_return(UInt64(43)))
-    tmp = (UInt64(1), UInt64(2)) == (UInt64(3), log_and_return(UInt64(44)))
+    tmp = (
+        String("abc"),
+        log_and_return(UInt64(43)),
+    ) == (
+        String("abc"),
+    )  # type: ignore[comparison-overlap]
+    assert not tmp
+
+    assert (String("abc"),) != (
+        String("abc"),
+        log_and_return(UInt64(44)),
+    )  # type: ignore[comparison-overlap]
+    tmp = (String("abc"),) == (
+        String("abc"),
+        log_and_return(UInt64(45)),
+    )  # type: ignore[comparison-overlap]
+    assert not tmp
+
+    assert (UInt64(1), UInt64(2)) != (UInt64(3), log_and_return(UInt64(46)))
+    tmp = (UInt64(1), UInt64(2)) == (UInt64(3), log_and_return(UInt64(47)))
+    assert not tmp
+
+
+@subroutine
+def test_tuple_cmp_empty() -> None:
+    assert () == ()
+    tmp = () != ()
+    assert not tmp
+
+    assert () != ("a",)  # type: ignore[comparison-overlap]
+    tmp = () == ("a",)  # type: ignore[comparison-overlap]
     assert not tmp
 
 
