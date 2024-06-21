31714d8eb377b1e45021feecfd9815171e1f0d6e
Bobby Lat <bobby.lat@makerx.com.au>
2024-06-21 10:37:26 +0800

    add stub implementation for urange

--- algopy_testing/src/algopy_testing/models/unsigned_builtins.py
+++ algopy_testing/src/algopy_testing/models/unsigned_builtins.py
@@ -2,13 +2,29 @@ from __future__ import annotations
 
 import typing
 
+from algopy_testing.utils import as_int64
+
 if typing.TYPE_CHECKING:
+    from collections.abc import Iterator
+
     import algopy
 
 
 class urange:  # noqa: N801
+    _value: range
+
     def __init__(self, *args: int | algopy.UInt64) -> None:
-        pass
+        self._value = range(*[as_int64(arg) for arg in args])
+
+    def __iter__(self) -> Iterator[algopy.UInt64]:
+        import algopy
+
+        return map(algopy.UInt64, self._value)
+
+    def __reversed__(self) -> Iterator[algopy.UInt64]:
+        import algopy
+
+        return map(algopy.UInt64, reversed(self._value))
 
 
 def uenumerate(_iterable: typing.Any) -> typing.Any:
