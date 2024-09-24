17778b859d4e0b0f3dbc329afbd64f8908470d5d
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-09-24 10:23:22 +0800

    fix: prevent error that occurs when removing a series of redundant Load and Store ops

---
+++ test_cases/bug_load_store_load_store/contract.py
@@ -0,0 +1,21 @@
+from algopy import Contract, UInt64, subroutine, urange
+
+
+@subroutine
+def get_bool() -> bool:
+    return True
+
+
+class MyContract(Contract):
+    def approval_program(self) -> UInt64:
+        val = UInt64(0)
+        for _idx in urange(2):
+            if get_bool():
+                pass
+            elif get_bool():  # noqa: SIM102
+                if not get_bool():
+                    val += UInt64(123)
+        return val
+
+    def clear_state_program(self) -> bool:
+        return True
