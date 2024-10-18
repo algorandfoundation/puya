04fe6d798e58939fd07028f07d15b2e25b9427e4
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-10-18 14:23:25 +0800

    fix: itxn arguments are now inferred correctly when using only a method name in `algopy.arc4.abi_call`

--- test_cases/typed_abi_call_txn/out/client_Caller.py
+++ test_cases/typed_abi_call_txn/out/client_Caller.py
@@ -24,6 +24,14 @@ class Caller(algopy.arc4.ARC4Client, typing.Protocol):
     ) -> None: ...
 
     @algopy.arc4.abimethod
+    def test_call_with_infer(
+        self,
+        a: algopy.arc4.DynamicBytes,
+        b: algopy.arc4.DynamicBytes,
+        app: algopy.Application,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
     def test_call_with_acfg_no_return(
         self,
         a: algopy.arc4.DynamicBytes,
