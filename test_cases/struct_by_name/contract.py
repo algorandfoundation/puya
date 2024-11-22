b48a7d4b976c3471a287a735bd4c6db443856e41
Tristan Menzel <tristanmenzel@gmail.com>
2024-11-22 11:07:01 -0800

    fix: Identify WTuples by name when treating them as structs in an ABI method

---
+++ test_cases/struct_by_name/contract.py
@@ -0,0 +1,39 @@
+import typing
+
+from algopy import ARC4Contract, arc4
+
+
+class StructOne(typing.NamedTuple):
+    x: arc4.UInt8
+    y: arc4.UInt8
+
+
+class StructTwo(typing.NamedTuple):
+    x: arc4.UInt8
+    y: arc4.UInt8
+
+
+class DemoContract(ARC4Contract):
+    """
+    Verify that even though named tuples with different names but the same structure should be
+    considered 'comparable' in the type system, they should be output separately when being
+    interpreted as an arc4 Struct in an abi method
+    """
+
+    @arc4.abimethod()
+    def get_one(self) -> StructOne:
+        return StructOne(
+            x=arc4.UInt8(1),
+            y=arc4.UInt8(1),
+        )
+
+    @arc4.abimethod()
+    def get_two(self) -> StructTwo:
+        return StructTwo(
+            x=arc4.UInt8(1),
+            y=arc4.UInt8(1),
+        )
+
+    @arc4.abimethod()
+    def compare(self) -> bool:
+        return self.get_one() == self.get_two()
