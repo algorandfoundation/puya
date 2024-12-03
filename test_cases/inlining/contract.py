a1bae7ad15b59ff9a0d46b612ffa9ca79750c614
Adam Chidlow <achidlow@users.noreply.github.com>
2024-12-03 13:53:21 +0800

    fix recursive inlining (tests)

--- test_cases/inlining/contract.py
+++ test_cases/inlining/contract.py
@@ -1,10 +1,14 @@
 import typing
 
-from algopy import Contract, op, subroutine
+from algopy import Contract, UInt64, op, subroutine
 
 
 class MyContract(Contract):
     def approval_program(self) -> bool:
+        z = zero()
+        a = one()
+        b = one()
+        assert z + a + b == 2
         never_returns()
         return True
 
@@ -15,3 +19,13 @@ class MyContract(Contract):
 @subroutine(inline=True)
 def never_returns() -> typing.Never:
     op.err()
+
+
+@subroutine(inline=True)
+def one() -> UInt64:
+    return zero() + 1
+
+
+@subroutine(inline=True)
+def zero() -> UInt64:
+    return UInt64(0)
