f1833b06378893fa7fa1ad42daade2eaa37d28dd
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-02-04 11:11:39 +0800

    chore: compile all

--- test_cases/array/out/client_ImmutableArrayContract.py
+++ test_cases/array/out/client_ImmutableArrayContract.py
@@ -42,3 +42,32 @@ class ImmutableArrayContract(algopy.arc4.ARC4Client, typing.Protocol):
     def test_bit_packed_tuples(
         self,
     ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def test_args(
+        self,
+        arr1: algopy.arc4.DynamicArray[algopy.arc4.UIntN[typing.Literal[64]]],
+        arr2: algopy.arc4.DynamicArray[algopy.arc4.Bool],
+        arr3: algopy.arc4.DynamicArray[algopy.arc4.Tuple[algopy.arc4.UIntN[typing.Literal[64]], algopy.arc4.Bool, algopy.arc4.Bool]],
+        arr4: algopy.arc4.DynamicArray[algopy.arc4.Tuple[algopy.arc4.UIntN[typing.Literal[64]], algopy.arc4.String]],
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def test_uint64_return(
+        self,
+    ) -> algopy.arc4.DynamicArray[algopy.arc4.UIntN[typing.Literal[64]]]: ...
+
+    @algopy.arc4.abimethod
+    def test_bool_return(
+        self,
+    ) -> algopy.arc4.DynamicArray[algopy.arc4.Bool]: ...
+
+    @algopy.arc4.abimethod
+    def test_tuple_return(
+        self,
+    ) -> algopy.arc4.DynamicArray[algopy.arc4.Tuple[algopy.arc4.UIntN[typing.Literal[64]], algopy.arc4.Bool, algopy.arc4.Bool]]: ...
+
+    @algopy.arc4.abimethod
+    def test_dynamic_tuple_return(
+        self,
+    ) -> algopy.arc4.DynamicArray[algopy.arc4.Tuple[algopy.arc4.UIntN[typing.Literal[64]], algopy.arc4.String]]: ...
