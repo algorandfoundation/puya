63b767c1d12ed92c42c46d737c2d545f5d966d2e
Tristan Menzel <tristanmenzel@gmail.com>
2025-02-13 15:10:35 -0800

    chore: Add script for generating TS classes for nodes

---
+++ tests/test_ts_code_gen.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from scripts.generate_ts_nodes import generate_file
+from tests.utils.git import check_for_diff
+
+VCS_ROOT = Path(__file__).parent.parent
+
+
+def test_ts_code_gen() -> None:
+    generate_file(out_path=VCS_ROOT / "tests/output/nodes.ts.txt", puya_path=VCS_ROOT / "src/puya")
+    diff = check_for_diff(VCS_ROOT / "tests/output", VCS_ROOT)
+    assert not diff, f"Uncommitted changes were found:\n{diff}"
