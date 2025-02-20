fd0768c8e5c23b22828eb1799384211099d26663
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-02-20 11:16:35 +0800

    support supplying prefix via lsp initialize

--- src/puyapy/options.py
+++ src/puyapy/options.py
@@ -17,3 +17,4 @@ class PuyaPyOptions(PuyaOptions):
     output_client: bool = False
     out_dir: Path | None = attrs.field(default=None, repr=False)
     log_level: LogLevel = LogLevel.info
+    prefix: Path | None = None
