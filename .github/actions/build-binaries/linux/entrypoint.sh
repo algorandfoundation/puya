d3554f5af61f9f978bb728698252b565da2174a0
Adam Chidlow <achidlow@users.noreply.github.com>
2025-03-10 11:55:08 +0800

    simplify binary building pipeline

--- .github/actions/build-binaries/linux/entrypoint.sh
+++ .github/actions/build-binaries/linux/entrypoint.sh
@@ -11,8 +11,17 @@ echo "Artifacts will be saved to $ARTIFACTS_DIR"
 # Install project dependencies
 cd ${GITHUB_WORKSPACE}
 poetry config virtualenvs.create false
-poetry install --no-interaction
+poetry install --no-interaction --without=dev --with=cicd
 
 # Build the binary
-poetry run poe package_unix
-echo "Build completed successfully for $ARCH architecture" 
+poetry run pyinstaller \
+    --clean \
+    --onedir \
+    --name puya \
+    --noconfirm src/puya/__main__.py \
+    --add-data './src/puya/ir/_puya_lib.awst.json:puya/ir/' \
+    --exclude-module colorama \
+    --exclude-module mypy_extensions \
+    --optimize=2
+
+echo "Build completed successfully for $ARCH architecture"
--- scripts/distribution/package_windows.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-pyinstaller --clean --onedir --name puya --noconfirm src/puya/__main__.py --add-data ./src/puya/ir/_puya_lib.awst.json;puya/ir/ --exclude-module puyapy --exclude-module mypy_extensions --optimize=2 --hidden-import colorama --runtime-hook scripts/distribution/hook_puya.py
