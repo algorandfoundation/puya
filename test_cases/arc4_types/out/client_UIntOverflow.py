1adb6a1f6b6d44e1bedb5e67d5fe328b0762e882
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-03-12 17:24:33 +0800

    fix: check for out of bound values when encoding ARC-4 UIntN less than 64 bits

---
+++ test_cases/arc4_types/out/client_UIntOverflow.py
@@ -0,0 +1,23 @@
+# This file is auto-generated, do not modify
+# flake8: noqa
+# fmt: off
+import typing
+
+import algopy
+
+
+class UIntOverflow(algopy.arc4.ARC4Client, typing.Protocol):
+    @algopy.arc4.abimethod
+    def test_uint8(
+        self,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def test_uint16(
+        self,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def test_uint32(
+        self,
+    ) -> None: ...
