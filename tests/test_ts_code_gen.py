faee67ca0464c1a5e690115e43da3b2f812a3508
Adam Chidlow <achidlow@users.noreply.github.com>
2025-04-03 16:03:11 +0800

    refactor: convert generate_ts_nodes.py to use introspection rather than parsing with MyPy

--- tests/test_ts_code_gen.py
+++ tests/test_ts_code_gen.py
@@ -7,6 +7,6 @@ VCS_ROOT = Path(__file__).parent.parent
 
 
 def test_ts_code_gen() -> None:
-    generate_file(out_path=VCS_ROOT / "tests/output/nodes.ts.txt", puya_path=VCS_ROOT / "src/puya")
+    generate_file(out_path=VCS_ROOT / "tests" / "output" / "nodes.ts.txt")
     diff = check_for_diff(VCS_ROOT / "tests/output", VCS_ROOT)
     assert not diff, f"Uncommitted changes were found:\n{diff}"
