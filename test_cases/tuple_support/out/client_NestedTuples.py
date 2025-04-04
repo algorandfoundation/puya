f9eab50d031e44b89351d11c3057a67ef54a7be6
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-04-04 10:01:23 +0200

    chore: compile all

--- test_cases/tuple_support/out/client_NestedTuples.py
+++ test_cases/tuple_support/out/client_NestedTuples.py
@@ -15,7 +15,44 @@ class Parent(algopy.arc4.Struct):
     foo_arc: algopy.arc4.UIntN[typing.Literal[64]]
     child: Child
 
+class ParentWithList(algopy.arc4.Struct):
+    parent: Parent
+    children: algopy.arc4.DynamicArray[algopy.arc4.Tuple[algopy.arc4.UIntN[typing.Literal[64]], algopy.arc4.DynamicBytes, algopy.arc4.String]]
+
+class SimpleTup(algopy.arc4.Struct):
+    a: algopy.arc4.UIntN[typing.Literal[64]]
+    b: algopy.arc4.UIntN[typing.Literal[64]]
+
 class NestedTuples(algopy.arc4.ARC4Client, typing.Protocol):
+    @algopy.arc4.abimethod
+    def store_tuple(
+        self,
+        pwl: ParentWithList,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def load_tuple(
+        self,
+    ) -> ParentWithList: ...
+
+    @algopy.arc4.abimethod
+    def store_tuple_in_box(
+        self,
+        key: SimpleTup,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def is_tuple_in_box(
+        self,
+        key: SimpleTup,
+    ) -> algopy.arc4.Bool: ...
+
+    @algopy.arc4.abimethod
+    def load_tuple_from_box(
+        self,
+        key: SimpleTup,
+    ) -> SimpleTup: ...
+
     @algopy.arc4.abimethod
     def run_tests(
         self,
