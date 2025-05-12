5dae02020df2000e4fd686aada26c5de39fcf978
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-05-12 10:03:22 +0800

    feat: optimize repeated box read/writes

--- src/puya/ir/optimize/repeated_loads_elimination.py
+++ src/puya/ir/optimize/repeated_loads_elimination.py
@@ -15,6 +15,8 @@ def constant_reads_and_unobserved_writes_elimination(
     for block in subroutine.body:
         while _optimise_global_read_write(block):
             any_modified = True
+        while _optimise_box_read_write(block):
+            any_modified = True
         while _optimise_slot_read_write(block):
             any_modified = True
     return any_modified
@@ -77,8 +79,7 @@ def _optimise_global_read_write(block: models.BasicBlock) -> bool:
                     block.ops.remove(last_write)
                     modified = True
                 last_write = write
-            # be conservative and treat any subroutine call or delete as a barrier to this
-            # particular optimisation
+            # be conservative and treat any subroutine call or modifications as a barrier
             case (
                 models.InvokeSubroutine()
                 | models.Assignment(source=models.InvokeSubroutine())
@@ -151,12 +152,78 @@ def _optimise_local_read_write(block: models.BasicBlock) -> bool:
                     block.ops.remove(last_write)
                     modified = True
                 last_write = write
-            # be conservative and treat any subroutine call or delete as a barrier to this
-            # particular optimisation
+            # be conservative and treat any subroutine call or modification as a barrier
             case (
                 models.InvokeSubroutine()
                 | models.Assignment(source=models.InvokeSubroutine())
                 | models.Intrinsic(op=AVMOp.app_local_del)
+            ):
+                last_write = None
+                read_results = {}
+    return modified
+
+
+def _optimise_box_read_write(block: models.BasicBlock) -> bool:
+    modified = False
+    # map of box_get key -> box_get result
+    read_results = dict[models.Value, tuple[models.Value, models.Value]]()
+    last_write: models.Intrinsic | None = None
+    for op in block.ops:
+        match op:
+            case models.Assignment(
+                source=models.Intrinsic(op=AVMOp.box_extract | AVMOp.box_len),
+            ):
+                # these count as reads
+                # TODO: optimize repeats
+                last_write = None
+            case models.Assignment(
+                targets=[value, exists],
+                source=models.Intrinsic(op=AVMOp.box_get, args=[key]),
+            ):
+                last_write = None  # doing a read resets
+                existing_value, existing_exists = read_results.setdefault(key, (value, exists))
+                if existing_exists and (value, exists) != (existing_value, existing_exists):
+                    logger.debug(
+                        f"removing repeated box_get for key: {key}",
+                        location=op.source_location,
+                    )
+                    op.source = models.ValueTuple(
+                        values=(existing_value, existing_exists), source_location=None
+                    )
+                    modified = True
+            case models.Intrinsic(op=AVMOp.box_put, args=[key, value]) as write:
+                # update cache with new value.
+                # this is conservative to naively avoid the problem when multiple
+                # values point to the same slot
+                read_results = {
+                    key: (
+                        value,
+                        models.UInt64Constant(
+                            value=1, ir_type=PrimitiveIRType.bool, source_location=None
+                        ),
+                    )
+                }
+                # remove last_write if it is overwritten with another write, and there
+                # are no intervening reads
+                if last_write and (key == last_write.args[0]):
+                    logger.debug(
+                        f"removing repeated box_put for key: {key}",
+                        location=op.source_location,
+                    )
+                    block.ops.remove(last_write)
+                    modified = True
+                last_write = write
+            # be conservative and treat any subroutine call or box modification as a barrier
+            case (
+                models.InvokeSubroutine()
+                | models.Assignment(source=models.InvokeSubroutine())
+                | models.Intrinsic(
+                    op=AVMOp.box_del
+                    | AVMOp.box_splice
+                    | AVMOp.box_resize
+                    | AVMOp.box_replace
+                    | AVMOp.box_create
+                )
             ):
                 last_write = None
                 read_results = {}
@@ -186,8 +253,7 @@ def _optimise_slot_read_write(block: models.BasicBlock) -> bool:
                     block.ops.remove(last_write)
                     modified = True
                 last_write = write
-            # be conservative and treat any subroutine call as a barrier to this
-            # particular optimisation
+            # be conservative and treat any subroutine call as a barrier
             case models.InvokeSubroutine() | models.Assignment(source=models.InvokeSubroutine()):
                 last_write = None
                 slot_read_results = {}
