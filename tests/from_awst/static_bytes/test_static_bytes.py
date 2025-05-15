b56de66d79eecf5daf50be307f03576a2b0260e9
Tristan Menzel <tristanmenzel@gmail.com>
2025-05-15 13:19:02 -0700

    test: Add test which compiles awst output of puya-ts for static bytes contract

---
+++ tests/from_awst/static_bytes/test_static_bytes.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import algokit_utils
+from algosdk.v2client.algod import AlgodClient
+
+from tests import FROM_AWST_DIR
+from tests.from_awst.util import compile_contract
+
+
+def test_compile_and_run(
+    algod_client: AlgodClient,
+    account: algokit_utils.Account,
+) -> None:
+    compile_contract(
+        awst_path=FROM_AWST_DIR / "static_bytes" / "module.awst.json",
+        compilation_set={"tests/approvals/static-bytes.algo.ts::StaticBytesAlgo": Path("out")},
+    )
+
+    app_spec_json = (Path("out") / "StaticBytesAlgo.arc32.json").read_text("utf8")
+
+    app_spec = algokit_utils.ApplicationSpecification.from_json(app_spec_json)
+    app_client = algokit_utils.ApplicationClient(algod_client, app_spec, signer=account)
+
+    app_client.create()
+
+    res_receive_b32 = app_client.call(call_abi_method="receiveB32", b=b"\0" * 32)
+    assert len(res_receive_b32.return_value) == 32
+
+    app_client.call(call_abi_method="test")
+
+    app_client.call(call_abi_method="testArray")
