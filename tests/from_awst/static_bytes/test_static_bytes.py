f73b44483cf6d9ede0326225c29e5a1aa338ea8d
Adam Chidlow <achidlow@users.noreply.github.com>
2025-05-16 10:39:18 +0800

    fix relative path issue

--- tests/from_awst/static_bytes/test_static_bytes.py
+++ tests/from_awst/static_bytes/test_static_bytes.py
@@ -11,12 +11,15 @@ def test_compile_and_run(
     algod_client: AlgodClient,
     account: algokit_utils.Account,
 ) -> None:
+    test_dir = Path(__file__).parent
+    out_dir = test_dir / "out"
+
     compile_contract(
         awst_path=FROM_AWST_DIR / "static_bytes" / "module.awst.json",
-        compilation_set={"tests/approvals/static-bytes.algo.ts::StaticBytesAlgo": Path("out")},
+        compilation_set={"tests/approvals/static-bytes.algo.ts::StaticBytesAlgo": out_dir},
     )
 
-    app_spec_json = (Path("out") / "StaticBytesAlgo.arc32.json").read_text("utf8")
+    app_spec_json = (out_dir / "StaticBytesAlgo.arc32.json").read_text("utf8")
 
     app_spec = algokit_utils.ApplicationSpecification.from_json(app_spec_json)
     app_client = algokit_utils.ApplicationClient(algod_client, app_spec, signer=account)
