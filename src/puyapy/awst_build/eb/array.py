52d532acc9860e495a4ffc34317d4c40677b9762
Adam Chidlow <achidlow@users.noreply.github.com>
2025-05-22 13:53:19 +0800

    linting

--- src/puyapy/awst_build/eb/array.py
+++ src/puyapy/awst_build/eb/array.py
@@ -25,18 +25,12 @@ from puyapy.awst_build.eb._base import (
     InstanceExpressionBuilder,
     TypeBuilder,
 )
-from puyapy.awst_build.eb._utils import (
-    dummy_value,
-    resolve_negative_literal_index,
-)
+from puyapy.awst_build.eb._utils import dummy_value, resolve_negative_literal_index
 
 # TODO: move these out of ARC-4 ?
 from puyapy.awst_build.eb.arc4._base import CopyBuilder
 from puyapy.awst_build.eb.factories import builder_for_instance
-from puyapy.awst_build.eb.interface import (
-    InstanceBuilder,
-    NodeBuilder,
-)
+from puyapy.awst_build.eb.interface import InstanceBuilder, NodeBuilder
 from puyapy.awst_build.eb.none import NoneExpressionBuilder
 from puyapy.awst_build.eb.uint64 import UInt64ExpressionBuilder
 
