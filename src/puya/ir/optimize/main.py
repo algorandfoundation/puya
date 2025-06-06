9b3ba67f4aef812d973535f86fbeadc62ce9eeb4
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-06-06 11:29:53 +0800

    feat(opt): merge chained aggregate reads

--- src/puya/ir/optimize/main.py
+++ src/puya/ir/optimize/main.py
@@ -20,6 +20,7 @@ from puya.ir.optimize.inlining import analyse_subroutines_for_inlining, perform_
 from puya.ir.optimize.inner_txn import inner_txn_field_replacer
 from puya.ir.optimize.intrinsic_simplification import intrinsic_simplifier
 from puya.ir.optimize.itxn_field_elision import elide_itxn_field_calls
+from puya.ir.optimize.repeated_aggregate_reads_merge import merge_chained_aggregate_reads
 from puya.ir.optimize.repeated_code_elimination import repeated_expression_elimination
 from puya.ir.optimize.repeated_loads_elimination import (
     constant_reads_and_unobserved_writes_elimination,
@@ -74,6 +75,7 @@ def get_subroutine_optimizations(optimization_level: int) -> Iterable[Subroutine
             SubroutineOptimization.from_function(remove_unreachable_blocks),
             SubroutineOptimization.from_function(repeated_expression_elimination),
             SubroutineOptimization.from_function(constant_reads_and_unobserved_writes_elimination),
+            SubroutineOptimization.from_function(merge_chained_aggregate_reads),
         ]
     else:
         return [
