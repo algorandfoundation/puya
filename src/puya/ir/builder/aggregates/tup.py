a50f89d660a4ec18fb4153ea5695eecc932ada2c
Adam Chidlow <achidlow@users.noreply.github.com>
2025-07-11 15:20:14 +0800

    ch-ch-ch-changes

--- src/puya/ir/builder/aggregates/tup.py
+++ src/puya/ir/builder/aggregates/tup.py
@@ -20,31 +20,24 @@ def read_at_index(
     factory = OpFactory(context, loc)
 
     # TODO: split by element_encoding
-    tuple_elements = tuple_encoding.elements
-    element_encoding = tuple_elements[index]
+    element_encoding = tuple_encoding.elements[index]
     head_offset_bits = tuple_encoding.get_head_bit_offset(index)
+
     if element_encoding.is_bit:
         return factory.get_bit(tup, head_offset_bits)
-    elif not element_encoding.is_dynamic:
+    elif element_encoding.is_fixed:
         head_offset = bits_to_bytes(head_offset_bits)
         return factory.extract3(tup, head_offset, element_encoding.checked_num_bytes)
     else:
+        assert element_encoding.is_dynamic, "expected dynamic encoding"
+
         head_offset = bits_to_bytes(head_offset_bits)
         item_start_offset = factory.extract_uint16(tup, head_offset)
-
-        next_index = index + 1
-        for tuple_item_index, tuple_item_type in enumerate(
-            tuple_elements[next_index:], start=next_index
-        ):
-            if tuple_item_type.is_dynamic:
-                next_dynamic_head_offset_bits = tuple_encoding.get_head_bit_offset(
-                    tuple_item_index
-                )
-                next_dynamic_head_offset = bits_to_bytes(next_dynamic_head_offset_bits)
-                item_end_offset = factory.extract_uint16(tup, next_dynamic_head_offset)
-                break
-        else:
+        dynamic_head_offsets = _get_subsequent_dynamic_head_offsets(tuple_encoding, index)
+        if not dynamic_head_offsets:
             item_end_offset = factory.len(tup)
+        else:
+            item_end_offset = factory.extract_uint16(tup, dynamic_head_offsets[0])
         return factory.substring3(tup, item_start_offset, item_end_offset)
 
 
@@ -57,73 +50,65 @@ def write_at_index(
     loc: SourceLocation | None,
 ) -> ir.Value:
     factory = OpFactory(context, loc)
+
+    # TODO: split by element_encoding, as with read_at_index
     element_encoding = tuple_encoding.elements[index]
+    head_offset_bits = tuple_encoding.get_head_bit_offset(index)
 
     if element_encoding.is_bit:
         # Use Set bit
         assert value.ir_type.avm_type == AVMType.uint64, "expected bool value"
         return factory.set_bit(
-            value=tup,
-            index=tuple_encoding.get_head_bit_offset(index),
-            bit=value,
-            temp_desc="updated_data",
+            value=tup, index=head_offset_bits, bit=value, temp_desc="updated_data"
         )
-    header_up_to_item_bytes = bits_to_bytes(tuple_encoding.get_head_bit_offset(index))
-    if not element_encoding.is_dynamic:
-        return factory.replace(
-            tup,
-            header_up_to_item_bytes,
-            value,
-            "updated_data",
+    elif element_encoding.is_fixed:
+        head_offset = bits_to_bytes(head_offset_bits)
+        return factory.replace(tup, head_offset, value, "updated_data")
+    else:
+        assert element_encoding.is_dynamic, "expected dynamic encoding"
+
+        head_offset = bits_to_bytes(head_offset_bits)
+        item_offset = factory.extract_uint16(tup, head_offset, "item_offset")
+
+        dynamic_head_offsets = _get_subsequent_dynamic_head_offsets(tuple_encoding, index)
+        data_up_to_item = factory.extract3(tup, 0, item_offset, "data_up_to_item")
+        if not dynamic_head_offsets:
+            # This is the last dynamic type in the tuple
+            # No need to update headers - just replace the data
+            return factory.concat(data_up_to_item, value, "updated_data")
+
+        # update tail portion with new item
+        next_item_offset = factory.extract_uint16(tup, dynamic_head_offsets[0], "next_item_offset")
+        total_data_length = factory.len(tup, "total_data_length")
+        data_beyond_item = factory.substring3(
+            tup, next_item_offset, total_data_length, "data_beyond_item"
         )
-    assert element_encoding.is_dynamic, "expected dynamic encoding"
-    dynamic_indices = [index for index, t in enumerate(tuple_encoding.elements) if t.is_dynamic]
+        updated_data = factory.concat(data_up_to_item, value, "updated_data")
+        updated_data = factory.concat(updated_data, data_beyond_item, "updated_data")
 
-    item_offset = factory.extract_uint16(tup, header_up_to_item_bytes, "item_offset")
-    data_up_to_item = factory.extract3(tup, 0, item_offset, "data_up_to_item")
-    dynamic_indices_after_item = [i for i in dynamic_indices if i > index]
+        # loop through head and update any offsets after modified item
+        item_length = factory.sub(next_item_offset, item_offset, "item_length")
+        new_value_length = factory.len(value, "new_value_length")
+        for header_up_to_dynamic_item in dynamic_head_offsets:
+            tail_offset = factory.extract_uint16(
+                updated_data, header_up_to_dynamic_item, "tail_offset"
+            )
+            # have to add the new length and then subtract the original to avoid underflow
+            tail_offset = factory.add(tail_offset, new_value_length, "tail_offset")
+            tail_offset = factory.sub(tail_offset, item_length, "tail_offset")
+            tail_offset_bytes = factory.as_u16_bytes(tail_offset, "tail_offset_bytes")
 
-    if not dynamic_indices_after_item:
-        # This is the last dynamic type in the tuple
-        # No need to update headers - just replace the data
-        return factory.concat(data_up_to_item, value, "updated_data")
-    header_up_to_next_dynamic_item = bits_to_bytes(
-        tuple_encoding.get_head_bit_offset(dynamic_indices_after_item[0])
-    )
+            updated_data = factory.replace(
+                updated_data, header_up_to_dynamic_item, tail_offset_bytes, "updated_data"
+            )
+        return updated_data
 
-    # update tail portion with new item
-    next_item_offset = factory.extract_uint16(
-        tup,
-        header_up_to_next_dynamic_item,
-        "next_item_offset",
-    )
-    total_data_length = factory.len(tup, "total_data_length")
-    data_beyond_item = factory.substring3(
-        tup,
-        next_item_offset,
-        total_data_length,
-        "data_beyond_item",
-    )
-    updated_data = factory.concat(data_up_to_item, value, "updated_data")
-    updated_data = factory.concat(updated_data, data_beyond_item, "updated_data")
 
-    # loop through head and update any offsets after modified item
-    item_length = factory.sub(next_item_offset, item_offset, "item_length")
-    new_value_length = factory.len(value, "new_value_length")
-    for dynamic_index in dynamic_indices_after_item:
-        header_up_to_dynamic_item = bits_to_bytes(
-            tuple_encoding.get_head_bit_offset(dynamic_index)
-        )
-
-        tail_offset = factory.extract_uint16(
-            updated_data, header_up_to_dynamic_item, "tail_offset"
-        )
-        # have to add the new length and then subtract the original to avoid underflow
-        tail_offset = factory.add(tail_offset, new_value_length, "tail_offset")
-        tail_offset = factory.sub(tail_offset, item_length, "tail_offset")
-        tail_offset_bytes = factory.as_u16_bytes(tail_offset, "tail_offset_bytes")
-
-        updated_data = factory.replace(
-            updated_data, header_up_to_dynamic_item, tail_offset_bytes, "updated_data"
-        )
-    return updated_data
+def _get_subsequent_dynamic_head_offsets(tuple_encoding: TupleEncoding, index: int) -> list[int]:
+    tuple_elements = tuple_encoding.elements
+    next_index = index + 1
+    return [
+        bits_to_bytes(tuple_encoding.get_head_bit_offset(idx))
+        for idx, elem in enumerate(tuple_elements[next_index:], start=next_index)
+        if elem.is_dynamic
+    ]
