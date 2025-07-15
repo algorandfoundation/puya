8c94e59a174533f01413837e8711984bb80c5c67
Adam Chidlow <achidlow@users.noreply.github.com>
2025-07-15 15:10:07 +0800

    reduce imports

--- src/puya/ir/builder/sequence.py
+++ src/puya/ir/builder/sequence.py
@@ -3,28 +3,16 @@ from collections.abc import Sequence
 from puya import log
 from puya.awst import wtypes
 from puya.errors import InternalError
-from puya.ir import models as ir
+from puya.ir import (
+    encodings,
+    models as ir,
+    types_ as types,
+)
 from puya.ir._puya_lib import PuyaLibIR
 from puya.ir.builder import mem
 from puya.ir.builder._utils import invoke_puya_lib_subroutine, undefined_value
-from puya.ir.encodings import (
-    ArrayEncoding,
-    Bool8Encoding,
-    BoolEncoding,
-    Encoding,
-    TupleEncoding,
-    wtype_to_encoding,
-)
 from puya.ir.op_utils import OpFactory, assert_value
 from puya.ir.register_context import IRRegisterContext
-from puya.ir.types_ import (
-    EncodedType,
-    IRType,
-    PrimitiveIRType,
-    SlotType,
-    TupleIRType,
-    wtype_to_ir_type,
-)
 from puya.parse import SourceLocation
 
 logger = log.get_logger(__name__)
@@ -46,7 +34,7 @@ def read_aggregate_index_and_decode(
     if isinstance(aggregate_wtype, wtypes.WTuple):
         (index,) = indexes
         assert isinstance(index, int), "expected integer"
-        tuple_ir_type = wtype_to_ir_type(aggregate_wtype, loc, allow_tuple=True)
+        tuple_ir_type = types.wtype_to_ir_type(aggregate_wtype, loc, allow_tuple=True)
         skip_values = sum(e.arity for e in tuple_ir_type.elements[:index])
         target_arity = tuple_ir_type.elements[index].arity
         element_values = values[skip_values : skip_values + target_arity]
@@ -57,24 +45,24 @@ def read_aggregate_index_and_decode(
             return ir.ValueTuple(values=element_values, source_location=loc)
 
     (base,) = values
-    if isinstance(base.ir_type, SlotType):
+    if isinstance(base.ir_type, types.SlotType):
         base = mem.read_slot(context, base, loc)
-    aggregate_encoding = wtype_to_encoding(aggregate_wtype, loc)
-    assert isinstance(aggregate_encoding, ArrayEncoding | TupleEncoding)
+    aggregate_encoding = encodings.wtype_to_encoding(aggregate_wtype, loc)
+    assert isinstance(aggregate_encoding, encodings.ArrayEncoding | encodings.TupleEncoding)
     element_encoding = _get_aggregate_element_encoding(aggregate_encoding, indexes, loc)
     if element_encoding.is_bit:
-        read_result_type: IRType = PrimitiveIRType.bool
+        read_result_type: types.IRType = types.PrimitiveIRType.bool
     else:
-        read_result_type = EncodedType(element_encoding)
+        read_result_type = types.EncodedType(element_encoding)
     read_index = ir.ExtractValue(
         base=base,
-        base_type=EncodedType(aggregate_encoding),
+        base_type=types.EncodedType(aggregate_encoding),
         ir_type=read_result_type,
         indexes=indexes,
         source_location=loc,
         check_bounds=check_bounds,
     )
-    is_tup = isinstance(aggregate_encoding, TupleEncoding)
+    is_tup = isinstance(aggregate_encoding, encodings.TupleEncoding)
     (tuple_item,) = context.materialise_value_provider(
         read_index, "tuple_item" if is_tup else "array_item"
     )
@@ -105,7 +93,7 @@ def encode_and_write_aggregate_index(
     if isinstance(aggregate_wtype, wtypes.WTuple):
         (index,) = indexes
         assert isinstance(index, int)
-        tuple_ir_type = wtype_to_ir_type(aggregate_wtype, loc, allow_tuple=True)
+        tuple_ir_type = types.wtype_to_ir_type(aggregate_wtype, loc, allow_tuple=True)
         skip_values = sum(e.arity for e in tuple_ir_type.elements[:index])
         target_arity = tuple_ir_type.elements[index].arity
         new_values = context.materialise_value_provider(base, "new_values")
@@ -116,8 +104,8 @@ def encode_and_write_aggregate_index(
             return ir.ValueTuple(values=new_values, source_location=loc)
     (base,) = context.materialise_value_provider(base, "base")
     aggregate_or_slot = base
-    aggregate_encoding = wtype_to_encoding(aggregate_wtype, loc)
-    assert isinstance(aggregate_encoding, TupleEncoding | ArrayEncoding)
+    aggregate_encoding = encodings.wtype_to_encoding(aggregate_wtype, loc)
+    assert isinstance(aggregate_encoding, encodings.TupleEncoding | encodings.ArrayEncoding)
     element_ir_type = _get_nested_element_ir_type(aggregate_wtype, indexes, loc)
     element_encoding = _get_aggregate_element_encoding(aggregate_encoding, indexes, loc)
     (encoded_value,) = context.materialise_value_provider(
@@ -129,12 +117,16 @@ def encode_and_write_aggregate_index(
         ),
         "encoded_value",
     )
-    desc = "updated_tuple" if isinstance(aggregate_encoding, TupleEncoding) else "updated_array"
-    if isinstance(aggregate_or_slot.ir_type, SlotType):
+    desc = (
+        "updated_tuple"
+        if isinstance(aggregate_encoding, encodings.TupleEncoding)
+        else "updated_array"
+    )
+    if isinstance(aggregate_or_slot.ir_type, types.SlotType):
         base = mem.read_slot(context, aggregate_or_slot, loc)
         write_index = ir.ReplaceValue(
             base=base,
-            base_type=EncodedType(aggregate_encoding),
+            base_type=types.EncodedType(aggregate_encoding),
             indexes=indexes,
             value=encoded_value,
             source_location=loc,
@@ -145,7 +137,7 @@ def encode_and_write_aggregate_index(
     else:
         write_index = ir.ReplaceValue(
             base=base,
-            base_type=EncodedType(aggregate_encoding),
+            base_type=types.EncodedType(aggregate_encoding),
             indexes=indexes,
             value=encoded_value,
             source_location=loc,
@@ -156,11 +148,11 @@ def encode_and_write_aggregate_index(
 
 def get_length(
     context: IRRegisterContext,
-    array_encoding: ArrayEncoding,
+    array_encoding: encodings.ArrayEncoding,
     array_or_slot: ir.Value,
     loc: SourceLocation | None,
 ) -> ir.Value:
-    if isinstance(array_or_slot.ir_type, SlotType):
+    if isinstance(array_or_slot.ir_type, types.SlotType):
         array = mem.read_slot(context, array_or_slot, loc)
     else:
         array = array_or_slot
@@ -185,18 +177,18 @@ def convert_array(
 ) -> ir.ValueProvider:
     factory = OpFactory(context, loc)
 
-    target_ir_type = wtype_to_ir_type(target_wtype, loc)
-    target_encoding = wtype_to_encoding(target_wtype, loc)
-    source_encoding = wtype_to_encoding(source_wtype, loc)
+    target_ir_type = types.wtype_to_ir_type(target_wtype, loc)
+    target_encoding = encodings.wtype_to_encoding(target_wtype, loc)
+    source_encoding = encodings.wtype_to_encoding(source_wtype, loc)
 
-    if isinstance(source.ir_type, SlotType):
+    if isinstance(source.ir_type, types.SlotType):
         source = mem.read_slot(context, source, loc)
     source_length = get_length(context, source_encoding, source, loc)
 
     match source_encoding.element, target_encoding.element:
         case (
-            BoolEncoding(),
-            Bool8Encoding(),
+            encodings.BoolEncoding(),
+            encodings.Bool8Encoding(),
         ):
             logger.error(
                 "converting from a bitpacked bool array"
@@ -205,8 +197,8 @@ def convert_array(
             )
             return undefined_value(target_ir_type, loc)
         case (
-            Bool8Encoding(),
-            BoolEncoding(),
+            encodings.Bool8Encoding(),
+            encodings.BoolEncoding(),
         ):
             assert not source_encoding.length_header, "expected ReferenceArray"
             empty_header = factory.constant(b"\0" * 2)
@@ -219,7 +211,9 @@ def convert_array(
             (source,) = context.materialise_value_provider(
                 bitpacked_source_provider, description="bit_packed_source"
             )
-            source_encoding = ArrayEncoding.dynamic(BoolEncoding(), length_header=True)
+            source_encoding = encodings.ArrayEncoding.dynamic(
+                encodings.BoolEncoding(), length_header=True
+            )
 
     if target_encoding.element != source_encoding.element:
         logger.error(
@@ -243,7 +237,7 @@ def convert_array(
             logger.error("static size conversion cannot add or remove elements", location=loc)
             return undefined_value(target_ir_type, loc)
 
-    if isinstance(target_ir_type, SlotType):
+    if isinstance(target_ir_type, types.SlotType):
         target_value_type = target_ir_type.contents
     else:
         target_value_type = target_ir_type
@@ -259,7 +253,7 @@ def convert_array(
     else:
         new_value = source
 
-    if isinstance(target_ir_type, SlotType):
+    if isinstance(target_ir_type, types.SlotType):
         new_slot = mem.new_slot(context, target_ir_type, loc)
         mem.write_slot(context, new_slot, new_value, loc)
         return new_slot
@@ -268,7 +262,7 @@ def convert_array(
 
 def _get_nested_element_ir_type(
     aggregate: WAggregate, indexes: Sequence[int | ir.Value], loc: SourceLocation
-) -> IRType | TupleIRType:
+) -> types.IRType | types.TupleIRType:
     last_i = len(indexes) - 1
     element = None
     for i, index in enumerate(indexes):
@@ -292,20 +286,20 @@ def _get_nested_element_ir_type(
             raise InternalError(f"invalid index sequence: {aggregate=!s}, {index=!s}", loc)
     if element is None:
         raise InternalError(f"invalid index sequence: {aggregate=!s}, {indexes=!s}", loc)
-    return wtype_to_ir_type(element, loc, allow_tuple=True)
+    return types.wtype_to_ir_type(element, loc, allow_tuple=True)
 
 
 def _get_aggregate_element_encoding(
-    aggregate_encoding: TupleEncoding | ArrayEncoding,
+    aggregate_encoding: encodings.TupleEncoding | encodings.ArrayEncoding,
     indexes: Sequence[int | ir.Value],
     loc: SourceLocation | None,
-) -> Encoding:
+) -> encodings.Encoding:
     last_i = len(indexes) - 1
     element_encoding = None
     for i, index in enumerate(indexes):
-        if isinstance(aggregate_encoding, TupleEncoding) and isinstance(index, int):
+        if isinstance(aggregate_encoding, encodings.TupleEncoding) and isinstance(index, int):
             element_encoding = aggregate_encoding.elements[index]
-        elif isinstance(aggregate_encoding, ArrayEncoding):
+        elif isinstance(aggregate_encoding, encodings.ArrayEncoding):
             element_encoding = aggregate_encoding.element
         else:
             raise InternalError(
@@ -314,7 +308,7 @@ def _get_aggregate_element_encoding(
         if i == last_i:
             # last index is the only one that doesn't need to be an aggregate
             pass
-        elif isinstance(element_encoding, TupleEncoding | ArrayEncoding):
+        elif isinstance(element_encoding, encodings.TupleEncoding | encodings.ArrayEncoding):
             aggregate_encoding = element_encoding
         else:
             # invalid index sequence
