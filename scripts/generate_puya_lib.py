e8836b09d04312b9f4a6c66ba4a761d34109f953
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-07-16 13:32:02 +0800

    refactor: add PuyaLibIR enum for puya lib functions

--- scripts/generate_puya_lib.py
+++ scripts/generate_puya_lib.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import json
 import subprocess
 from pathlib import Path
 
@@ -15,14 +15,30 @@ def main() -> None:
     subprocess.run(["puyapy", "--output-awst-json", f"src/{LIB_NAME}"], check=True, cwd=VCS_ROOT)
     awst_path = VCS_ROOT / "module.awst.json"
     puya_lib_path = VCS_ROOT / "src" / LIB_NAME
-    output_path = VCS_ROOT / "src" / "puya" / "ir" / "_puya_lib.awst.json"
+    json_output_path = VCS_ROOT / "src" / "puya" / "ir" / "_puya_lib.awst.json"
     replace_awst = awst_path.read_text(encoding="utf8")
     for lib_path in puya_lib_path.glob("*.py"):
         path_as_str = str(lib_path).replace("\\", "\\\\")
         find_str = f'"file": "{path_as_str}",'
         replace_str = '"file": null,'
         replace_awst = replace_awst.replace(find_str, replace_str)
-    output_path.write_text(replace_awst, encoding="utf8")
+    json_output_path.write_text(replace_awst, encoding="utf8")
+
+    enum_output_path = VCS_ROOT / "src" / "puya" / "ir" / "_puya_lib.py"
+    lib_name_ids = {n["name"]: n["id"] for n in json.loads(replace_awst)}
+    lib_enum = [
+        "# ruff: noqa: E501",
+        "# fmt: off",
+        "",
+        "import enum",
+        "",
+        "",
+        "@enum.unique",
+        "class PuyaLibIR(enum.StrEnum):",
+        *(f'    {name} = "{full_name}"' for name, full_name in lib_name_ids.items()),
+        "",
+    ]
+    enum_output_path.write_text("\n".join(lib_enum), encoding="utf8")
     awst_path.unlink(missing_ok=True)
 
 
