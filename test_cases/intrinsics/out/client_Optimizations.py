b31492db1fa72c1d686c5cf8ad6ea1daa4fb9010
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-07-21 15:06:16 +0800

    chore: compile all

--- test_cases/intrinsics/out/client_Optimizations.py
+++ test_cases/intrinsics/out/client_Optimizations.py
@@ -11,3 +11,18 @@ class Optimizations(algopy.arc4.ARC4Client, typing.Protocol):
     def sha256(
         self,
     ) -> algopy.arc4.DynamicBytes: ...
+
+    @algopy.arc4.abimethod
+    def sha3_256(
+        self,
+    ) -> algopy.arc4.DynamicBytes: ...
+
+    @algopy.arc4.abimethod
+    def sha512_256(
+        self,
+    ) -> algopy.arc4.DynamicBytes: ...
+
+    @algopy.arc4.abimethod
+    def keccak256(
+        self,
+    ) -> algopy.arc4.DynamicBytes: ...
