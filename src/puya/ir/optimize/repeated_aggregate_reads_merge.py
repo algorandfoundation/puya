db0c9cff492233658f96ddfb9bebc1828b8ba5d6
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-07-23 09:57:00 +0800

    refactor: update merge_chained_aggregate_reads to look through redundant `ByteEncode` nodes

--- src/puya/ir/optimize/repeated_aggregate_reads_merge.py
+++ src/puya/ir/optimize/repeated_aggregate_reads_merge.py
@@ -4,7 +4,10 @@ import attrs
 
 from puya import log
 from puya.context import CompileContext
-from puya.ir import models
+from puya.ir import (
+    models,
+    types_ as types,
+)
 from puya.ir.visitor import IRTraverser
 from puya.parse import sequential_source_locations_merge
 
@@ -14,10 +17,10 @@ logger = log.get_logger(__name__)
 def merge_chained_aggregate_reads(_: CompileContext, subroutine: models.Subroutine) -> bool:
     modified = False
 
-    reads = _AggregateReadsCollector.collect(subroutine)
-    for ass, read in reads.values():
+    collector = _AggregateReadsCollector.collect(subroutine)
+    for ass, read in collector.reads.values():
         merged_read = read
-        while base_read_ass := reads.get(merged_read.base):
+        while base_read_ass := collector.get_read(merged_read.base):
             base_read = base_read_ass.read
             merged_source_location = sequential_source_locations_merge(
                 (merged_read.source_location, base_read.source_location)
@@ -47,15 +50,28 @@ class _AggregateRead(typing.NamedTuple):
 @attrs.define(kw_only=True)
 class _AggregateReadsCollector(IRTraverser):
     reads: dict[models.Value, _AggregateRead] = attrs.field(factory=dict)
+    decodes: dict[models.Value, models.Value] = attrs.field(factory=dict)
+
+    def get_read(self, value: models.Value) -> _AggregateRead | None:
+        while True:
+            try:
+                value = self.decodes[value]
+            except KeyError:
+                break
+        return self.reads.get(value)
 
     @classmethod
-    def collect(cls, sub: models.Subroutine) -> dict[models.Value, _AggregateRead]:
+    def collect(cls, sub: models.Subroutine) -> typing.Self:
         collector = cls()
         collector.visit_all_blocks(sub.body)
-        return collector.reads
+        return collector
 
     @typing.override
     def visit_assignment(self, ass: models.Assignment) -> None:
         if isinstance(ass.source, models.ExtractValue):
             (target,) = ass.targets
             self.reads[target] = _AggregateRead(ass, ass.source)
+        elif isinstance(ass.source, models.DecodeBytes):
+            if types.EncodedType(ass.source.encoding) == ass.source.ir_type:
+                (target,) = ass.targets
+                self.decodes[target] = ass.source.value
