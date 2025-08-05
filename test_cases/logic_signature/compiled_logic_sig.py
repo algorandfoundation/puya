e3bd299bac6d50315956be0fc89b692371f9853c
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-08-05 16:28:19 +0800

    test: expand test coverage to verify op.args optimizations

--- test_cases/logic_signature/compiled_logic_sig.py
+++ test_cases/logic_signature/compiled_logic_sig.py
@@ -4,6 +4,11 @@ from algopy import Txn, arc4, compile_logicsig, logicsig, op
 @logicsig
 def dont_use_this() -> bool:
     assert op.arg(1) == b"this is not secure", "arg is not correct"
+    assert op.arg(0) == b"0"
+    assert op.arg(2) == b"2"
+    assert op.arg(3) == b"3"
+    assert op.arg(4) == b"4"
+    assert op.arg(Txn.num_app_args) == b"cant_happen"
     return True
 
 
