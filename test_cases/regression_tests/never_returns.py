690e833a2ffa7154b0d83d5f07b802f3ddd5d53e
Adam Chidlow <achidlow@users.noreply.github.com>
2025-08-12 16:30:04 +0800

    add test case for F-Stack being generated in non-proto subroutine

--- test_cases/regression_tests/never_returns.py
+++ test_cases/regression_tests/never_returns.py
@@ -1,4 +1,4 @@
-from algopy import ARC4Contract, UInt64, arc4, op, subroutine
+from algopy import ARC4Contract, Txn, UInt64, arc4, log, op, subroutine, urange
 
 
 class NeverReturnsInlining(ARC4Contract):
@@ -10,4 +10,27 @@ class NeverReturnsInlining(ARC4Contract):
 
 @subroutine(inline=True)
 def invoke() -> UInt64:
+    op.err()
+
+
+class NeverReturnsFStack(ARC4Contract):
+    @arc4.abimethod
+    def method(self) -> UInt64:
+        result = invoke_with_fstack()
+        return result
+
+
+@subroutine(inline=True)
+def invoke_with_fstack() -> UInt64:
+    x = UInt64(1)
+    if Txn.num_app_args > 0:
+        x = UInt64(2)
+
+    one = UInt64(0)
+    for idx in urange(Txn.num_app_args):
+        x += idx
+        if idx == 0:
+            one += 1
+
+    log("you won't see this", one)
     op.err()
