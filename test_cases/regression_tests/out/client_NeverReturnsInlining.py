417df40bed2c70ea3ab49e16a3fefa6b229d3571
Adam Chidlow <achidlow@users.noreply.github.com>
2025-08-12 16:07:44 +0800

    chore: compile all

---
+++ test_cases/regression_tests/out/client_NeverReturnsInlining.py
@@ -0,0 +1,13 @@
+# This file is auto-generated, do not modify
+# flake8: noqa
+# fmt: off
+import typing
+
+import algopy
+
+
+class NeverReturnsInlining(algopy.arc4.ARC4Client, typing.Protocol):
+    @algopy.arc4.abimethod
+    def method(
+        self,
+    ) -> algopy.arc4.UIntN[typing.Literal[64]]: ...
