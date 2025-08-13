8efe56f840d503769fdd85db396cd3f551717b31
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-08-13 18:09:48 +0800

    refactor: extract match_compilation_set method

--- src/puya/main.py
+++ src/puya/main.py
@@ -1,3 +1,4 @@
+from collections.abc import Mapping
 from pathlib import Path
 
 import attrs
@@ -8,6 +9,7 @@ from puya.awst import (
     nodes as awst_nodes,
     serialize,
 )
+from puya.awst.nodes import AWST
 from puya.compile import awst_to_teal
 from puya.errors import log_exceptions
 from puya.options import PuyaOptions
@@ -30,18 +32,25 @@ def main(*, options_json: str, awst_json: str, source_annotations_json: str | No
         log_ctx.sources_by_path = sources_by_path
         awst = serialize.awst_from_json(awst_json)
         options = json_converter.loads(options_json, PuyaOptionsWithCompilationSet)
-        compilation_set = dict[ContractReference | LogicSigReference, Path]()
-        awst_lookup = {n.id: n for n in awst}
-        for target_id, path in options.compilation_set.items():
-            match awst_lookup.get(target_id):
-                case awst_nodes.Contract(id=contract_id):
-                    compilation_set[contract_id] = path
-                case awst_nodes.LogicSignature(id=lsig_id):
-                    compilation_set[lsig_id] = path
-                case None:
-                    logger.error(f"compilation target {target_id!r} not found in AWST")
-                case other:
-                    logger.error(f"unexpected compilation target type: {type(other).__name__}")
+        compilation_set = match_compilation_set(options.compilation_set, awst)
         awst_to_teal(log_ctx, options, compilation_set, sources_by_path, awst)
     # note: needs to be outside the with block
     log_ctx.exit_if_errors()
+
+
+def match_compilation_set(
+    targets: Mapping[str, Path], awst: AWST
+) -> Mapping[ContractReference | LogicSigReference, Path]:
+    compilation_set = dict[ContractReference | LogicSigReference, Path]()
+    awst_lookup = {n.id: n for n in awst}
+    for target_id, path in targets.items():
+        match awst_lookup.get(target_id):
+            case awst_nodes.Contract(id=contract_id):
+                compilation_set[contract_id] = path
+            case awst_nodes.LogicSignature(id=lsig_id):
+                compilation_set[lsig_id] = path
+            case None:
+                logger.error(f"compilation target {target_id!r} not found in AWST")
+            case other:
+                logger.error(f"unexpected compilation target type: {type(other).__name__}")
+    return compilation_set
