251da907aa4bec0498b492e5a49c849a3e800c62
Adam Chidlow <achidlow@users.noreply.github.com>
2025-09-30 17:59:44 +0800

    wip (need to change test_transaction_fields.py approach)

--- src/puyapy/awst_build/main.py
+++ src/puyapy/awst_build/main.py
@@ -30,19 +30,9 @@ def _transform_ast(
 
     for module_name, src in parse_result.ordered_modules.items():
         module_rel_path = make_path_relative_to_cwd(src.path)
-        if src.node.is_stub:
-            if module_name in ("abc", "typing", "collections.abc"):
-                logger.debug(f"Skipping stdlib stub {module_rel_path}")  # TODO: lowercase
-            elif module_name.startswith("algopy"):
-                logger.debug(f"Skipping algopy stub {module_rel_path}")
-            elif src.is_typeshed_file:
-                logger.debug(f"Skipping typeshed stub {module_rel_path}")
-            else:
-                logger.warning(f"Skipping stub: {module_rel_path}")
-        else:
-            logger.debug(f"Discovered user module {module_name} at {module_rel_path}")
-            module_ctx = ctx.for_module(src.path)
-            user_modules.append((src, ModuleASTConverter(module_ctx, src.node)))
+        logger.debug(f"Discovered user module {module_name} at {module_rel_path}")
+        module_ctx = ctx.for_module(src.path)
+        user_modules.append((src, ModuleASTConverter(module_ctx, src.node)))
 
     compilation_set = list[ContractReference | LogicSigReference]()
     awst = list[RootNode]()
