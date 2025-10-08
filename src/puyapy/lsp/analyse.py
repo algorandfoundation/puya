cb5ce33019742c56c8c654f822043818545c4a5c
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-08 21:47:28 +0800

    refactor: use mypy symbol table for symbol resolution and state node for dependencies

--- src/puyapy/lsp/analyse.py
+++ src/puyapy/lsp/analyse.py
@@ -3,23 +3,25 @@ import contextlib
 import contextvars
 import platform
 import re
+import typing
 from collections import defaultdict
 from collections.abc import Iterable, Iterator, Mapping, Sequence
 from pathlib import Path
 from urllib.parse import unquote, urlparse
 
 import attrs
-import mypy.nodes
+from lsprotocol import types as lsp
 from pygls.workspace import Workspace
 
 from puya.compile import awst_to_teal
 from puya.errors import PuyaExitError, log_exceptions
 from puya.log import Log, LogLevel, get_logger, logging_context
+from puya.parse import SourceLocation
 from puya.utils import StableSet, coalesce, get_cwd, set_cwd
+from puyapy import code_fixes
 from puyapy.awst_build.main import transform_ast
-from puyapy.code_fixes import CodeFix
 from puyapy.options import PuyaPyOptions
-from puyapy.parse import ParseResult, parse_python
+from puyapy.parse import SourceModule, parse_python
 
 logger = get_logger(__name__)
 
@@ -30,12 +32,10 @@ class DocumentAnalysis:
     """uri of document"""
     version: int | None
     """version of document when it was analysed"""
-    symbols: dict[str, str | int]
-    """a mapping of symbols (incl. modules) to their aliases or the line after their import stmt"""
-    logs: list[Log]
+    diagnostics: list[lsp.Diagnostic]
     """warning or errors present in document"""
-    fixes: list[CodeFix]
-    """possible fixes suggested in document"""
+    actions: list[tuple[lsp.Range, lsp.CodeAction]]
+    """possible code actions suggested in document"""
 
 
 @attrs.define
@@ -65,40 +65,11 @@ class CodeAnalyser:
 
         root_path = Path(self._workspace.root_path)
         with set_cwd(root_path):
-            analysis_results = self._compile_and_capture_symbols_fixes_and_logs(paths)
-
-        # if there are no results then there was a parsing error so don't update anything
-        if analysis_results is None:
-            return {}
-        # need to be able to map Paths back to their original uri's as simply calling
-        # .as_uri() may not give back the original uri
-        path_to_uri_version = dict[Path, tuple[str, int | None]]()
-        for uri, doc in self._workspace.text_documents.items():
-            path = _uri_to_path(uri)
-            if path is not None:
-                path_to_uri_version[path] = uri, doc.version
-
-        analysis = {}
-        for path in paths:
-            try:
-                uri, version = path_to_uri_version[path]
-            except KeyError:
-                uri = path.as_uri()
-                version = None
-            symbols, fixes, logs = analysis_results[path]
-            analysis[uri] = DocumentAnalysis(
-                uri=uri,
-                version=version,
-                symbols=symbols,
-                fixes=fixes,
-                logs=logs,
-            )
-
-        return analysis
+            return self._compile_and_capture_symbols_fixes_and_logs(paths)
 
     def _compile_and_capture_symbols_fixes_and_logs(
         self, paths: Sequence[Path]
-    ) -> dict[Path, tuple[dict[str, str | int], list[CodeFix], list[Log]]] | None:
+    ) -> dict[str, DocumentAnalysis]:
         # parse Typed AST
         puyapy_options = PuyaPyOptions(
             log_level=LogLevel.info,
@@ -122,10 +93,13 @@ class CodeAnalyser:
         except Exception as ex:
             # TODO: how should we recover if mypy has critical errors
             logger.debug(f"error during parsing: {ex}")
-            return None
+            return {}
         logger.debug("AST parse complete")
-        symbols = self._analyse_imports(parse_result)
-        fixes = list[CodeFix]()
+        modules = parse_result.ordered_modules
+        # capture dependencies for future passes so we can refresh modules that may require
+        # updating when their dependencies change
+        self._analyse_dependencies(modules)
+        fixes = list[code_fixes.CodeFix]()
         logs = list(parse_ctx.logs)
         # lower Typed AST to AWST
         if not parse_ctx.num_errors:
@@ -160,19 +134,49 @@ class CodeAnalyser:
                     )
                 logs.extend(awst_ctx.logs)
         logger.debug("compile complete")
+        modules_by_path = {m.path: m for m in modules.values()}
         result = {}
+
+        # need to be able to map Paths back to their original uri's as simply calling
+        # .as_uri() may not give back the original uri
+        path_to_uri_version = dict[Path, tuple[str, int | None]]()
+        for uri, doc in self._workspace.text_documents.items():
+            path = _uri_to_path(uri)
+            if path is not None:
+                path_to_uri_version[path] = uri, doc.version
+
         for path in puyapy_options.paths:
-            path_symbols = symbols.get(path, {})
-            path_fixes = [fix for fix in fixes if fix.location.file == path]
+            try:
+                uri, version = path_to_uri_version[path]
+            except KeyError:
+                uri = path.as_uri()
+                version = None
+            module = modules_by_path[path]
             path_logs = [
                 log
                 for log in logs
                 if log.level > LogLevel.info and log.location and log.location.file == path
             ]
-            result[path] = path_symbols, path_fixes, path_logs
+            path_fixes = [fix for fix in fixes if fix.location.file == path]
+            path_actions = []
+            if path_fixes:
+                symbol_resolver = _AlgopySymbolResolver.create(module)
+                path_actions = [
+                    (
+                        _source_location_to_range(fix.location),
+                        symbol_resolver.fix_to_code_action(uri, fix),
+                    )
+                    for fix in fixes
+                    if fix.location.file == path
+                ]
             logger.debug(
-                f"analysis found {path} has"
-                f" {len(path_symbols)} symbols {len(path_fixes)} fixes, {len(path_logs)} logs"
+                f"analysis found {path} has {len(path_actions)} actions, {len(path_logs)} logs"
+            )
+            result[uri] = DocumentAnalysis(
+                uri=uri,
+                version=version,
+                actions=path_actions,
+                diagnostics=list(map(_log_to_diagnostic, path_logs)),
             )
         return result
 
@@ -190,66 +194,24 @@ class CodeAnalyser:
             result[path] = doc.source
         return result
 
-    def _analyse_imports(self, parse_result: ParseResult) -> dict[Path, dict[str, str | int]]:
+    def _analyse_dependencies(self, modules: Mapping[str, SourceModule]) -> None:
         # TODO: how do we discover dependencies before they are opened in the workspace
-        modules = parse_result.ordered_modules
         path_dependencies = defaultdict[Path, list[Path]](list)
-        path_algopy_symbols = dict[Path, dict[str, str | int]]()
         for module_id, module in modules.items():
-            if module.is_typeshed_file:
+            if module.path.suffix.lower() != ".py":
                 continue
-            dependencies = StableSet[str]()
-            symbols = dict[str, str | int]()
-            for import_ in module.node.imports:
-                if isinstance(import_, mypy.nodes.Import):
-                    for id_, alias in import_.ids:
-                        # modules directly imported can be added to the dependency graph
-                        dependencies.add(id_)
-                        if id_.startswith("algopy.") or id_ == "algopy":
-                            symbols[id_] = alias or id_
-                elif isinstance(import_, mypy.nodes.ImportFrom):
-                    # import from can contain modules or other symbols
-                    # update graph with any names that are in modules
-                    # also need to resolve relative imports to full namespace
-                    import_module_id = _correct_relative_import(
-                        file_id=module_id, file_path=module.path, import_=import_
-                    )
-                    dependencies.add(import_module_id)
-
-                    symbols[import_module_id] = coalesce(import_.end_line, import_.line) + 1
-                    for name, alias in import_.names:
-                        maybe_sub_module_id = f"{import_module_id}.{name}"
-                        symbols[maybe_sub_module_id] = alias or name
-                        if maybe_sub_module_id in modules:
-                            dependencies.add(maybe_sub_module_id)
-                elif isinstance(import_, mypy.nodes.ImportAll):
-                    import_module_id = _correct_relative_import(
-                        file_id=module_id, file_path=module.path, import_=import_
-                    )
-                    symbols[import_module_id] = ""
-                    # include any known submodules as a dependency
-                    sub_module_pattern = re.compile(f"{import_module_id}\\.\\w+$")
-                    for maybe_sub_module_id in modules:
-                        if sub_module_pattern.match(maybe_sub_module_id):
-                            dependencies.add(maybe_sub_module_id)
-            # convert module ids to paths
-            module_path = module.path
             non_stub_dependencies = [
-                dep
-                for dep in dependencies
-                if dep in modules
-                and not modules[dep].is_typeshed_file
-                and not modules[dep].node.is_stub
+                dep.path
+                for dep_id in sorted(module.dependencies)
+                if (dep := modules.get(dep_id)) is not None and dep.path.suffix.lower() == ".py"
             ]
             if non_stub_dependencies:
-                logger.debug(f"{module_id} depends on ({', '.join(non_stub_dependencies)})")
-            for dependency in non_stub_dependencies:
-                dependency_path = modules[dependency].path
-                path_dependencies[dependency_path].append(module_path)
-            if symbols:
-                path_algopy_symbols[module_path] = symbols
+                logger.debug(
+                    f"{module_id} depends on ({', '.join(map(str, non_stub_dependencies))})"
+                )
+            for dependency_path in non_stub_dependencies:
+                path_dependencies[dependency_path].append(module.path)
         self._dependencies.update(path_dependencies)
-        return path_algopy_symbols
 
     def _get_algopy_related_files(self, uris: Sequence[str]) -> Sequence[Path]:
         """
@@ -290,28 +252,179 @@ class CodeAnalyser:
             yield from self._get_dependencies(dependency)
 
 
-def _correct_relative_import(
-    *, file_id: str, file_path: Path, import_: mypy.nodes.ImportFrom | mypy.nodes.ImportAll
-) -> str:
-    """Function to correct for relative imports."""
-    if import_.relative:
-        rel = import_.relative
-        assert rel > 0
-        if file_path.stem == "__init__":
-            rel -= 1
-        if rel != 0:
-            file_id = ".".join(file_id.split(".")[:-rel])
+@attrs.frozen
+class _AlgopySymbolResolver:
+    _lines: Sequence[str]
+    _symbols: dict[str, str | int]
+    _import_block_end: int
 
-        if import_.id:
-            new_id = file_id + "." + import_.id
+    @classmethod
+    def create(cls, module: SourceModule) -> typing.Self:
+        parent_modules = dict[str, int]()
+        module_symbols = dict[str, str]()
+        for name, sym in module.node.names.items():
+            fullname = sym.fullname or ""
+            root_module, _, _ = fullname.partition(".")
+            if root_module != "algopy":
+                continue
+            module_symbols[fullname] = name
+            if sym.node is None or fullname == "algopy":
+                continue
+            parent_module, _, _ = fullname.rpartition(".")
+            parent_module = _friendly_algopy(parent_module)
+            stmt_end_line = coalesce(sym.node.end_line, sym.node.line)
+            assert stmt_end_line > 0, f"expected valid stmt line for {sym}"
+            insert_line = stmt_end_line + 1
+            try:
+                parent_modules[parent_module] = max(parent_modules[parent_module], insert_line)
+            except KeyError:
+                parent_modules[parent_module] = insert_line
+
+        import_block_end = max(
+            (
+                coalesce(i.end_line, i.line) + 1
+                for i in module.node.imports
+                if i.is_top_level and not i.is_mypy_only and not i.is_unreachable
+            ),
+            default=1,
+        )
+        assert module.lines is not None, f"missing source for {module.name}"
+        return cls(
+            lines=module.lines,
+            symbols={
+                **parent_modules,
+                **module_symbols,
+            },
+            import_block_end=import_block_end,
+        )
+
+    def fix_to_code_action(self, uri: str, fix: code_fixes.CodeFix) -> lsp.CodeAction:
+        title = ""
+        edits = list[lsp.TextEdit | None]()
+        loc = _source_location_to_range(fix.location)
+        match fix.edit:
+            case code_fixes.WrapWithSymbol(symbol=symbol):
+                title = f"Use {_friendly_algopy(symbol)}"
+                symbol_alias, maybe_add_import = self._resolve_symbol(symbol)
+                edits = [
+                    maybe_add_import,
+                    lsp.TextEdit(range=lsp.Range(loc.end, loc.end), new_text=")"),
+                    lsp.TextEdit(
+                        range=lsp.Range(loc.start, loc.start), new_text=f"{symbol_alias}("
+                    ),
+                ]
+            case code_fixes.ReplaceWithSymbol(symbol=symbol):
+                title = f"Use {_friendly_algopy(symbol)}"
+
+                symbol_alias, maybe_add_import = self._resolve_symbol(symbol)
+                edits = [maybe_add_import, lsp.TextEdit(range=loc, new_text=symbol_alias)]
+            case code_fixes.DecorateFunction(symbol=symbol):
+                title = f"Use @{_friendly_algopy(symbol)}"
+
+                symbol_alias, maybe_add_import = self._resolve_symbol(symbol)
+                func_line = loc.start.line
+                func_column = loc.start.character
+                insert_line = func_line - 1
+                insert_dec_pos = lsp.Position(line=insert_line, character=0)
+                insert_dec_loc = lsp.Range(insert_dec_pos, insert_dec_pos)
+
+                # use the same whitespace as function for consistent indentation
+                whitespace = self._lines[func_line][:func_column]
+                edits = [
+                    maybe_add_import,
+                    lsp.TextEdit(
+                        range=insert_dec_loc,
+                        new_text=f"\n{whitespace}@{symbol_alias}",
+                    ),
+                ]
+            case _:
+                typing.assert_never(fix.edit)
+        return lsp.CodeAction(
+            title=title,
+            kind=lsp.CodeActionKind.QuickFix,
+            edit=lsp.WorkspaceEdit(
+                changes={
+                    uri: list(filter(None, edits)),
+                }
+            ),
+        )
+
+    def _resolve_symbol(self, full_name: str) -> tuple[str, lsp.TextEdit | None]:
+        """
+        Give the algopy import symbols in a document, return the appropriate alias for a
+        fully qualified symbol in that document, and optionally an edit to add an import stmt
+        """
+
+        # if full symbol has an alias return that
+        try:
+            import_symbol = self._symbols[full_name]
+        except KeyError:
+            pass
         else:
-            new_id = file_id
+            assert isinstance(import_symbol, str), "expected a symbol"
+            return import_symbol, None
 
-        logger.debug(f"resolved relative import {import_} to {new_id}")
-        return new_id
-    else:
-        import_module_id = import_.id
-    return import_module_id
+        # use the friendly name of the symbol so we don't attempt to use a private module
+        full_name = _friendly_algopy(full_name)
+        symbol_module, _, symbol = full_name.rpartition(".")
+
+        module = self._symbols.get(symbol_module, self._import_block_end)
+        match module:
+            case str():
+                # if module itself was imported then reference symbol relative to that
+                return f"{module}.{symbol}", None
+            case int(line):
+                # if just siblings of the parent module were imported then use symbol
+                # and also add an edit to add the appropriate import importing from the same module
+                line_pos = lsp.Position(line=line - 1, character=0)
+                insert_at = lsp.Range(start=line_pos, end=line_pos)
+                edit = lsp.TextEdit(
+                    new_text=f"from {symbol_module} import {symbol}\n", range=insert_at
+                )
+                return symbol, edit
+            case _:
+                typing.assert_never(module)
+
+
+_ALGOPY_FRIENDLY_SUB = re.compile(r"algopy\._[^.]+")
+
+
+def _friendly_algopy(symbol: str) -> str:
+    # algopy._private.foo => algopy.foo
+    return _ALGOPY_FRIENDLY_SUB.sub("algopy", symbol)
+
+
+def _log_to_diagnostic(log: Log) -> lsp.Diagnostic:
+    assert log.location, "expected non none locations when converting to a diagnostic"
+    return lsp.Diagnostic(
+        message=log.message,
+        severity=_log_level_to_severity(log.level),
+        range=_source_location_to_range(log.location),
+        source="puyapy",
+    )
+
+
+def _log_level_to_severity(log_level: LogLevel) -> lsp.DiagnosticSeverity:
+    if log_level == LogLevel.error:
+        return lsp.DiagnosticSeverity.Error
+    if log_level == LogLevel.warning:
+        return lsp.DiagnosticSeverity.Warning
+    return lsp.DiagnosticSeverity.Information
+
+
+def _source_location_to_range(loc: SourceLocation) -> lsp.Range:
+    # positions lines are 0-indexed while SourceLocation are 1-indexed
+    line = loc.line - 1
+    column = loc.column or 0
+    end_line = loc.end_line - 1
+    end_column = loc.end_column
+    if end_column is None:
+        end_line += 1
+        end_column = 0
+    return lsp.Range(
+        start=lsp.Position(line=line, character=column),
+        end=lsp.Position(line=end_line, character=end_column),
+    )
 
 
 def _is_puyapy_compatible_src(src: str) -> bool | None:
@@ -374,12 +487,12 @@ def _uri_to_path(uri: str) -> Path | None:
     return Path(value)
 
 
-_CODE_FIXES = contextvars.ContextVar[list[CodeFix]]("_CODE_FIXES")
+_CODE_FIXES = contextvars.ContextVar[list[code_fixes.CodeFix]]("_CODE_FIXES")
 
 
 @contextlib.contextmanager
-def _code_fix_context() -> Iterator[list[CodeFix]]:
-    fixes = list[CodeFix]()
+def _code_fix_context() -> Iterator[list[code_fixes.CodeFix]]:
+    fixes = list[code_fixes.CodeFix]()
     token = _CODE_FIXES.set(fixes)
     try:
         yield fixes
@@ -387,5 +500,5 @@ def _code_fix_context() -> Iterator[list[CodeFix]]:
         _CODE_FIXES.reset(token)
 
 
-def get_code_fix_context() -> list[CodeFix]:
+def get_code_fix_context() -> list[code_fixes.CodeFix]:
     return _CODE_FIXES.get()
