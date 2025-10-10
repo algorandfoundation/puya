b155e17c1f4dcdf1f84a471594fdfd213ca32a8c
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-10 14:35:34 +0800

    feat: send log output to language client

--- src/puyapy/lsp/log.py
+++ src/puyapy/lsp/log.py
@@ -3,6 +3,8 @@ from collections.abc import Sequence
 
 import attrs
 import structlog
+from lsprotocol import types as lsp
+from pygls.lsp.server import LanguageServer
 
 from puya import log
 from puya.parse import SourceLocation
@@ -15,24 +17,32 @@ def configure_logging(
     *,
     min_log_level: log.LogLevel,
     file: typing.TextIO,
-) -> None:
+) -> "LogToClient":
+    """
+    Configures logging for the language server, which happens before the language server is
+    available, so also returns an interface that can be used to provide language server instance
+    and configured log level for client
+    """
     log.configure_stdio()
 
+    log_to_client = LogToClient()
     processors: list[structlog.typing.Processor] = [
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
         structlog.processors.StackInfoRenderer(),
         _capture_fixes,  # capture fixes before filtering output to lsp log messages
         _filter_loggers_to_lsp,  # dont output log output outside puyapy.lsp namespace
+        log_to_client,
+        log.FilterByLogLevel(min_log_level),  # min_log_level from CLI is just for stdio output
         log.PuyaConsoleRender(colors=False, base_path=str(get_cwd())),
     ]
-    processors.insert(0, log.FilterByLogLevel(min_log_level))
     structlog.configure(
         processors=processors,
         context_class=dict,
         logger_factory=_NamedPrintLoggerFactory(file=file),
         cache_logger_on_first_use=True,
     )
+    return log_to_client
 
 
 @attrs.frozen
@@ -77,3 +87,34 @@ def _filter_loggers_to_lsp(
     if not logger.name.startswith("puyapy.lsp"):
         raise structlog.DropEvent
     return event_dict
+
+
+_LOG_LEVEL_TO_MESSAGE_TYPE = {
+    log.LogLevel.debug.name: lsp.MessageType.Debug,
+    log.LogLevel.info.name: lsp.MessageType.Info,
+    log.LogLevel.warning.name: lsp.MessageType.Warning,
+    log.LogLevel.error.name: lsp.MessageType.Error,
+    log.LogLevel.critical.name: lsp.MessageType.Error,
+}
+
+
+@attrs.define
+class LogToClient:
+    server: LanguageServer | None = None
+    log_level: lsp.MessageType = lsp.MessageType.Info
+
+    def __call__(
+        self,
+        _logger: _NamedPrintLogger,
+        _name: str,
+        event_dict: structlog.typing.EventDict,
+    ) -> structlog.typing.EventDict:
+        if self.server is not None:
+            level = event_dict["level"]
+            message_type = _LOG_LEVEL_TO_MESSAGE_TYPE[level]
+            if message_type <= self.log_level:
+                message = event_dict["event"]
+                self.server.window_log_message(
+                    lsp.LogMessageParams(type=message_type, message=message)
+                )
+        return event_dict
