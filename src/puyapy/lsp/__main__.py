7eec016e0275b5ba05a405cbf09ad3ec03284889
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-10 14:33:05 +0800

    refactor: improve help output for puyapy-ls

--- src/puyapy/lsp/__main__.py
+++ src/puyapy/lsp/__main__.py
@@ -15,22 +15,26 @@ def main(
     stdio: bool = True,
     host: str = "localhost",
     port: int = 8888,
-    log_level: log.LogLevel = log.LogLevel.info,
+    log_level: log.LogLevel = log.LogLevel.error,
+    # defaults to error since most of the time any logs will be displayed via the language client
+    # based on the users IDE setting
 ) -> None:
     """
     Starts the puyapy language server
 
     Parameters:
-        stdio (bool): Use stdio
+        stdio (bool): Use stdio for communication otherwise hostname and socket
         host (str): Specifies the hostname
         port (int): Specifies the port number
-        log_level: (LogLevel): Minimum log level to output
+        log_level: (LogLevel): Minimum log level to output to stdio
     """
     # logging to stdout interferes with stdio protocol, so log to stderr instead
     log_file = sys.stderr if stdio else sys.stdout
     configure_logging(min_log_level=log_level, file=log_file)
     server = create_server()
     if stdio:
+        # helpful tip for users that run puyapy manually
+        print("Language server started using stdio, Ctrl+D to exit", file=log_file)
         server.start_io()
     else:
         server.start_tcp(host, port)
