56f98db4d5cc6491a6a80e447af9defbb82a790c
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-17 23:17:12 +0800

    build: algorand-python 2.10

--- src/puyapy/parse.py
+++ src/puyapy/parse.py
@@ -28,7 +28,7 @@ logger = log.get_logger(__name__)
 
 # this should contain the lowest version number that this compiler does NOT support
 # i.e. the next minor version after what is defined in stubs/pyproject.toml:tool.poetry.version
-MAX_SUPPORTED_ALGOPY_VERSION_EX = version.parse("2.10.0")
+MAX_SUPPORTED_ALGOPY_VERSION_EX = version.parse("2.11.0")
 MIN_SUPPORTED_ALGOPY_VERSION = version.parse(f"{MAX_SUPPORTED_ALGOPY_VERSION_EX.major}.0.0")
 
 _MYPY_SEVERITY_TO_LOG_LEVEL = {
