801d5b5078a3023ab4f6781a8ba0f44dd30e2cb0
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-10-18 12:18:01 +0800

    test: validate contract

--- tests/test_arc32.py
+++ tests/test_arc32.py
@@ -1,7 +1,9 @@
 import base64
+import contextlib
 import hashlib
 import math
 import random
+import re
 from collections.abc import Sequence
 from pathlib import Path
 
@@ -2168,6 +2170,177 @@ def test_intrinsic_optimizations(
 
     response = app_client.call("sha256")
     assert bytes(response.return_value) == hashlib.sha256(b"Hello World").digest()
+
+
+@pytest.fixture(scope="session")
+def arc4_validation_client(
+    algod_client: AlgodClient, account: algokit_utils.Account
+) -> ApplicationClient:
+    app_spec = algokit_utils.ApplicationSpecification.from_json(
+        compile_arc32(TEST_CASES_DIR / "arc4_validation")
+    )
+    app_client = algokit_utils.ApplicationClient(algod_client, app_spec, signer=account)
+    app_client.create()
+    return app_client
+
+
+def _invalid_size(type_name: str) -> str:
+    return f"invalid number of bytes for {type_name}"
+
+
+def _invalid_arr_size(element_type_name: str, size: int | None = None) -> str:
+    if size is None:
+        return _invalid_size(f"arc4.dynamic_array<{element_type_name}>")
+    else:
+        return _invalid_size(f"arc4.static_array<{element_type_name}, {size}>")
+
+
+_VALID_DYNAMIC_STRUCT_BYTES = b"\x00" * 9 + b"\x00\x0b\x00\x00"
+_INVALID_DYNAMIC_STRUCT_BYTES = b"\x00" * 9 + b"\x00\x0b\x00\x01"
+_STATIC_STRUCT = "test_cases.arc4_validation.contract.ARC4StaticStruct"
+_DYNAMIC_STRUCT = "test_cases.arc4_validation.contract.ARC4DynamicStruct"
+
+
+@pytest.mark.parametrize(
+    ("type_name", "size_or_bytes_value", "expected_error"),
+    [
+        ("uint64", 8, None),
+        ("uint64", 7, _invalid_size("arc4.uint64")),
+        ("ufixed64", 8, None),
+        ("ufixed64", 7, _invalid_size("arc4.ufixed64x2")),
+        ("uint8", 1, None),
+        ("uint8", 2, _invalid_size("arc4.uint8")),
+        ("bool", 1, None),
+        ("bool", 2, _invalid_size("arc4.bool")),
+        ("byte", 1, None),
+        ("byte", 2, _invalid_size("arc4.uint8")),
+        ("string", 2, None),
+        ("string", (4, b"\x00" * 4), None),
+        ("string", 5, _invalid_arr_size("arc4.uint8")),
+        ("bytes", 2, None),
+        ("bytes", (4, b"\x00" * 4), None),
+        ("bytes", 5, _invalid_arr_size("arc4.uint8")),
+        ("address", 32, None),
+        ("address", 0, _invalid_arr_size("arc4.uint8", 32)),
+        ("address", 33, _invalid_arr_size("arc4.uint8", 32)),
+        ("uint512", 64, None),
+        ("uint512", 63, _invalid_size("arc4.uint512")),
+        ("uint8_arr", 2, None),
+        ("uint8_arr", b"\x00\x01\x00", None),
+        ("uint8_arr", 1, "invalid array length header"),
+        ("uint8_arr", 3, _invalid_arr_size("arc4.uint8")),
+        ("uint8_arr3", 3, None),
+        ("uint8_arr3", 1, _invalid_arr_size("arc4.uint8", 3)),
+        ("uint8_arr3", 4, _invalid_arr_size("arc4.uint8", 3)),
+        ("bool_arr", b"\x00\x00", None),
+        ("bool_arr", b"\x00\x01\x00", None),
+        ("bool_arr", b"\x00\x08\x00", None),
+        ("bool_arr", b"\x00\x0a\x00\x00", None),
+        ("bool_arr", b"\x00\x01", _invalid_arr_size("arc4.bool")),
+        ("bool_arr", b"\x00\x09\x00", _invalid_arr_size("arc4.bool")),
+        ("static_struct", 9, None),
+        ("static_struct", 8, _invalid_size(_STATIC_STRUCT)),
+        ("dynamic_struct", 0, "invalid tuple encoding"),
+        ("dynamic_struct", _VALID_DYNAMIC_STRUCT_BYTES, None),
+        ("dynamic_struct", 11, "invalid tail pointer"),
+        ("dynamic_struct", _INVALID_DYNAMIC_STRUCT_BYTES, _invalid_size(_DYNAMIC_STRUCT)),
+        ("static_tuple", 9, None),
+        ("static_tuple", 8, _invalid_size("arc4.tuple<arc4.uint64,arc4.uint8>")),
+        ("dynamic_tuple", 0, "invalid tuple encoding"),
+        ("dynamic_tuple", _VALID_DYNAMIC_STRUCT_BYTES, None),
+        ("dynamic_tuple", 11, "invalid tail pointer"),
+        (
+            "dynamic_tuple",
+            _INVALID_DYNAMIC_STRUCT_BYTES,
+            _invalid_size("arc4.tuple<arc4.uint64,arc4.uint8,arc4.dynamic_array<arc4.uint8>>"),
+        ),
+        ("static_struct_arr", b"\x00\x03" + b"\x00" * 27, None),
+        ("static_struct_arr", 0, "invalid array length header"),
+        ("static_struct_arr", 1, "invalid array length header"),
+        ("static_struct_arr", 29, _invalid_arr_size(_STATIC_STRUCT)),
+        ("static_struct_arr3", 27, None),
+        ("static_struct_arr3", 26, _invalid_arr_size(_STATIC_STRUCT, 3)),
+        ("dynamic_struct_arr", 2, None),
+        (
+            "dynamic_struct_arr",
+            (
+                2,  # len
+                4,  # ptr 1
+                4 + len(_VALID_DYNAMIC_STRUCT_BYTES),  # ptr 2
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+            ),
+            None,
+        ),
+        ("dynamic_struct_arr", 0, "invalid array length header"),
+        ("dynamic_struct_arr", 1, "invalid array length header"),
+        ("dynamic_struct_arr", 29, _invalid_arr_size(_DYNAMIC_STRUCT)),
+        ("dynamic_struct_arr3", 27, "invalid tail pointer"),
+        (
+            "dynamic_struct_arr3",
+            (
+                6,
+                6,
+                6,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+            ),
+            # invalid tuple encoding because the offsets result in a 0-length read of the 1st tuple
+            "invalid tuple encoding",
+        ),
+        (
+            "dynamic_struct_arr3",
+            0,
+            "invalid array encoding",
+        ),
+        (
+            "dynamic_struct_arr3",
+            (
+                6,
+                6 + len(_VALID_DYNAMIC_STRUCT_BYTES),
+                6 + len(_VALID_DYNAMIC_STRUCT_BYTES) * 2,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+            ),
+            None,
+        ),
+        (
+            "dynamic_struct_arr3",
+            (
+                6,
+                6 + len(_VALID_DYNAMIC_STRUCT_BYTES),
+                6 + len(_VALID_DYNAMIC_STRUCT_BYTES) * 2,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _VALID_DYNAMIC_STRUCT_BYTES,
+                _INVALID_DYNAMIC_STRUCT_BYTES,
+            ),
+            _invalid_arr_size(_DYNAMIC_STRUCT, 3),
+        ),
+    ],
+)
+def test_arc4_validation(
+    arc4_validation_client: ApplicationClient,
+    type_name: str,
+    size_or_bytes_value: bytes | int | list[bytes | int],
+    expected_error: str | None,
+) -> None:
+    if isinstance(size_or_bytes_value, bytes):
+        bytes_value = size_or_bytes_value
+    elif isinstance(size_or_bytes_value, int):
+        bytes_value = b"\x00" * size_or_bytes_value
+    else:
+        bytes_value = b"".join(
+            b if isinstance(b, bytes) else b.to_bytes(length=2) for b in size_or_bytes_value
+        )
+    ctx = (
+        pytest.raises(LogicError, match=re.escape(expected_error) + r"[^\n]*<-- Error")
+        if expected_error is not None
+        else contextlib.nullcontext()
+    )
+    with ctx:
+        arc4_validation_client.call(f"validate_{type_name}", value=bytes_value)
 
 
 def _get_immutable_array_app(
