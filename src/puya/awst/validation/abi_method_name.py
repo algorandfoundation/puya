70a68a01dd1b2068007d01ce3aac54d2d1f77799
Bobby Lat <bobby.lat@makerx.com.au>
2025-10-21 17:28:18 +0800

    refactor: update validator to fit into visitor pattern

--- src/puya/awst/validation/abi_method_name.py
+++ src/puya/awst/validation/abi_method_name.py
@@ -2,7 +2,7 @@ import re
 
 from puya import log
 from puya.awst import nodes as awst_nodes
-from puya.awst.function_traverser import FunctionTraverser
+from puya.awst.awst_traverser import AWSTTraverser
 
 logger = log.get_logger(__name__)
 
@@ -10,21 +10,24 @@ logger = log.get_logger(__name__)
 _VALID_NAME_PATTERN = re.compile("^[_A-Za-z][A-Za-z0-9_]*$")
 
 
-class AbiMethodNameValidator(FunctionTraverser):
+class AbiMethodNameValidator(AWSTTraverser):
+    """
+    Validates that ABI method names conform to the ARC-4 specification.
+    """
+
     @classmethod
     def validate(cls, module: awst_nodes.AWST) -> None:
         for module_statement in module:
-            if isinstance(module_statement, awst_nodes.Contract):
-                for method in module_statement.methods:
-                    cls(method)
+            validator = cls()
+            module_statement.accept(validator)
 
-    def __init__(self, method: awst_nodes.ContractMethod) -> None:
-        if method.arc4_method_config is not None and isinstance(
-            method.arc4_method_config, awst_nodes.ARC4ABIMethodConfig
+    def visit_contract_method(self, statement: awst_nodes.ContractMethod) -> None:
+        if statement.arc4_method_config is not None and isinstance(
+            statement.arc4_method_config, awst_nodes.ARC4ABIMethodConfig
         ):
-            method_name = method.arc4_method_config.name or method.member_name
+            method_name = statement.arc4_method_config.name
             if not _VALID_NAME_PATTERN.match(method_name):
                 logger.warning(
-                    f"method name '{method_name}' does not conform to ARC4 spec",
-                    location=method.source_location,
+                    f"method name '{method_name}' does not conform to ARC-4 spec",
+                    location=statement.source_location,
                 )
