acc21e9bde2d741417e902912fb4a97a31166be4
Adam Chidlow <achidlow@users.noreply.github.com>
2025-11-06 15:31:31 +0800

    refactor: improve the slot elimination pass - reduces the amount of work being done by tracking where updates need to be made and doing them directly without relying on other optimisation passes - uses an allow-list rather than a deny-list approach for determining which ops are non-aliasing - eliminates local slots that are only written to and never read, this improves code generation in some test cases but is unlikely to occur in user code

--- src/puya/ir/optimize/slot_elimination.py
+++ src/puya/ir/optimize/slot_elimination.py
@@ -1,12 +1,11 @@
 import itertools
+from collections import defaultdict
 
 from puya import log
 from puya.context import ArtifactCompileContext
 from puya.ir import models
 from puya.ir.avm_ops import AVMOp
 from puya.ir.models import SlotAllocationStrategy
-from puya.ir.optimize.constant_propagation import constant_replacer
-from puya.ir.optimize.dead_code_elimination import remove_unused_variables
 from puya.ir.to_text_visitor import render_program
 from puya.ir.visitor import IRTraverser
 from puya.program_refs import ContractReference, LogicSigReference
@@ -21,9 +20,8 @@ def slot_elimination(
     ref: ContractReference | LogicSigReference,
 ) -> None:
     logger.debug(f"removing local static slots in {program.kind} program of {ref}")
-    # these properties need to be considered across the entire program
-    dynamic_slots = 0
-    non_local_slots = 0
+    # needs to be considered across the entire program
+    dynamic_slot_allocation_required = False
     for sub in program.all_subroutines:
         visitor = _SlotGatherer()
         visitor.visit_all_blocks(sub.body)
@@ -35,30 +33,37 @@ def slot_elimination(
 
         local_slots = itertools.count()
         modified = False
-        for slot_reg, (ass, new_slot) in visitor.new_slot_registers.items():
-            if slot_reg in visitor.dynamic_registers:
-                dynamic_slots += 1
-                continue
-            if slot_reg in visitor.non_local_registers:
-                non_local_slots += 1
-                continue
-            modified = True
-            logger.debug(
-                f"eliminating local static slot assigned to {slot_reg.local_id}",
-                location=slot_reg.source_location,
-            )
-            ass.source = models.SlotConstant(
-                value=next(local_slots),
-                ir_type=new_slot.ir_type,
-                source_location=new_slot.source_location,
-            )
+        for slot_reg, (block, ass, new_slot) in visitor.new_slot_registers.items():
+            if slot_reg in visitor.non_slot_register_reads:
+                dynamic_slot_allocation_required = True
+            else:
+                block.ops.remove(ass)
+                modified = True
+                reads = visitor.slot_reads[slot_reg]
+                if reads:
+                    logger.debug(
+                        f"eliminating local static slot assigned to {slot_reg.local_id}",
+                        location=slot_reg.source_location,
+                    )
+                    slot_const = models.SlotConstant(
+                        value=next(local_slots),
+                        ir_type=new_slot.ir_type,
+                        source_location=new_slot.source_location,
+                    )
+                    for read in reads:
+                        read.slot = slot_const
+                    for _, write in visitor.slot_writes[slot_reg]:
+                        write.slot = slot_const
+                else:
+                    logger.debug(
+                        f"eliminating unused local slot assigned to {slot_reg.local_id}",
+                        location=slot_reg.source_location,
+                    )
+                    for write_block, write in visitor.slot_writes[slot_reg]:
+                        write_block.ops.remove(write)
         if modified:
-            # replace any slot constants referenced by other ops
-            constant_replacer(ctx, sub)
-            # remove now unused assignments
-            remove_unused_variables(ctx, sub)
             sub.validate_with_ssa()
-    if dynamic_slots or non_local_slots:
+    if dynamic_slot_allocation_required:
         logger.debug("Using dynamic slot allocation strategy")
         program.slot_allocation.strategy = SlotAllocationStrategy.dynamic
     else:
@@ -70,47 +75,52 @@ def slot_elimination(
 
 
 class _SlotGatherer(IRTraverser):
+    active_block: models.BasicBlock
+
     def __init__(self) -> None:
         self.intrinsic_slots = set[int]()
         # Registers containing any assigned new_slot:
-        self.new_slot_registers = dict[models.Register, tuple[models.Assignment, models.NewSlot]]()
-        # Any registers that are dynamic:
-        self.dynamic_registers = set[models.Register]()
-        # Any registers that are not local (passed to other subroutines):
-        self.non_local_registers = set[models.Register]()
+        self.new_slot_registers = dict[
+            models.Register, tuple[models.BasicBlock, models.Assignment, models.NewSlot]
+        ]()
+        # Any registers that have read usages outside of a WriteSlot or ReadSlot
+        self.non_slot_register_reads = set[models.Register]()
+        self.slot_reads = defaultdict[models.Value, list[models.ReadSlot]](list)
+        self.slot_writes = defaultdict[
+            models.Value, list[tuple[models.BasicBlock, models.WriteSlot]]
+        ](list)
+
+    def visit_block(self, block: models.BasicBlock) -> None:
+        self.active_block = block
+        super().visit_block(block)
 
     def visit_assignment(self, ass: models.Assignment) -> None:
-        super().visit_assignment(ass)
         # capture new slots
         if isinstance(ass.source, models.NewSlot):
             (slot_register,) = ass.targets
-            self.new_slot_registers[slot_register] = ass, ass.source
-        # excluded copied slots
-        # if copy propagation has been done it will potentially allow more slots to be removed
-        elif isinstance(ass.source, models.Register):
-            self.dynamic_registers.add(ass.source)
+            self.new_slot_registers[slot_register] = self.active_block, ass, ass.source
+        else:
+            ass.source.accept(self)
 
-    def visit_phi_argument(self, arg: models.PhiArgument) -> None:
-        # registers involved in phis are considered dynamic
-        self.dynamic_registers.add(arg.value)
+    def visit_read_slot(self, read: models.ReadSlot) -> None:
+        # don't visit read.slot
+        self.slot_reads[read.slot].append(read)
 
-    def visit_subroutine_return(self, retsub: models.SubroutineReturn) -> None:
-        super().visit_subroutine_return(retsub)
-        for value in retsub.result:
-            # registers that leave a subroutine are non local
-            if isinstance(value, models.Register):
-                self.non_local_registers.add(value)
+    def visit_write_slot(self, write: models.WriteSlot) -> None:
+        write.value.accept(self)
+        # don't visit write.slot
+        self.slot_writes[write.slot].append((self.active_block, write))
 
-    def visit_invoke_subroutine(self, callsub: models.InvokeSubroutine) -> None:
-        super().visit_invoke_subroutine(callsub)
-        # registers passed to other subroutines are non local
-        for arg in callsub.args:
-            if isinstance(arg, models.Register):
-                self.non_local_registers.add(arg)
+    def visit_phi(self, phi: models.Phi) -> None:
+        for arg in phi.args:
+            self.visit_phi_argument(arg)
+
+    def visit_register(self, reg: models.Register) -> None:
+        self.non_slot_register_reads.add(reg)
 
     def visit_intrinsic_op(self, intrinsic: models.Intrinsic) -> None:
         super().visit_intrinsic_op(intrinsic)
         if intrinsic.op in (AVMOp.load, AVMOp.store):
-            slot = intrinsic.immediates[0]
+            (slot,) = intrinsic.immediates
             assert isinstance(slot, int)
             self.intrinsic_slots.add(slot)
