5d37cecca6ef6489ca6bd3e2ba26ce2e3232111b
Bobby Lat <bobby.lat@makerx.com.au>
2025-11-21 16:31:26 +0800

    feat: add `algopy.Contract` as an alias of `algopy.arc4.ARC4Contract`

--- examples/hello_world_arc4/contract.py
+++ examples/hello_world_arc4/contract.py
@@ -1,4 +1,4 @@
-from algopy import ARC4Contract, String, arc4
+from algopy import Contract, String, arc4
 
 # Note: this contract is also used in the Puya AlgoKit template. So any breaking changes
 # that require fixing this contract should also be made there
@@ -6,7 +6,7 @@ from algopy import ARC4Contract, String, arc4
 # https://github.com/algorandfoundation/algokit-puya-template/blob/main/template_content/.algokit/generators/create_contract/smart_contracts/%7B%25%20raw%20%25%7D%7B%7B%20contract_name%20%7D%7D%7B%25%20endraw%20%25%7D/contract.py.j2
 
 
-class HelloWorldContract(ARC4Contract):
+class HelloWorldContract(Contract):
     @arc4.abimethod
     def hello(self, name: String) -> String:
         return "Hello, " + name
