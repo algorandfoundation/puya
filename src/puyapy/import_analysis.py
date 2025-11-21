826859ad3a42e85bd15bc1e3a49f2ef4c20accdd
Adam Chidlow <achidlow@users.noreply.github.com>
2025-11-21 15:29:52 +0800

    connect import / dependency analysis to FAST model

--- src/puyapy/import_analysis.py
+++ src/puyapy/import_analysis.py
@@ -1,12 +1,12 @@
-import ast
 import typing
 from collections.abc import Iterator
 from pathlib import Path
 
 import attrs
 
-from puya.errors import CodeError
 from puya.parse import SourceLocation
+from puyapy.fast import nodes as fast_nodes
+from puyapy.fast.traversers import StatementTraverser
 from puyapy.find_sources import ResolvedSource
 from puyapy.modulefinder import FindModuleCache
 
@@ -20,114 +20,18 @@ _ALLOWED_STDLIB_STUBS: typing.Final = frozenset(
 )
 
 
-@attrs.frozen
-class ImportAs:
-    loc: SourceLocation
-    name: str
-    as_name: str | None
-
-
-@attrs.frozen
-class ModuleImport:
-    loc: SourceLocation
-    names: list[ImportAs] = attrs.field(validator=attrs.validators.min_len(1))
-
-
-@attrs.frozen
-class FromImport:
-    loc: SourceLocation
-    module: str
-    names: list[ImportAs] | None = attrs.field(
-        validator=attrs.validators.optional(attrs.validators.min_len(1))
-    )
-    """if None, then import all (ie star import)"""
-
-
-AnyImport = ModuleImport | FromImport
-
-
 @attrs.define
-class _ImportCollector(ast.NodeVisitor):
-    source: ResolvedSource
-    module_imports: list[ModuleImport] = attrs.field(factory=list, init=False)
-    from_imports: list[FromImport] = attrs.field(factory=list, init=False)
+class _ImportCollector(StatementTraverser):
+    module_imports: list[fast_nodes.ModuleImport] = attrs.field(factory=list, init=False)
+    from_imports: list[fast_nodes.FromImport] = attrs.field(factory=list, init=False)
 
     @typing.override
-    def visit_Import(self, node: ast.Import) -> None:
-        names = [
-            ImportAs(
-                name=alias.name,
-                as_name=alias.asname,
-                loc=self._loc(alias),
-            )
-            for alias in node.names
-        ]
-        self.module_imports.append(
-            ModuleImport(
-                names=names,
-                loc=self._loc(node),
-            )
-        )
+    def visit_module_import(self, module_import: fast_nodes.ModuleImport) -> None:
+        self.module_imports.append(module_import)
 
     @typing.override
-    def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
-        match node.names:
-            case [ast.alias("*", None)]:
-                names = None
-            case _:
-                names = list[ImportAs](
-                    ImportAs(
-                        name=alias.name,
-                        as_name=alias.asname,
-                        loc=self._loc(alias),
-                    )
-                    for alias in node.names
-                )
-
-        node_loc = self._loc(node)
-        if not node.level:
-            assert node.module
-            module = node.module
-        else:
-            module = self._correct_relative_import(
-                module=node.module, level=node.level, loc=node_loc
-            )
-
-        self.from_imports.append(
-            FromImport(
-                module=module,
-                names=names,
-                loc=node_loc,
-            )
-        )
-
-    def _loc(self, node: ast.expr | ast.stmt | ast.alias) -> SourceLocation:
-        return _ast_source_location(node, self.source.path)
-
-    def _correct_relative_import(self, module: str | None, level: int, loc: SourceLocation) -> str:
-        """Function to correct for relative imports."""
-        file_id = self.source.module
-        rel = level
-        assert level > 0
-        if self.source.path.stem == "__init__":
-            rel -= 1
-        if rel != 0:
-            file_id = ".".join(file_id.split(".")[:-rel])
-
-        if module:
-            new_id = file_id + "." + module
-        else:
-            new_id = file_id
-
-        if not new_id:
-            # TODO: log and exit at end of discovery
-            raise CodeError("no parent module, cannot perform relative import", loc)
-
-        return new_id
-
-
-def _ast_source_location(node: ast.expr | ast.stmt | ast.alias, file: Path) -> SourceLocation:
-    return SourceLocation(file=file, line=node.lineno)
+    def visit_from_import(self, from_import: fast_nodes.FromImport) -> None:
+        self.from_imports.append(from_import)
 
 
 @attrs.frozen
@@ -140,11 +44,11 @@ class Dependency:
 
 def resolve_import_dependencies(
     source: ResolvedSource,
-    tree: ast.Module,
+    tree: fast_nodes.Module,
     fmc: FindModuleCache,
 ) -> list[Dependency]:
-    visitor = _ImportCollector(source)
-    visitor.visit(tree)
+    visitor = _ImportCollector()
+    visitor.visit_module(tree)
 
     dependencies = []
     import_base_dir = source.base_dir or source.path.parent
@@ -154,17 +58,26 @@ def resolve_import_dependencies(
                 continue
             if alias.name.partition(".")[0] == "algopy":
                 continue  # TODO: expose as dependency somehow?
-            path_str = fmc.find_module(alias.name, alias.loc, import_base_dir=import_base_dir)
+            path_str = fmc.find_module(
+                alias.name, alias.source_location, import_base_dir=import_base_dir
+            )
             if path_str is None:
                 continue
 
             path = Path(path_str)
-            dependencies.append(Dependency(alias.name, path, alias.loc, implicit=False))
+            dependencies.append(
+                Dependency(alias.name, path, alias.source_location, implicit=False)
+            )
             for ancestor_module_id, ancestor_init_path in _expand_init_dependencies(
                 alias.name, path
             ):
                 dependencies.append(
-                    Dependency(ancestor_module_id, ancestor_init_path, alias.loc, implicit=True)
+                    Dependency(
+                        ancestor_module_id,
+                        ancestor_init_path,
+                        alias.source_location,
+                        implicit=True,
+                    )
                 )
     for from_imp in visitor.from_imports:
         module_id = from_imp.module
@@ -177,7 +90,9 @@ def resolve_import_dependencies(
         # an implicit namespace package without an __init__.py, in that case however a from-import
         # behaves differently depending on what has already been imported, which adds significant
         # complexity here - so just error altogether for now.
-        path_str = fmc.find_module(module_id, from_imp.loc, import_base_dir=import_base_dir)
+        path_str = fmc.find_module(
+            module_id, from_imp.source_location, import_base_dir=import_base_dir
+        )
         if path_str is None:
             continue
 
@@ -208,15 +123,19 @@ def resolve_import_dependencies(
                 if maybe_path.is_file():
                     submodule_id = ".".join((module_id, alias.name))
                     sub_deps.append(
-                        Dependency(submodule_id, maybe_path, alias.loc, implicit=False)
+                        Dependency(submodule_id, maybe_path, alias.source_location, implicit=False)
                     )
                 else:
                     any_unresolved = True
-        dependencies.append(Dependency(module_id, path, from_imp.loc, implicit=not any_unresolved))
+        dependencies.append(
+            Dependency(module_id, path, from_imp.source_location, implicit=not any_unresolved)
+        )
         dependencies.extend(sub_deps)
         for ancestor_module_id, ancestor_init_path in _expand_init_dependencies(module_id, path):
             dependencies.append(
-                Dependency(ancestor_module_id, ancestor_init_path, from_imp.loc, implicit=True)
+                Dependency(
+                    ancestor_module_id, ancestor_init_path, from_imp.source_location, implicit=True
+                )
             )
 
     for ancestor_module_id, ancestor_init_path in _expand_init_dependencies(
