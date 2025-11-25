5c8762563d0df3f2f2dd5bb9209587bee3beaae2
Adam Chidlow <achidlow@users.noreply.github.com>
2025-11-25 12:52:11 +0800

    add testing for TYPE_CHECKING support with circular dependencies

--- test_cases/circular_dependency/a.py
+++ test_cases/circular_dependency/a.py
@@ -5,6 +5,10 @@ from algopy import Application, String, arc4
 from test_cases.circular_dependency.b import B, PingPongBase
 
 
+class MyInnerStruct(arc4.Struct):
+    value: arc4.UInt64
+
+
 class A(PingPongBase):
     @typing.override
     @arc4.abimethod
