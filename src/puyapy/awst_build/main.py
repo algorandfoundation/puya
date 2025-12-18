27963854582e306c962acb992023da916235961b
Adam Chidlow <achidlow@users.noreply.github.com>
2025-12-18 13:51:29 +0800

    fix TODO

--- src/puyapy/awst_build/main.py
+++ src/puyapy/awst_build/main.py
@@ -33,13 +33,8 @@ def _transform_ast(
         module_rel_path = make_path_relative_to_cwd(src.path)
         logger.debug(f"Discovered user module {module_name} at {module_rel_path}")
         module_ctx = ctx.for_module(src.name, src.path)
-        if src.fast is None:
-            logger.debug(
-                f"skipping transformation of AST for {module_name} due to parsing failures"
-            )
-        else:
-            user_modules.append((src, ModuleASTConverter(module_ctx, src.mypy_module, src.fast)))
-            _ = ModuleFASTConverter(module_ctx, src.fast)
+        user_modules.append((src, ModuleASTConverter(module_ctx, src.mypy_module, src.fast)))
+        _ = ModuleFASTConverter(module_ctx, src.fast)
 
     compilation_set = list[ContractReference | LogicSigReference]()
     awst = list[RootNode]()
