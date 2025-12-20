93a93d229f1b6ccb14a533d6b8b58a702f9ddc68
Adam Chidlow <achidlow@users.noreply.github.com>
2025-12-20 12:25:15 +0800

    remove vendored mypy and make it a dependency instead

--- src/puyapy/__init__.py
+++
@@ -1,6 +0,0 @@
-import pathlib
-import sys
-
-_VENDOR_PATH = pathlib.Path(__file__).parent / "_vendor"
-
-sys.path.insert(0, str(_VENDOR_PATH))
--- src/puyapy/_vendor/mypy/typeshed/stdlib/encodings/__init__.pyi
+++
@@ -1,9 +0,0 @@
-from codecs import CodecInfo
-
-class CodecRegistryError(LookupError, SystemError): ...
-
-def normalize_encoding(encoding: str | bytes) -> str: ...
-def search_function(encoding: str) -> CodecInfo | None: ...
-
-# Needed for submodules
-def __getattr__(name: str): ...  # incomplete module
