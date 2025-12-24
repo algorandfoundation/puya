a71112e0e2b0520ad8eb4035f1522c995e7b0471
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-12-24 16:02:44 +0800

    test: migrate test_logic_sig

--- tests/test_logic_sig.py
+++ tests/test_logic_sig.py
@@ -1,22 +1,21 @@
 from collections.abc import Mapping
 from pathlib import Path
 
-import algokit_utils
-import algosdk
 import pytest
-from algokit_utils import TransferAssetParameters
-from algosdk.atomic_transaction_composer import (
-    AtomicTransactionComposer,
-    LogicSigTransactionSigner,
-    TransactionWithSigner,
+from algokit_common import public_key_from_address
+from algokit_utils import (
+    AddressWithSigners,
+    AlgoAmount,
+    AlgorandClient,
+    AssetTransferParams,
+    LogicSigAccount,
+    PaymentParams,
 )
-from algosdk.transaction import AssetTransferTxn, LogicSigAccount, LogicSigTransaction, PaymentTxn
-from algosdk.v2client.algod import AlgodClient
 
 from puya.compilation_artifacts import CompiledLogicSig
 from puyapy.options import PuyaPyOptions
 from tests import TEST_CASES_DIR
-from tests.utils import compile_src_from_options
+from tests.utils.compile import compile_src_from_options
 
 pytestmark = pytest.mark.localnet
 
@@ -46,113 +45,102 @@ def compile_logic_sig(
     return bytecode
 
 
-def test_logic_sig(algod_client: AlgodClient, account: algokit_utils.Account) -> None:
+def test_logic_sig(localnet: AlgorandClient, account: AddressWithSigners) -> None:
     logic_sig_prog = compile_logic_sig(
         TEST_CASES_DIR / "logic_signature" / "always_allow.py",
     )
-    logic_sig = LogicSigAccount(
-        program=logic_sig_prog,
+    logic_sig = LogicSigAccount(logic=logic_sig_prog)
+    localnet.account.set_signer(logic_sig.addr, logic_sig.signer)
+    localnet.send.payment(
+        PaymentParams(
+            sender=account.addr,
+            receiver=logic_sig.addr,
+            amount=AlgoAmount.from_algo(2),
+        )
     )
-    algokit_utils.transfer(
-        client=algod_client,
-        parameters=algokit_utils.TransferParameters(
-            from_account=account,
-            to_address=logic_sig.address(),
-            micro_algos=2_000_000,
+
+    result = localnet.send.payment(
+        PaymentParams(
+            sender=logic_sig.addr,
+            receiver=account.addr,
+            amount=AlgoAmount.from_algo(1),
+        )
+    )
+    assert result.confirmation.confirmed_round is not None
+
+    delegated_logic_sig = LogicSigAccount(logic=logic_sig_prog).delegated_account(account.addr)
+    delegated_logic_sig.sign_for_delegation(account)
+
+    localnet.account.set_signer_from_account(delegated_logic_sig)
+
+    result = localnet.send.payment(
+        PaymentParams(
+            sender=account.addr,
+            receiver=delegated_logic_sig.addr,
+            amount=AlgoAmount.from_micro_algo(500_000),
         ),
     )
-
-    paytxn = PaymentTxn(
-        sender=logic_sig.address(),
-        sp=algod_client.suggested_params(),
-        receiver=account.address,
-        amt=1_000_000,
-    )
-    signed_paytxn = LogicSigTransaction(
-        transaction=paytxn,
-        lsig=logic_sig.lsig,
-    )
-    algod_client.send_transaction(txn=signed_paytxn)
-
-    delegated_logic_sig = LogicSigAccount(program=logic_sig_prog)
-    delegated_logic_sig.sign(account.private_key)
-
-    paytxn = PaymentTxn(
-        sender=account.address,
-        sp=algod_client.suggested_params(),
-        receiver=logic_sig.address(),
-        amt=500_000,
-    )
-    signed_paytxn = algosdk.transaction.LogicSigTransaction(
-        transaction=paytxn,
-        lsig=delegated_logic_sig.lsig,
-    )
-    algod_client.send_transaction(txn=signed_paytxn)
+    assert result.confirmation.confirmed_round is not None
 
 
 @pytest.fixture
-def account_2(algod_client: AlgodClient) -> algokit_utils.Account:
-    v = algosdk.account.generate_account()
-    account = algokit_utils.Account(private_key=v[0], address=v[1])
-    algokit_utils.transfer(
-        client=algod_client,
-        parameters=algokit_utils.TransferParameters(
-            from_account=algokit_utils.get_localnet_default_account(algod_client),
-            to_address=account.address,
-            micro_algos=500_000_000,
-        ),
+def account_2(localnet: AlgorandClient, account: AddressWithSigners) -> AddressWithSigners:
+    new_account = localnet.account.random()
+    localnet.send.payment(
+        PaymentParams(
+            sender=account.addr,
+            receiver=new_account.addr,
+            amount=AlgoAmount.from_algo(500),
+        )
     )
-    return account
+    return new_account
 
 
 def test_pre_approved_sale(
-    algod_client: AlgodClient,
-    account: algokit_utils.Account,
-    account_2: algokit_utils.Account,
+    localnet: AlgorandClient,
+    account: AddressWithSigners,
+    account_2: AddressWithSigners,
     asset_a: int,
 ) -> None:
-    algokit_utils.transfer_asset(
-        client=algod_client,
-        parameters=TransferAssetParameters(
-            from_account=account_2,
-            to_address=account_2.address,
+    localnet.send.asset_transfer(
+        AssetTransferParams(
+            sender=account_2.addr,
+            receiver=account_2.addr,
             asset_id=asset_a,
             amount=0,  # Opt in
-        ),
+        )
     )
+
     logic_sig_prog = compile_logic_sig(
         TEST_CASES_DIR / "logic_signature" / "signature.py",
         template_variables={
-            "SELLER": algosdk.encoding.decode_address(account.address),
+            "SELLER": public_key_from_address(account.addr),
             "PRICE": 10_000_000,
             "ASSET_ID": asset_a,
         },
     )
-    logic_sig = LogicSigAccount(program=logic_sig_prog)
-    logic_sig.sign(account.private_key)
+    logic_sig = LogicSigAccount(logic=logic_sig_prog).delegated_account(account.addr)
+    logic_sig.sign_for_delegation(account)
 
-    # Payment for the asset
-    paytxn = PaymentTxn(
-        sender=account_2.address,
-        sp=algod_client.suggested_params(),
-        receiver=account.address,
-        amt=10_000_000,
+    localnet.account.set_signer_from_account(account_2, logic_sig)
+    result = (
+        localnet.new_group()
+        .add_payment(
+            PaymentParams(
+                sender=account_2.addr,
+                receiver=account.addr,
+                amount=AlgoAmount.from_algo(10),
+            )
+        )
+        .add_asset_transfer(
+            AssetTransferParams(
+                sender=account.addr,
+                receiver=account_2.addr,
+                amount=1,
+                asset_id=asset_a,
+            )
+        )
+        .send()
     )
 
-    # Transfer the asset
-    axfertxn = AssetTransferTxn(
-        sender=account.address,
-        sp=algod_client.suggested_params(),
-        receiver=account_2.address,
-        amt=1,
-        index=asset_a,
-    )
-
-    atc = AtomicTransactionComposer()
-
-    atc.add_transaction(TransactionWithSigner(txn=paytxn, signer=account_2.signer))
-    atc.add_transaction(
-        TransactionWithSigner(txn=axfertxn, signer=LogicSigTransactionSigner(logic_sig))
-    )
-
-    atc.execute(algod_client, wait_rounds=5)
+    assert result.confirmations[1].confirmed_round is not None
