af6701a53f2454deb97ce77cb6b6b08b5bae3d09
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2025-12-28 14:20:27 +0800

    test: migrate test_subroutine_parameter_overwrite

---
+++ tests/onchain/test_subroutine_parameter_overwrite.py
@@ -0,0 +1,30 @@
+from tests.utils import decode_logs
+from tests.utils.compile import compile_arc56_from_closure
+from tests.utils.deployer import Deployer
+
+
+def test_subroutine_parameter_overwrite(deployer: Deployer, optimization_level: int) -> None:
+    def contract() -> None:
+        from algopy import Bytes, Contract, log, op, subroutine
+
+        class Exclaimer(Contract):
+            def approval_program(self) -> bool:
+                num_args = op.Txn.num_app_args
+                assert num_args == 1, "expected one arg"
+                msg = op.Txn.application_args(0)
+                exclaimed = self.exclaim(msg)
+                log(exclaimed)
+                return True
+
+            @subroutine
+            def exclaim(self, value: Bytes) -> Bytes:
+                value = value + b"!"
+                return value
+
+            def clear_state_program(self) -> bool:
+                return True
+
+    app_spec = compile_arc56_from_closure(contract, optimization_level=optimization_level)
+    response = deployer.create_bare(app_spec, args=[b"whoop"])
+
+    assert decode_logs(response.logs, "u") == ["whoop!"]
