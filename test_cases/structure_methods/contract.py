b5ca8f2fc3fe91e134f1955bb6f8856599270f14
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2025-12-30 11:21:37 -0300

    [WIP] Include algopy.Struct on the test

--- test_cases/structure_methods/contract.py
+++ test_cases/structure_methods/contract.py
@@ -1,18 +1,30 @@
-from algopy import ARC4Contract, UInt64, arc4, public
+from algopy import ARC4Contract, Struct, UInt64, arc4, public
 
 
-class TestStruct(arc4.Struct):
+class ARC4TestStruct(arc4.Struct):
     value: arc4.UInt64
 
     def return_value_times(self, times: UInt64) -> UInt64:
         return self.value.as_uint64() * times
 
 
+class TestStruct(Struct):
+    value: UInt64
+
+    def return_value_times(self, times: UInt64) -> UInt64:
+        return self.value * times
+
+
 class TestContract(ARC4Contract):
     @public
-    def test_method(self, value: UInt64) -> UInt64:
-        """Test method using @public decorator"""
-        return value + self.internal_method()
+    def test(self, expected: UInt64) -> None:
+        assert expected == self.arc4_struct_test()
+        assert expected == self.struct_test()
 
-    def internal_method(self) -> UInt64:
-        return TestStruct(arc4.UInt64(9)).return_value_times(UInt64(2))
+    def arc4_struct_test(self) -> UInt64:
+        arc4_struct = ARC4TestStruct(arc4.UInt64(9))
+        return arc4_struct.return_value_times(UInt64(2))
+
+    def struct_test(self) -> UInt64:
+        struct = TestStruct(UInt64(9))
+        return struct.return_value_times(UInt64(2))
--- test_cases/structure_methods/out/client_TestContract.py
+++ test_cases/structure_methods/out/client_TestContract.py
@@ -8,10 +8,7 @@ import algopy
 
 class TestContract(algopy.arc4.ARC4Client, typing.Protocol):
     @algopy.arc4.abimethod
-    def test_method(
+    def test(
         self,
-        value: algopy.arc4.UIntN[typing.Literal[64]],
-    ) -> algopy.arc4.UIntN[typing.Literal[64]]:
-        """
-        Test method using @public decorator
-        """
+        expected: algopy.arc4.UIntN[typing.Literal[64]],
+    ) -> None: ...
