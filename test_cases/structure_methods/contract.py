e72d1312e448b5c035aedaa5f3e743e2d04057ee
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2026-01-02 09:43:39 -0300

    Add a named tuple to the structure methods test

--- test_cases/structure_methods/contract.py
+++ test_cases/structure_methods/contract.py
@@ -1,3 +1,5 @@
+import typing
+
 from algopy import ARC4Contract, Struct, UInt64, arc4, public
 
 
@@ -15,11 +17,19 @@ class TestStruct(Struct):
         return self.value * times
 
 
+class TestNamedTuple(typing.NamedTuple):
+    value: UInt64
+
+    def return_value_times(self, times: UInt64) -> UInt64:
+        return self.value * times
+
+
 class TestContract(ARC4Contract):
     @public
     def test(self, expected: UInt64) -> None:
         assert expected == self.arc4_struct_test()
         assert expected == self.struct_test()
+        assert expected == self.named_tuple_test()
 
     def arc4_struct_test(self) -> UInt64:
         arc4_struct = ARC4TestStruct(arc4.UInt64(9))
@@ -28,3 +38,7 @@ class TestContract(ARC4Contract):
     def struct_test(self) -> UInt64:
         struct = TestStruct(UInt64(9))
         return struct.return_value_times(UInt64(2))
+
+    def named_tuple_test(self) -> UInt64:
+        named_tuple = TestNamedTuple(UInt64(9))
+        return named_tuple.return_value_times(UInt64(2))
