e2cc1c30883f774d57c9433e21b3eba92d45c0ab
Bobby Lat <bobby.lat@makerx.com.au>
2026-01-22 16:58:17 +0700

    chore: compile all

---
+++ test_cases/conditional_execution/out/client_ConditionalExecutionContract.py
@@ -0,0 +1,10 @@
+# This file is auto-generated, do not modify
+# flake8: noqa
+# fmt: off
+import typing
+
+import algopy
+
+
+class ConditionalExecutionContract(algopy.arc4.ARC4Client, typing.Protocol):
+    pass
---
+++ test_cases/with_reentrancy/out/client_WithReentrancy.py
@@ -0,0 +1,13 @@
+# This file is auto-generated, do not modify
+# flake8: noqa
+# fmt: off
+import typing
+
+import algopy
+
+
+class WithReentrancy(algopy.arc4.ARC4Client, typing.Protocol):
+    """
+    My re-entrant contract
+    """
+    pass
