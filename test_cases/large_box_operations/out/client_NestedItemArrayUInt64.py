57db7b65ed7a2cca31d654993b1c1a8dbc0be8f7
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2026-02-17 11:37:55 +0800

    chore: compile all

--- test_cases/large_box_operations/out/client_NestedItemArrayUInt64.py
+++ test_cases/large_box_operations/out/client_NestedItemArrayUInt64.py
@@ -13,6 +13,9 @@ class Parent(algopy.arc4.Struct):
     child: Child
     bar: algopy.arc4.UIntN[typing.Literal[64]]
 
+class ItemWithArray(algopy.arc4.Struct):
+    arr: algopy.arc4.DynamicArray[algopy.arc4.UIntN[typing.Literal[64]]]
+
 class NestedItemArrayUInt64(algopy.arc4.ARC4Client, typing.Protocol):
     @algopy.arc4.abimethod
     def bootstrap(
@@ -54,6 +57,17 @@ class NestedItemArrayUInt64(algopy.arc4.ARC4Client, typing.Protocol):
         self,
         item_idx: algopy.arc4.UIntN[typing.Literal[64]],
     ) -> algopy.arc4.UIntN[typing.Literal[64]]: ...
+
+    @algopy.arc4.abimethod
+    def dynamic_append(
+        self,
+        item: ItemWithArray,
+    ) -> None: ...
+
+    @algopy.arc4.abimethod
+    def dynamic_pop(
+        self,
+    ) -> ItemWithArray: ...
 
     @algopy.arc4.abimethod
     def clear_padding(
