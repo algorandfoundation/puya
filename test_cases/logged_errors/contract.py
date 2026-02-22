26d1192d1829b4d124011f15db20dea886cf7c33
Argimirodelpozo <argimirodelpozo@gmail.com>
2026-02-21 23:58:13 -0300

    test: add more thorough coverage in expected outputs + minor changes in on-chain test

--- test_cases/logged_errors/contract.py
+++ test_cases/logged_errors/contract.py
@@ -5,12 +5,24 @@ from algopy.arc4 import ARC4Contract, abimethod
 class LoggedErrorsContract(ARC4Contract):
     @abimethod
     def test_logged_errs(self, arg: UInt64) -> None:
-        # code only (default prefix "ERR") -> "ERR:01"
+        # "ERR:01"
         logged_assert(arg != 1, "01")
-        # code + message -> "ERR:02:arg is two"
-        logged_assert(arg != 2, "02", error_message="arg is two")
-        # code + custom prefix -> "AER:03"
-        logged_assert(arg != 3, "03", prefix="AER")
-        # unconditional logged_err -> "ERR:04"
-        if arg == 4:
-            logged_err("04")
+        # "ERR:arg02:arg is two"
+        logged_assert(arg != 2, "arg02", error_message="arg is two")
+        # "AER:arg03"
+        logged_assert(arg != 3, "arg03", prefix="AER")
+        # "AER:arg04:arg is 4"
+        logged_assert(arg != 4, "arg04", error_message="arg is 4", prefix="AER")
+
+        # "ERR:arg05"
+        if arg == 5:
+            logged_err("arg05")
+        # "ERR:arg06:arg was 6"
+        if arg == 6:
+            logged_err("06", error_message="arg was 6")
+        # "AER:arg07"
+        if arg == 7:
+            logged_err("arg07", prefix="AER")
+        # "AER:arg08:arg is seven (08)"
+        if arg == 8:
+            logged_err("arg08", error_message="arg is eight (08)", prefix="AER")
