e60214cb57170178dba381562bd1af2a282bdcc9
Adam Chidlow <achidlow@users.noreply.github.com>
2026-02-24 16:53:34 +0800

    optimise dynamic_array_pop_dynamic_element

--- src/_puya_lib/arc4.py
+++ src/_puya_lib/arc4.py
@@ -107,20 +107,19 @@ def dynamic_array_pop_dynamic_element(array: Bytes) -> Bytes:
     """
     array_length = extract_uint16(array, 0)
     length_minus_1 = array_length - 1
-    popped_header_offset = length_minus_1 * UINT16_SIZE
     head_and_tail = extract(array, UINT16_SIZE, 0)
-    popped_offset = extract_uint16(head_and_tail, popped_header_offset)
 
-    new_head = Bytes()
-    for head_offset in urange(0, length_minus_1 * UINT16_SIZE, UINT16_SIZE):
+    updated = extract(itob(length_minus_1), UINT16_OFFSET, UINT16_SIZE)
+    popped_header_offset = length_minus_1 * UINT16_SIZE
+    for head_offset in urange(0, popped_header_offset, UINT16_SIZE):
         item_offset = extract_uint16(head_and_tail, head_offset)
         item_offset -= UINT16_SIZE
-        new_head += extract(itob(item_offset), UINT16_OFFSET, UINT16_SIZE)
+        updated += extract(itob(item_offset), UINT16_OFFSET, UINT16_SIZE)
 
-    updated = (
-        extract(itob(length_minus_1), UINT16_OFFSET, UINT16_SIZE)
-        + new_head
-        + substring(head_and_tail, popped_header_offset + UINT16_SIZE, popped_offset)
+    updated += substring(
+        head_and_tail,
+        popped_header_offset + UINT16_SIZE,
+        extract_uint16(head_and_tail, popped_header_offset),
     )
 
     return updated
