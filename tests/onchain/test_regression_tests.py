4f1ddd52d40b1fc7574e7447a8103e7947844574
ArgimiroCF <argimirodelpozo@gmail.com>
2026-04-15 21:34:44 -0300

    chore: lint

--- tests/onchain/test_regression_tests.py
+++ tests/onchain/test_regression_tests.py
@@ -32,15 +32,11 @@ def test_xor_identity_fold_biguint(deployer_o: Deployer) -> None:
 def test_gebit_bytes_fold_oob(deployer_o: Deployer) -> None:
     # solves compiler crash when trying to fold OOB getbit acces into Bytes constant.
     with pytest.raises(au.LogicError, match="had error 'getbit index beyond byteslice'"):
-        deployer_o.create_bare(
-            TEST_CASES_DIR / "regression_tests" / "getbit_fold_oob_bytes.py"
-        )
+        deployer_o.create_bare(TEST_CASES_DIR / "regression_tests" / "getbit_fold_oob_bytes.py")
 
 
 def test_getbit_uint64_fold_oob(deployer_o: Deployer) -> None:
     # OOB getbit access in uint64 should panic on the AVM.
     # Without the fix, the optimizer folds this to UInt64(0) and the panic is lost.
     with pytest.raises(au.LogicError, match="had error 'getbit index > 63 with Uint'"):
-        deployer_o.create_bare(
-            TEST_CASES_DIR / "regression_tests" / "getbit_fold_oob_uint64.py"
-        )
+        deployer_o.create_bare(TEST_CASES_DIR / "regression_tests" / "getbit_fold_oob_uint64.py")
