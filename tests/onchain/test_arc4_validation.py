9f41fabbd9c289ea6264fce74e29dc03621e6ddd
Ignacio Losiggio <ignacio.losiggio@algorand.foundation>
2026-05-08 12:14:59 -0300

    chore: revert: Update the tests we broke

--- tests/onchain/test_arc4_validation.py
+++ tests/onchain/test_arc4_validation.py
@@ -9,9 +9,12 @@ from tests.utils.deployer import Deployer
 
 _VALID_DYNAMIC_STRUCT_BYTES = b"\x00" * 9 + b"\x00\x0b\x00\x00"
 _INVALID_DYNAMIC_STRUCT_BYTES = b"\x00" * 9 + b"\x00\x0b\x00\x01"
-_STATIC_STRUCT = "(uint64,uint8)"
-_DYNAMIC_STRUCT = "(uint64,uint8,(len+utf8[]))"
-_WITH_A_BOOL = "(uint8,(len+uint8[]),bool1)"
+_STATIC_STRUCT = "test_cases.arc4_validation.contract.ARC4StaticStruct"
+_DYNAMIC_STRUCT = "test_cases.arc4_validation.contract.ARC4DynamicStruct"
+_WITH_A_BOOL = "test_cases.arc4_validation.contract.WithABool"
+_FROZEN_DYNAMIC_STRUCT = "test_cases.arc4_validation.contract.ARC4FrozenDynamicStruct"
+_NATIVE_STATIC_STRUCT = "test_cases.arc4_validation.contract.NativeStaticStruct"
+_NATIVE_DYNAMIC_STRUCT = "test_cases.arc4_validation.contract.NativeDynamicStruct"
 
 
 @pytest.fixture(scope="module")
@@ -95,32 +98,32 @@ def _invalid_size(type_name: str) -> str:
 
 def _invalid_arr_size(element_type_name: str, size: int | None = None) -> str:
     if size is None:
-        return _invalid_size(f"(len+{element_type_name}[])")
+        return _invalid_size(f"arc4.dynamic_array<{element_type_name}>")
     else:
-        return _invalid_size(f"{element_type_name}[{size}]")
+        return _invalid_size(f"arc4.static_array<{element_type_name}, {size}>")
 
 
 @pytest.mark.parametrize(
     ("type_name", "size_or_bytes_value", "expected_error"),
     [
-        ("uint64", 7, _invalid_size("uint64")),
-        ("ufixed64", 7, _invalid_size("uint64")),
-        ("uint8", 2, _invalid_size("uint8")),
-        ("bool", 2, _invalid_size("bool")),
-        ("byte", 2, _invalid_size("uint8")),
-        ("string", 5, _invalid_arr_size("utf8")),
-        ("bytes", 5, _invalid_arr_size("uint8")),
-        ("address", 0, _invalid_arr_size("uint8", 32)),
-        ("address", 33, _invalid_arr_size("uint8", 32)),
-        ("account", 0, _invalid_size("uint8")),
-        ("account", 33, _invalid_size("uint8")),
-        ("uint512", 63, _invalid_size("uint512")),
+        ("uint64", 7, _invalid_size("arc4.uint64")),
+        ("ufixed64", 7, _invalid_size("arc4.ufixed64x2")),
+        ("uint8", 2, _invalid_size("arc4.uint8")),
+        ("bool", 2, _invalid_size("arc4.bool")),
+        ("byte", 2, _invalid_size("arc4.uint8")),
+        ("string", 5, _invalid_arr_size("arc4.uint8")),
+        ("bytes", 5, _invalid_arr_size("arc4.uint8")),
+        ("address", 0, _invalid_arr_size("arc4.uint8", 32)),
+        ("address", 33, _invalid_arr_size("arc4.uint8", 32)),
+        ("account", 0, _invalid_size("account")),
+        ("account", 33, _invalid_size("account")),
+        ("uint512", 63, _invalid_size("arc4.uint512")),
         ("uint8_arr", 1, "invalid array length header"),
-        ("uint8_arr", 3, _invalid_arr_size("uint8")),
-        ("uint8_arr3", 1, _invalid_arr_size("uint8", 3)),
-        ("uint8_arr3", 4, _invalid_arr_size("uint8", 3)),
-        ("bool_arr", b"\x00\x01", _invalid_arr_size("bool1")),
-        ("bool_arr", b"\x00\x09\x00", _invalid_arr_size("bool1")),
+        ("uint8_arr", 3, _invalid_arr_size("arc4.uint8")),
+        ("uint8_arr3", 1, _invalid_arr_size("arc4.uint8", 3)),
+        ("uint8_arr3", 4, _invalid_arr_size("arc4.uint8", 3)),
+        ("bool_arr", b"\x00\x01", _invalid_arr_size("arc4.bool")),
+        ("bool_arr", b"\x00\x09\x00", _invalid_arr_size("arc4.bool")),
         ("static_struct", 8, _invalid_size(_STATIC_STRUCT)),
         ("dynamic_struct", 0, "invalid tuple encoding"),
         ("dynamic_struct", 11, "invalid tail pointer"),
@@ -132,13 +135,13 @@ def _invalid_arr_size(element_type_name: str, size: int | None = None) -> str:
             _invalid_size(_WITH_A_BOOL),
         ),
         ("dynamic_struct_with_a_bool", b"\x01\x00\x03\x80\x00\x01\xff", "invalid tail pointer"),
-        ("static_tuple", 8, _invalid_size("(uint64,uint8)")),
+        ("static_tuple", 8, _invalid_size("arc4.tuple<arc4.uint64,arc4.uint8>")),
         ("dynamic_tuple", 0, "invalid tuple encoding"),
         ("dynamic_tuple", 11, "invalid tail pointer"),
         (
             "dynamic_tuple",
             _INVALID_DYNAMIC_STRUCT_BYTES,
-            _invalid_size("(uint64,uint8,(len+utf8[]))"),
+            _invalid_size("arc4.tuple<arc4.uint64,arc4.uint8,arc4.dynamic_array<arc4.uint8>>"),
         ),
         ("static_struct_arr", 0, "invalid array length header"),
         ("static_struct_arr", 1, "invalid array length header"),
@@ -257,17 +260,17 @@ def test_native_validation_valid(
 @pytest.mark.parametrize(
     ("type_name", "size_or_bytes_value", "expected_error"),
     [
-        ("static_struct", 8, _invalid_size(_STATIC_STRUCT)),
+        ("static_struct", 8, _invalid_size(_NATIVE_STATIC_STRUCT)),
         ("dynamic_struct", 0, "invalid tuple encoding"),
         ("dynamic_struct", 11, "invalid tail pointer"),
-        ("dynamic_struct", _INVALID_DYNAMIC_STRUCT_BYTES, _invalid_size(_DYNAMIC_STRUCT)),
+        ("dynamic_struct", _INVALID_DYNAMIC_STRUCT_BYTES, _invalid_size(_NATIVE_DYNAMIC_STRUCT)),
         ("static_struct_arr", 0, "invalid array length header"),
         ("static_struct_arr", 1, "invalid array length header"),
-        ("static_struct_arr", 29, _invalid_arr_size(_STATIC_STRUCT)),
-        ("static_struct_arr3", 26, _invalid_arr_size(_STATIC_STRUCT, 3)),
+        ("static_struct_arr", 29, _invalid_arr_size(_NATIVE_STATIC_STRUCT)),
+        ("static_struct_arr3", 26, _invalid_arr_size(_NATIVE_STATIC_STRUCT, 3)),
         ("dynamic_struct_arr", 0, "invalid array length header"),
         ("dynamic_struct_arr", 1, "invalid array length header"),
-        ("dynamic_struct_arr", 29, _invalid_arr_size(_DYNAMIC_STRUCT)),
+        ("dynamic_struct_arr", 29, _invalid_arr_size(_NATIVE_DYNAMIC_STRUCT)),
         ("dynamic_struct_arr3", 27, "invalid tail pointer"),
         (
             "dynamic_struct_arr3",
@@ -291,14 +294,14 @@ def test_native_validation_valid(
                 _VALID_DYNAMIC_STRUCT_BYTES,
                 _INVALID_DYNAMIC_STRUCT_BYTES,
             ),
-            _invalid_arr_size(_DYNAMIC_STRUCT, 3),
+            _invalid_arr_size(_NATIVE_DYNAMIC_STRUCT, 3),
         ),
         ("dynamic_struct_imm_arr", 0, "invalid array length header"),
         ("dynamic_struct_imm_arr", 1, "invalid array length header"),
         (
             "dynamic_struct_imm_arr",
             29,
-            _invalid_arr_size(_DYNAMIC_STRUCT),
+            _invalid_arr_size(_FROZEN_DYNAMIC_STRUCT),
         ),
         ("dynamic_struct_imm_arr3", 27, "invalid tail pointer"),
         (
@@ -323,7 +326,7 @@ def test_native_validation_valid(
                 _VALID_DYNAMIC_STRUCT_BYTES,
                 _INVALID_DYNAMIC_STRUCT_BYTES,
             ),
-            _invalid_arr_size(_DYNAMIC_STRUCT, 3),
+            _invalid_arr_size(_FROZEN_DYNAMIC_STRUCT, 3),
         ),
     ],
 )
