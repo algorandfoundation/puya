8c2cbceb3136cc21cdd41472462a521060b69a31
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2026-05-20 04:40:35 +0000

    refactor: reorder functions in top-down order, reduce public API

--- src/puya/mir/stack_allocation/x_stack.py
+++ src/puya/mir/stack_allocation/x_stack.py
@@ -13,187 +13,56 @@ from puya.mir.vla import VariableLifetimeAnalysis
 logger = log.get_logger(__name__)
 
 
+def x_stack_allocation(_ctx: ProgramMIRContext, sub: mir.MemorySubroutine) -> None:
+    # this is basically baileys algorithm
+    vla = VariableLifetimeAnalysis(sub)
+    edge_sets = _get_edge_sets(sub, vla)
+    if not edge_sets:
+        # nothing to do
+        return
+
+    logger.debug(f"Found {len(edge_sets)} edge set/s for {sub.signature.name}")
+    _schedule_sets(vla, edge_sets)
+
+    if not _validate_x_stacks(edge_sets):
+        raise InternalError("Could not schedule x-stack")
+
+    _add_x_stack_ops_to_edge_sets(sub, edge_sets)
+
+
 @attrs.define(eq=False, repr=False)
-class BlockRecord:
+class _BlockRecord:
     block: mir.MemoryBasicBlock
     local_references: list[mir.AbstractStore | mir.AbstractLoad]
     live_in: Set[str]
     live_out: Set[str]
-    children: "list[BlockRecord]" = attrs.field(factory=list)
-    parents: "list[BlockRecord]" = attrs.field(factory=list)
-    co_parents: "list[BlockRecord]" = attrs.field(factory=list)
-    siblings: "list[BlockRecord]" = attrs.field(factory=list)
+    children: "list[_BlockRecord]" = attrs.field(factory=list)
+    parents: "list[_BlockRecord]" = attrs.field(factory=list)
+    co_parents: "list[_BlockRecord]" = attrs.field(factory=list)
+    siblings: "list[_BlockRecord]" = attrs.field(factory=list)
 
     x_stack_in: Sequence[str] | None = None
     x_stack_out: Sequence[str] | None = None
 
     def __repr__(self) -> str:
-        # due to recursive nature of BlockRecord, provide str implementation to
+        # due to recursive nature of _BlockRecord, provide str implementation to
         # simplify output
-        return f"BlockRecord({self.block})"
+        return f"_BlockRecord({self.block})"
 
     @staticmethod
-    def by_index(block: "BlockRecord") -> int:
+    def by_index(block: "_BlockRecord") -> int:
         return block.block.id
 
 
 @attrs.frozen
-class EdgeSet:
-    out_blocks: Sequence[BlockRecord] = attrs.field(converter=tuple[BlockRecord, ...])
-    in_blocks: Sequence[BlockRecord] = attrs.field(converter=tuple[BlockRecord, ...])
+class _EdgeSet:
+    out_blocks: Sequence[_BlockRecord] = attrs.field(converter=tuple[_BlockRecord, ...])
+    in_blocks: Sequence[_BlockRecord] = attrs.field(converter=tuple[_BlockRecord, ...])
 
 
-def sort_by_appearance(
-    variables: Set[str], block: mir.MemoryBasicBlock, *, load: bool = True
-) -> Sequence[str]:
-    appearance = list[str]()
-    block_ops = block.ops if load else reversed(block.ops)
-    if load:
-        virtual_ops = (o.local_id for o in block_ops if isinstance(o, mir.AbstractLoad))
-    else:
-        virtual_ops = (o.local_id for o in block_ops if isinstance(o, mir.AbstractStore))
-    for local_id in virtual_ops:
-        if local_id in variables and local_id not in appearance:
-            appearance.append(local_id)
-            # don't keep searching once we are done
-            if len(appearance) == len(variables):
-                break
-    return appearance
-
-
-def len_and_value(value: tuple[str, ...]) -> tuple[int, tuple[str, ...]]:
-    return len(value), value
-
-
-def find_shared_x_stack(x_stack_candidates: Sequence[Sequence[str]]) -> Sequence[str]:
-    """Find a common subsequence that is shared by all x-stacks"""
-    cache = dict[tuple[tuple[str, ...], tuple[str, ...]], tuple[str, ...]]()
-
-    def lcs(s1: tuple[str, ...], s2: tuple[str, ...]) -> tuple[str, ...]:
-        key = (s1, s2)
-        result = cache.get(key)
-        if result is None:
-            i = len(s1)
-            j = len(s2)
-            if i == 0 or j == 0:
-                result = ()
-            elif s1[-1] == s2[-1]:
-                result = (*lcs(s1[:-1], s2[:-1]), s1[-1])
-            else:
-                result = max(lcs(s1[:-1], s2), lcs(s1, s2[:-1]), key=len_and_value)
-            cache[key] = result
-        return result
-
-    shared, *others = sorted({tuple(s) for s in x_stack_candidates}, key=len_and_value)
-    for other in others:
-        shared = lcs(shared, other)
-    return shared
-
-
-def get_x_stack_load_ops(record: BlockRecord) -> set[mir.AbstractLoad]:
-    block = record.block
-    assert block.x_stack_in is not None
-
-    remaining = set(block.x_stack_in)
-    load_ops = []
-    for ref in record.local_references:
-        if isinstance(ref, mir.AbstractLoad) and ref.local_id in remaining:
-            remaining.remove(ref.local_id)
-            load_ops.append(ref)
-
-    if remaining:
-        raise InternalError(
-            f"Failed to move {', '.join(remaining)} from the x-stack",
-            location=block.source_location,
-        )
-
-    return set(load_ops)
-
-
-def get_x_stack_store_ops(record: BlockRecord) -> set[mir.AbstractStore]:
-    block = record.block
-    assert block.x_stack_out is not None
-
-    remaining = set(block.x_stack_out)
-    store_ops = []
-    for ref in reversed(record.local_references):
-        if isinstance(ref, mir.AbstractStore) and ref.local_id in remaining:
-            remaining.remove(ref.local_id)
-            store_ops.append(ref)
-
-    if remaining:
-        raise InternalError(
-            f"Failed to copy {', '.join(remaining)} to the x-stack",
-            location=block.source_location,
-        )
-
-    return set(store_ops)
-
-
-def add_x_stack_ops(sub: mir.MemorySubroutine, record: BlockRecord) -> None:
-    block = record.block
-    # determine ops to replace
-    load_ops = get_x_stack_load_ops(record)
-    store_ops = get_x_stack_store_ops(record)
-
-    stack = Stack.begin_block(sub, block)
-    for index, op in enumerate(block.mem_ops):
-        if op in store_ops:
-            assert isinstance(op, mir.AbstractStore)
-            # can replace virtual store op because only variables that could be fully
-            # scheduled are on the x-stack
-            block.mem_ops[index] = op = mir.StoreXStack(
-                local_id=op.local_id,
-                depth=stack.xl_height - 1,  # store to bottom
-                atype=op.atype,
-                source_location=op.source_location,
-            )
-        elif op in load_ops:
-            assert isinstance(op, mir.AbstractLoad)
-            block.mem_ops[index] = op = mir.LoadXStack(
-                local_id=op.local_id,
-                depth=stack.xl_height - stack.x_stack.index(op.local_id) - 1,
-                atype=op.atype,
-                source_location=op.source_location,
-            )
-        op.accept(stack)
-
-
-def add_x_stack_ops_to_edge_sets(sub: mir.MemorySubroutine, edge_sets: Sequence[EdgeSet]) -> None:
-    records = dict.fromkeys(
-        b
-        for edge_set in edge_sets
-        for b in itertools.chain(edge_set.out_blocks, edge_set.in_blocks)
-        if b.x_stack_in or b.x_stack_out
-    )
-    for record in records:
-        assert record.x_stack_in is not None
-        assert record.x_stack_out is not None
-        record.block.x_stack_in = record.x_stack_in
-        record.block.x_stack_out = record.x_stack_out
-        add_x_stack_ops(sub, record)
-
-
-def _unique_ordered_blocks(blocks: Iterable[BlockRecord]) -> list[BlockRecord]:
-    return sorted(set(blocks), key=BlockRecord.by_index)
-
-
-def get_edge_set(block: BlockRecord) -> EdgeSet | None:
-    out_blocks = _unique_ordered_blocks(itertools.chain((block,), block.co_parents))
-    # keep expanding out_blocks (and consequently in_blocks) until out_blocks stabilize
-    while True:
-        in_blocks = _unique_ordered_blocks(s for p in out_blocks for s in p.children)
-        new_out_blocks = _unique_ordered_blocks(p for s in in_blocks for p in s.parents)
-        if new_out_blocks == out_blocks:
-            break
-        out_blocks = new_out_blocks
-
-    return EdgeSet(out_blocks, in_blocks) if in_blocks else None
-
-
-def get_edge_sets(sub: mir.MemorySubroutine, vla: VariableLifetimeAnalysis) -> Sequence[EdgeSet]:
+def _get_edge_sets(sub: mir.MemorySubroutine, vla: VariableLifetimeAnalysis) -> Sequence[_EdgeSet]:
     records = {
-        block.block_name: BlockRecord(
+        block.block_name: _BlockRecord(
             block=block,
             local_references=[
                 op for op in block.ops if isinstance(op, mir.AbstractStore | mir.AbstractLoad)
@@ -240,10 +109,10 @@ def get_edge_sets(sub: mir.MemorySubroutine, vla: VariableLifetimeAnalysis) -> S
                 if parent is not block and parent not in block.co_parents:
                     block.co_parents.append(parent)
 
-    edge_sets = dict[EdgeSet, None]()
+    edge_sets = dict[_EdgeSet, None]()
     for block in blocks:
         # keep expanding edge set until it stabilizes
-        edge_set = get_edge_set(block)
+        edge_set = _get_edge_set(block)
         if edge_set:
             edge_sets[edge_set] = None
         else:
@@ -255,7 +124,24 @@ def get_edge_sets(sub: mir.MemorySubroutine, vla: VariableLifetimeAnalysis) -> S
     return list(edge_sets.keys())
 
 
-def schedule_sets(vla: VariableLifetimeAnalysis, edge_sets: Sequence[EdgeSet]) -> None:
+def _get_edge_set(block: _BlockRecord) -> _EdgeSet | None:
+    out_blocks = _unique_ordered_blocks(itertools.chain((block,), block.co_parents))
+    # keep expanding out_blocks (and consequently in_blocks) until out_blocks stabilize
+    while True:
+        in_blocks = _unique_ordered_blocks(s for p in out_blocks for s in p.children)
+        new_out_blocks = _unique_ordered_blocks(p for s in in_blocks for p in s.parents)
+        if new_out_blocks == out_blocks:
+            break
+        out_blocks = new_out_blocks
+
+    return _EdgeSet(out_blocks, in_blocks) if in_blocks else None
+
+
+def _unique_ordered_blocks(blocks: Iterable[_BlockRecord]) -> list[_BlockRecord]:
+    return sorted(set(blocks), key=_BlockRecord.by_index)
+
+
+def _schedule_sets(vla: VariableLifetimeAnalysis, edge_sets: Sequence[_EdgeSet]) -> None:
     # determine all blocks referencing variables, so we can track if all references to a
     # variable are scheduled to x-stack
     stores = dict[str, set[mir.MemoryBasicBlock]]()
@@ -281,12 +167,12 @@ def schedule_sets(vla: VariableLifetimeAnalysis, edge_sets: Sequence[EdgeSet]) -
 
         # TODO: better results might be possible if we allow reordering of x-stack
         x_stack_candidates = [
-            *(sort_by_appearance(common_locals, b.block, load=False) for b in out_blocks),
-            *(sort_by_appearance(common_locals, b.block, load=True) for b in in_blocks),
+            *(_sort_by_appearance(common_locals, b.block, load=False) for b in out_blocks),
+            *(_sort_by_appearance(common_locals, b.block, load=True) for b in in_blocks),
         ]
 
         # find an x_stack for this EdgeSet
-        x_stack = find_shared_x_stack(x_stack_candidates)
+        x_stack = _find_shared_x_stack(x_stack_candidates)
 
         for block in out_blocks:
             assert block.x_stack_out is None
@@ -322,7 +208,63 @@ def schedule_sets(vla: VariableLifetimeAnalysis, edge_sets: Sequence[EdgeSet]) -
         )
 
 
-def validate_pair(parent: BlockRecord, child: BlockRecord) -> bool:
+def _find_shared_x_stack(x_stack_candidates: Sequence[Sequence[str]]) -> Sequence[str]:
+    """Find a common subsequence that is shared by all x-stacks"""
+    cache = dict[tuple[tuple[str, ...], tuple[str, ...]], tuple[str, ...]]()
+
+    def lcs(s1: tuple[str, ...], s2: tuple[str, ...]) -> tuple[str, ...]:
+        key = (s1, s2)
+        result = cache.get(key)
+        if result is None:
+            i = len(s1)
+            j = len(s2)
+            if i == 0 or j == 0:
+                result = ()
+            elif s1[-1] == s2[-1]:
+                result = (*lcs(s1[:-1], s2[:-1]), s1[-1])
+            else:
+                result = max(lcs(s1[:-1], s2), lcs(s1, s2[:-1]), key=_len_and_value)
+            cache[key] = result
+        return result
+
+    shared, *others = sorted({tuple(s) for s in x_stack_candidates}, key=_len_and_value)
+    for other in others:
+        shared = lcs(shared, other)
+    return shared
+
+
+def _len_and_value(value: tuple[str, ...]) -> tuple[int, tuple[str, ...]]:
+    return len(value), value
+
+
+def _sort_by_appearance(
+    variables: Set[str], block: mir.MemoryBasicBlock, *, load: bool = True
+) -> Sequence[str]:
+    appearance = list[str]()
+    block_ops = block.ops if load else reversed(block.ops)
+    if load:
+        virtual_ops = (o.local_id for o in block_ops if isinstance(o, mir.AbstractLoad))
+    else:
+        virtual_ops = (o.local_id for o in block_ops if isinstance(o, mir.AbstractStore))
+    for local_id in virtual_ops:
+        if local_id in variables and local_id not in appearance:
+            appearance.append(local_id)
+            # don't keep searching once we are done
+            if len(appearance) == len(variables):
+                break
+    return appearance
+
+
+def _validate_x_stacks(edge_sets: Sequence[_EdgeSet]) -> bool:
+    ok = True
+    for edge_set in edge_sets:
+        for parent in edge_set.out_blocks:
+            for child in edge_set.in_blocks:
+                ok = _validate_pair(parent, child) and ok
+    return ok
+
+
+def _validate_pair(parent: _BlockRecord, child: _BlockRecord) -> bool:
     parent_x = parent.x_stack_out
     child_x = child.x_stack_in
     assert parent_x is not None
@@ -338,27 +280,87 @@ def validate_pair(parent: BlockRecord, child: BlockRecord) -> bool:
     return True
 
 
-def validate_x_stacks(edge_sets: Sequence[EdgeSet]) -> bool:
-    ok = True
-    for edge_set in edge_sets:
-        for parent in edge_set.out_blocks:
-            for child in edge_set.in_blocks:
-                ok = validate_pair(parent, child) and ok
-    return ok
+def _add_x_stack_ops_to_edge_sets(
+    sub: mir.MemorySubroutine, edge_sets: Sequence[_EdgeSet]
+) -> None:
+    records = dict.fromkeys(
+        b
+        for edge_set in edge_sets
+        for b in itertools.chain(edge_set.out_blocks, edge_set.in_blocks)
+        if b.x_stack_in or b.x_stack_out
+    )
+    for record in records:
+        assert record.x_stack_in is not None
+        assert record.x_stack_out is not None
+        record.block.x_stack_in = record.x_stack_in
+        record.block.x_stack_out = record.x_stack_out
+        _add_x_stack_ops(sub, record)
 
 
-def x_stack_allocation(_ctx: ProgramMIRContext, sub: mir.MemorySubroutine) -> None:
-    # this is basically baileys algorithm
-    vla = VariableLifetimeAnalysis(sub)
-    edge_sets = get_edge_sets(sub, vla)
-    if not edge_sets:
-        # nothing to do
-        return
+def _add_x_stack_ops(sub: mir.MemorySubroutine, record: _BlockRecord) -> None:
+    block = record.block
+    # determine ops to replace
+    load_ops = _get_x_stack_load_ops(record)
+    store_ops = _get_x_stack_store_ops(record)
 
-    logger.debug(f"Found {len(edge_sets)} edge set/s for {sub.signature.name}")
-    schedule_sets(vla, edge_sets)
+    stack = Stack.begin_block(sub, block)
+    for index, op in enumerate(block.mem_ops):
+        if op in store_ops:
+            assert isinstance(op, mir.AbstractStore)
+            # can replace virtual store op because only variables that could be fully
+            # scheduled are on the x-stack
+            block.mem_ops[index] = op = mir.StoreXStack(
+                local_id=op.local_id,
+                depth=stack.xl_height - 1,  # store to bottom
+                atype=op.atype,
+                source_location=op.source_location,
+            )
+        elif op in load_ops:
+            assert isinstance(op, mir.AbstractLoad)
+            block.mem_ops[index] = op = mir.LoadXStack(
+                local_id=op.local_id,
+                depth=stack.xl_height - stack.x_stack.index(op.local_id) - 1,
+                atype=op.atype,
+                source_location=op.source_location,
+            )
+        op.accept(stack)
 
-    if not validate_x_stacks(edge_sets):
-        raise InternalError("Could not schedule x-stack")
 
-    add_x_stack_ops_to_edge_sets(sub, edge_sets)
+def _get_x_stack_load_ops(record: _BlockRecord) -> set[mir.AbstractLoad]:
+    block = record.block
+    assert block.x_stack_in is not None
+
+    remaining = set(block.x_stack_in)
+    load_ops = []
+    for ref in record.local_references:
+        if isinstance(ref, mir.AbstractLoad) and ref.local_id in remaining:
+            remaining.remove(ref.local_id)
+            load_ops.append(ref)
+
+    if remaining:
+        raise InternalError(
+            f"Failed to move {', '.join(remaining)} from the x-stack",
+            location=block.source_location,
+        )
+
+    return set(load_ops)
+
+
+def _get_x_stack_store_ops(record: _BlockRecord) -> set[mir.AbstractStore]:
+    block = record.block
+    assert block.x_stack_out is not None
+
+    remaining = set(block.x_stack_out)
+    store_ops = []
+    for ref in reversed(record.local_references):
+        if isinstance(ref, mir.AbstractStore) and ref.local_id in remaining:
+            remaining.remove(ref.local_id)
+            store_ops.append(ref)
+
+    if remaining:
+        raise InternalError(
+            f"Failed to copy {', '.join(remaining)} to the x-stack",
+            location=block.source_location,
+        )
+
+    return set(store_ops)
