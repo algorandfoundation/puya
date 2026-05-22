efd9f48b4ca5e0d0f570e3f1b94ec5b40ae84042
Adam Chidlow <achidlow@users.noreply.github.com>
2026-05-22 11:15:16 +0800

    simplify convert_stack_args_to_immediates

--- src/puya/ir/optimize/convert_stack_args_to_immediates.py
+++ src/puya/ir/optimize/convert_stack_args_to_immediates.py
@@ -1,3 +1,5 @@
+import typing
+
 import attrs
 
 from puya import log
@@ -5,6 +7,7 @@ from puya.avm import AVMType
 from puya.ir import models
 from puya.ir.avm_ops import AVMOp
 from puya.ir.optimize.context import IROptimizationContext
+from puya.ir.visitor_mutator import IRMutator
 
 logger = log.get_logger(__name__)
 
@@ -12,107 +15,93 @@ logger = log.get_logger(__name__)
 def convert_stack_args_to_immediates(
     _context: IROptimizationContext, subroutine: models.Subroutine
 ) -> bool:
-    modified = 0
+    mutator = _StackArgsToImmediatesMutator()
     for block in subroutine.body:
-        ops = []
-        for op in block.ops:
-            ops.append(op)
-            match op:
-                case models.Assignment(source=models.Intrinsic(args=args) as intrinsic) if args:
-                    with_immediates = _try_convert_stack_args_to_immediates(intrinsic)
-                    if with_immediates is not None:
-                        logger.debug(f"Simplified {op.source} to {with_immediates}")
-                        op.source = with_immediates
-                        modified += 1
-                case models.Intrinsic(args=args) as intrinsic if args:
-                    with_immediates = _try_convert_stack_args_to_immediates(intrinsic)
-                    if with_immediates is not None:
-                        logger.debug(f"Simplified {op} to {with_immediates}")
-                        ops[-1] = with_immediates
-                        modified += 1
-        block.ops[:] = ops
-    return modified > 0
+        mutator.visit_block(block)
+    return mutator.modified > 0
 
 
-def _try_convert_stack_args_to_immediates(intrinsic: models.Intrinsic) -> models.Intrinsic | None:
+@attrs.define(kw_only=True)
+class _StackArgsToImmediatesMutator(IRMutator):
+    modified: int = 0
+
+    @typing.override
+    def visit_intrinsic_op(self, intrinsic: models.Intrinsic) -> None:
+        if (
+            intrinsic.args
+            and (replacement := _try_convert_stack_args_to_immediates(intrinsic)) is not None
+        ):
+            before = str(intrinsic)
+            # note: assignment order here relies on current attrs validators, if that changes
+            # an alternative would be to assign to list slices first, then op, then call
+            # attrs.validate() manually
+            intrinsic.op, intrinsic.immediates, intrinsic.args = replacement
+            logger.debug(f"Simplified {before} to {intrinsic}")
+            self.modified += 1
+
+
+def _try_convert_stack_args_to_immediates(
+    intrinsic: models.Intrinsic,
+) -> tuple[AVMOp, list[str | int], list[models.Value]] | None:
     match intrinsic:
         case models.Intrinsic(
             op=AVMOp.gitxnas,
             args=[models.UInt64Constant(value=array_index)],
             immediates=[group_index, field],
         ) if array_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gitxna,
-                args=[],
-                immediates=[group_index, field, array_index],
-            )
+            return AVMOp.gitxna, [group_index, field, array_index], []
         case models.Intrinsic(
             op=AVMOp.itxnas,
             args=[models.UInt64Constant(value=array_index)],
             immediates=[field],
         ) if array_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.itxna,
-                args=[],
-                immediates=[field, array_index],
-            )
+            return AVMOp.itxna, [field, array_index], []
         case models.Intrinsic(
             op=(AVMOp.loads | AVMOp.stores as op),
             args=[models.UInt64Constant(value=slot), *rest],
         ) if slot <= 255:
-            return attrs.evolve(
-                intrinsic,
-                immediates=[slot],
-                args=rest,
-                op=AVMOp.load if op == AVMOp.loads else AVMOp.store,
-            )
+            return AVMOp.load if op == AVMOp.loads else AVMOp.store, [slot], rest
         case models.Intrinsic(
             op=AVMOp.extract3,
             args=[
-                models.Value(atype=AVMType.bytes),
+                models.Value(atype=AVMType.bytes) as bytes_arg,
                 models.UInt64Constant(value=S),
                 models.UInt64Constant(value=L),
             ],
         ) if S <= 255 and 1 <= L <= 255:
             # note the lower bound of 1 on length, extract with immediates vs extract3
             # have *very* different behaviour if the length is 0
-            return attrs.evolve(
-                intrinsic, immediates=[S, L], args=intrinsic.args[:1], op=AVMOp.extract
-            )
+            return AVMOp.extract, [S, L], [bytes_arg]
         case models.Intrinsic(
             op=AVMOp.substring3,
             args=[
-                models.Value(atype=AVMType.bytes),
+                models.Value(atype=AVMType.bytes) as bytes_arg,
                 models.UInt64Constant(value=S),
                 models.UInt64Constant(value=E),
             ],
         ) if S <= 255 and E <= 255:
-            return attrs.evolve(
-                intrinsic, immediates=[S, E], args=intrinsic.args[:1], op=AVMOp.substring
-            )
+            return AVMOp.substring, [S, E], [bytes_arg]
         case models.Intrinsic(
             op=AVMOp.replace3,
             args=[a, models.UInt64Constant(value=S), b],
         ) if S <= 255:
-            return attrs.evolve(intrinsic, immediates=[S], args=[a, b], op=AVMOp.replace2)
+            return AVMOp.replace2, [S], [a, b]
         case models.Intrinsic(
             op=AVMOp.args,
             args=[models.UInt64Constant(value=idx)],
         ) if idx <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.arg, immediates=[idx], args=[])
+            return AVMOp.arg, [idx], []
         case models.Intrinsic(
             op=AVMOp.gaids,
             args=[models.UInt64Constant(value=group_index)],
         ) if group_index <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.gaid, immediates=[group_index], args=[])
+            return AVMOp.gaid, [group_index], []
         case models.Intrinsic(
             op=AVMOp.gloads,
             args=[models.UInt64Constant(value=group_index)],
             immediates=[slot],
         ) if group_index <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.gload, immediates=[group_index, slot], args=[])
+            return AVMOp.gload, [group_index, slot], []
         case models.Intrinsic(
             op=AVMOp.gloadss,
             args=[
@@ -120,48 +109,36 @@ def _try_convert_stack_args_to_immediates(intrinsic: models.Intrinsic) -> models
                 models.UInt64Constant(value=slot),
             ],
         ) if group_index <= 255 and slot <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.gload, immediates=[group_index, slot], args=[])
+            return AVMOp.gload, [group_index, slot], []
         case models.Intrinsic(
             op=AVMOp.gloadss,
             args=[group_index_arg, models.UInt64Constant(value=slot)],
         ) if slot <= 255:
-            return attrs.evolve(
-                intrinsic, op=AVMOp.gloads, immediates=[slot], args=[group_index_arg]
-            )
+            return AVMOp.gloads, [slot], [group_index_arg]
         case models.Intrinsic(
             op=AVMOp.txnas,
             args=[models.UInt64Constant(value=array_index)],
             immediates=[field],
         ) if array_index <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.txna, immediates=[field, array_index], args=[])
+            return AVMOp.txna, [field, array_index], []
         case models.Intrinsic(
             op=AVMOp.gtxns,
             args=[models.UInt64Constant(value=group_index)],
             immediates=[field],
         ) if group_index <= 255:
-            return attrs.evolve(intrinsic, op=AVMOp.gtxn, immediates=[group_index, field], args=[])
+            return AVMOp.gtxn, [group_index, field], []
         case models.Intrinsic(
             op=AVMOp.gtxnas,
             args=[models.UInt64Constant(value=array_index)],
             immediates=[group_index, field],
         ) if array_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gtxna,
-                immediates=[group_index, field, array_index],
-                args=[],
-            )
+            return AVMOp.gtxna, [group_index, field, array_index], []
         case models.Intrinsic(
             op=AVMOp.gtxnsa,
             args=[models.UInt64Constant(value=group_index)],
             immediates=[field, array_index],
         ) if group_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gtxna,
-                immediates=[group_index, field, array_index],
-                args=[],
-            )
+            return AVMOp.gtxna, [group_index, field, array_index], []
         case models.Intrinsic(
             op=AVMOp.gtxnsas,
             args=[
@@ -170,32 +147,17 @@ def _try_convert_stack_args_to_immediates(intrinsic: models.Intrinsic) -> models
             ],
             immediates=[field],
         ) if group_index <= 255 and array_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gtxna,
-                immediates=[group_index, field, array_index],
-                args=[],
-            )
+            return AVMOp.gtxna, [group_index, field, array_index], []
         case models.Intrinsic(
             op=AVMOp.gtxnsas,
             args=[models.UInt64Constant(value=group_index), array_index_arg],
             immediates=[field],
         ) if group_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gtxnas,
-                immediates=[group_index, field],
-                args=[array_index_arg],
-            )
+            return AVMOp.gtxnas, [group_index, field], [array_index_arg]
         case models.Intrinsic(
             op=AVMOp.gtxnsas,
             args=[group_index_arg, models.UInt64Constant(value=array_index)],
             immediates=[field],
         ) if array_index <= 255:
-            return attrs.evolve(
-                intrinsic,
-                op=AVMOp.gtxnsa,
-                immediates=[field, array_index],
-                args=[group_index_arg],
-            )
+            return AVMOp.gtxnsa, [field, array_index], [group_index_arg]
     return None
