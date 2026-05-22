b33572f61b069a11eb986e7e07c2758d3d2bca83
Adam Chidlow <achidlow@users.noreply.github.com>
2026-05-22 11:27:42 +0800

    simplify convert_stack_args_to_immediates

--- src/puya/ir/optimize/convert_stack_args_to_immediates.py
+++ src/puya/ir/optimize/convert_stack_args_to_immediates.py
@@ -3,7 +3,6 @@ import typing
 import attrs
 
 from puya import log
-from puya.avm import AVMType
 from puya.ir import models
 from puya.ir.avm_ops import AVMOp
 from puya.ir.optimize.context import IROptimizationContext
@@ -43,121 +42,98 @@ class _StackArgsToImmediatesMutator(IRMutator):
 def _try_convert_stack_args_to_immediates(
     intrinsic: models.Intrinsic,
 ) -> tuple[AVMOp, list[str | int], list[models.Value]] | None:
-    match intrinsic:
-        case models.Intrinsic(
-            op=AVMOp.gitxnas,
-            args=[models.UInt64Constant(value=array_index)],
-            immediates=[group_index, field],
-        ) if array_index <= 255:
-            return AVMOp.gitxna, [group_index, field, array_index], []
-        case models.Intrinsic(
-            op=AVMOp.itxnas,
-            args=[models.UInt64Constant(value=array_index)],
-            immediates=[field],
-        ) if array_index <= 255:
-            return AVMOp.itxna, [field, array_index], []
-        case models.Intrinsic(
-            op=(AVMOp.loads | AVMOp.stores as op),
-            args=[models.UInt64Constant(value=slot), *rest],
-        ) if slot <= 255:
-            return AVMOp.load if op == AVMOp.loads else AVMOp.store, [slot], rest
-        case models.Intrinsic(
-            op=AVMOp.extract3,
-            args=[
-                models.Value(atype=AVMType.bytes) as bytes_arg,
-                models.UInt64Constant(value=S),
-                models.UInt64Constant(value=L),
-            ],
-        ) if S <= 255 and 1 <= L <= 255:
-            # note the lower bound of 1 on length, extract with immediates vs extract3
-            # have *very* different behaviour if the length is 0
-            return AVMOp.extract, [S, L], [bytes_arg]
-        case models.Intrinsic(
-            op=AVMOp.substring3,
-            args=[
-                models.Value(atype=AVMType.bytes) as bytes_arg,
-                models.UInt64Constant(value=S),
-                models.UInt64Constant(value=E),
-            ],
-        ) if S <= 255 and E <= 255:
-            return AVMOp.substring, [S, E], [bytes_arg]
-        case models.Intrinsic(
-            op=AVMOp.replace3,
-            args=[a, models.UInt64Constant(value=S), b],
-        ) if S <= 255:
-            return AVMOp.replace2, [S], [a, b]
-        case models.Intrinsic(
-            op=AVMOp.args,
-            args=[models.UInt64Constant(value=idx)],
-        ) if idx <= 255:
-            return AVMOp.arg, [idx], []
-        case models.Intrinsic(
-            op=AVMOp.gaids,
-            args=[models.UInt64Constant(value=group_index)],
-        ) if group_index <= 255:
-            return AVMOp.gaid, [group_index], []
-        case models.Intrinsic(
-            op=AVMOp.gloads,
-            args=[models.UInt64Constant(value=group_index)],
-            immediates=[slot],
-        ) if group_index <= 255:
-            return AVMOp.gload, [group_index, slot], []
-        case models.Intrinsic(
-            op=AVMOp.gloadss,
-            args=[
-                models.UInt64Constant(value=group_index),
-                models.UInt64Constant(value=slot),
-            ],
-        ) if group_index <= 255 and slot <= 255:
-            return AVMOp.gload, [group_index, slot], []
-        case models.Intrinsic(
-            op=AVMOp.gloadss,
-            args=[group_index_arg, models.UInt64Constant(value=slot)],
-        ) if slot <= 255:
-            return AVMOp.gloads, [slot], [group_index_arg]
-        case models.Intrinsic(
-            op=AVMOp.txnas,
-            args=[models.UInt64Constant(value=array_index)],
-            immediates=[field],
-        ) if array_index <= 255:
-            return AVMOp.txna, [field, array_index], []
-        case models.Intrinsic(
-            op=AVMOp.gtxns,
-            args=[models.UInt64Constant(value=group_index)],
-            immediates=[field],
-        ) if group_index <= 255:
-            return AVMOp.gtxn, [group_index, field], []
-        case models.Intrinsic(
-            op=AVMOp.gtxnas,
-            args=[models.UInt64Constant(value=array_index)],
-            immediates=[group_index, field],
-        ) if array_index <= 255:
-            return AVMOp.gtxna, [group_index, field, array_index], []
-        case models.Intrinsic(
-            op=AVMOp.gtxnsa,
-            args=[models.UInt64Constant(value=group_index)],
-            immediates=[field, array_index],
-        ) if group_index <= 255:
-            return AVMOp.gtxna, [group_index, field, array_index], []
-        case models.Intrinsic(
-            op=AVMOp.gtxnsas,
-            args=[
-                models.UInt64Constant(value=group_index),
-                models.UInt64Constant(value=array_index),
-            ],
-            immediates=[field],
-        ) if group_index <= 255 and array_index <= 255:
-            return AVMOp.gtxna, [group_index, field, array_index], []
-        case models.Intrinsic(
-            op=AVMOp.gtxnsas,
-            args=[models.UInt64Constant(value=group_index), array_index_arg],
-            immediates=[field],
-        ) if group_index <= 255:
-            return AVMOp.gtxnas, [group_index, field], [array_index_arg]
-        case models.Intrinsic(
-            op=AVMOp.gtxnsas,
-            args=[group_index_arg, models.UInt64Constant(value=array_index)],
-            immediates=[field],
-        ) if array_index <= 255:
-            return AVMOp.gtxnsa, [field, array_index], [group_index_arg]
+    match intrinsic.op:
+        case AVMOp.gitxnas:
+            (array_index_arg,) = intrinsic.args
+            if (array_index := _extract_uint8(array_index_arg)) is not None:
+                group_index_imm, field = intrinsic.immediates
+                return AVMOp.gitxna, [group_index_imm, field, array_index], []
+        case AVMOp.itxnas:
+            (array_index_arg,) = intrinsic.args
+            if (array_index := _extract_uint8(array_index_arg)) is not None:
+                (field,) = intrinsic.immediates
+                return AVMOp.itxna, [field, array_index], []
+        case AVMOp.loads:
+            (slot_arg,) = intrinsic.args
+            if (slot := _extract_uint8(slot_arg)) is not None:
+                return AVMOp.load, [slot], []
+        case AVMOp.stores:
+            slot_arg, value = intrinsic.args
+            if (slot := _extract_uint8(slot_arg)) is not None:
+                return AVMOp.store, [slot], [value]
+        case AVMOp.extract3:
+            bytes_arg, start_arg, length_arg = intrinsic.args
+            if (
+                (start := _extract_uint8(start_arg)) is not None
+                and (length := _extract_uint8(length_arg)) is not None
+                # note the lower bound of 1 on length, extract with immediates vs extract3
+                # have *very* different behaviour if the length is 0
+                and length >= 1
+            ):
+                return AVMOp.extract, [start, length], [bytes_arg]
+        case AVMOp.substring3:
+            bytes_arg, start_arg, end_arg = intrinsic.args
+            if (start := _extract_uint8(start_arg)) is not None and (
+                end := _extract_uint8(end_arg)
+            ) is not None:
+                return AVMOp.substring, [start, end], [bytes_arg]
+        case AVMOp.replace3:
+            a, start_arg, b = intrinsic.args
+            if (start := _extract_uint8(start_arg)) is not None:
+                return AVMOp.replace2, [start], [a, b]
+        case AVMOp.args:
+            (idx_arg,) = intrinsic.args
+            if (idx := _extract_uint8(idx_arg)) is not None:
+                return AVMOp.arg, [idx], []
+        case AVMOp.gaids:
+            (group_index_arg,) = intrinsic.args
+            if (group_index := _extract_uint8(group_index_arg)) is not None:
+                return AVMOp.gaid, [group_index], []
+        case AVMOp.gloads:
+            (group_index_arg,) = intrinsic.args
+            if (group_index := _extract_uint8(group_index_arg)) is not None:
+                (slot_imm,) = intrinsic.immediates
+                return AVMOp.gload, [group_index, slot_imm], []
+        case AVMOp.gloadss:
+            group_index_arg, slot_arg = intrinsic.args
+            if (slot := _extract_uint8(slot_arg)) is not None:
+                if (group_index := _extract_uint8(group_index_arg)) is not None:
+                    return AVMOp.gload, [group_index, slot], []
+                return AVMOp.gloads, [slot], [group_index_arg]
+        case AVMOp.txnas:
+            (array_index_arg,) = intrinsic.args
+            if (array_index := _extract_uint8(array_index_arg)) is not None:
+                (field,) = intrinsic.immediates
+                return AVMOp.txna, [field, array_index], []
+        case AVMOp.gtxns:
+            (group_index_arg,) = intrinsic.args
+            if (group_index := _extract_uint8(group_index_arg)) is not None:
+                (field,) = intrinsic.immediates
+                return AVMOp.gtxn, [group_index, field], []
+        case AVMOp.gtxnas:
+            (array_index_arg,) = intrinsic.args
+            if (array_index := _extract_uint8(array_index_arg)) is not None:
+                group_index_imm, field = intrinsic.immediates
+                return AVMOp.gtxna, [group_index_imm, field, array_index], []
+        case AVMOp.gtxnsa:
+            (group_index_arg,) = intrinsic.args
+            if (group_index := _extract_uint8(group_index_arg)) is not None:
+                field, array_index_imm = intrinsic.immediates
+                return AVMOp.gtxna, [group_index, field, array_index_imm], []
+        case AVMOp.gtxnsas:
+            group_index_arg, array_index_arg = intrinsic.args
+            (field,) = intrinsic.immediates
+            if (group_index := _extract_uint8(group_index_arg)) is not None:
+                if (array_index := _extract_uint8(array_index_arg)) is not None:
+                    return AVMOp.gtxna, [group_index, field, array_index], []
+                return AVMOp.gtxnas, [group_index, field], [array_index_arg]
+            if (array_index := _extract_uint8(array_index_arg)) is not None:
+                return AVMOp.gtxnsa, [field, array_index], [group_index_arg]
+    return None
+
+
+def _extract_uint8(arg: models.Value) -> int | None:
+    match arg:
+        case models.UInt64Constant(value=value) if value <= 255:
+            return value
     return None
