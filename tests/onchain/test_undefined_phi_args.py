70aa59c0825f716c6141d1df5212cf228efe3042
Adam Chidlow <achidlow@users.noreply.github.com>
2026-05-29 23:53:00 +0800

    test: add tests for GVN, ahead of implementation so we can see the deltas

--- tests/onchain/test_undefined_phi_args.py
+++ tests/onchain/test_undefined_phi_args.py
@@ -5,7 +5,7 @@ import pytest
 from tests import TEST_CASES_DIR
 from tests.utils.deployer import Deployer
 
-_CONTRACT_PATH = TEST_CASES_DIR / "undefined_phi_args"
+_CONTRACT_DIR = TEST_CASES_DIR / "undefined_phi_args"
 
 
 @pytest.mark.parametrize(
@@ -16,16 +16,20 @@ _CONTRACT_PATH = TEST_CASES_DIR / "undefined_phi_args"
         (b"mixed", "itob arg 0 wanted uint64 but got []byte"),
     ],
 )
-def test_undefined_phi_args(
-    deployer_o: Deployer, test_case: bytes, expected_logic_error: str
-) -> None:
+def test_baddie(deployer_o: Deployer, test_case: bytes, expected_logic_error: str) -> None:
+    contract_path = _CONTRACT_DIR / "baddie.py"
     deployer_debug1 = attrs.evolve(deployer_o, debug_level=1)
-    deployer_debug1.create_bare(_CONTRACT_PATH, args=[test_case])
+    deployer_debug1.create_bare(contract_path, args=[test_case])
 
     deployer_debug2 = attrs.evolve(deployer_o, debug_level=2)
     with pytest.raises(au.LogicError) as ex_info:
-        deployer_debug2.create_bare(_CONTRACT_PATH, args=[test_case, b"\x01"])
+        deployer_debug2.create_bare(contract_path, args=[test_case, b"\x01"])
     ex = ex_info.value
     assert ex.message == expected_logic_error
     assert ex.line_no is not None
     assert "💥" in "".join(ex.lines[ex.line_no - 5 : ex.line_no])
+
+
+def test_runtime_dominance(deployer_o: Deployer) -> None:
+    contract_path = _CONTRACT_DIR / "runtime_dominance.py"
+    deployer_o.create_bare(contract_path)
